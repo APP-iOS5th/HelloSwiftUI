77ce76b9a5e75df98eced548f75a8f133a897945
seonggeun an <eventscripts@nate.com>
2024-04-08 15:00:35 +0900

    SystemImage With SF Symbol

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-           
+            Label("Text", systemImage: "clock.circle").font(.title)
            
         }
     }
