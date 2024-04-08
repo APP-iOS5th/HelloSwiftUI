225de073ba0917a5b397f9360a790351b4633356
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 15:11:36 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,20 +7,10 @@
 
 import SwiftUI
 
-//아이콘 추가하기
 struct ContentView: View {
     var body: some View {
         VStack {
-            Label("Text", systemImage: "clock.circle").font(.title)
-            
-            //내가 추가한 이미지를 Assets파일에서 불러와서 출력
-            Label {
-                Text("No modifiers")
-            } icon: {
-                Image("clock-solid")
-                    .resizable()//아이콘 크기를 작게 수정해줌
-                    .frame(width: 20.0, height: 20.0)
-            }
+
         }
     }
 }
