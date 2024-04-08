d3137a32b82389592d090301f3c705e9d2d6d605
MadCow <lobasketve@naver.com>
2024-04-08 17:33:58 +0900

    change text by picker's onChange event

--- HelloSwiftUI/SegmentButtonExample.swift
+++ HelloSwiftUI/SegmentButtonExample.swift
@@ -10,18 +10,30 @@ import SwiftUI
 struct SegmentButtonExample: View {
     
     @State private var selectedColor: Color = Color.gray
+    @State private var message: String = ""
     
     var body: some View {
         VStack(spacing: 28) {
+            
+            Text(message)
+            
             Rectangle()
                 .fill(selectedColor)
             
-            Picker("Favorite Color", selection: $selectedColor) {
-                Text("Red").tag(Color.red)
-                Text("Blue").tag(Color.blue)
-                Text("Green").tag(Color.green)
+            Picker("Favorite Color", selection: $message) {
+                Text("Happy").tag("happy")
+                Text("Sad").tag("sad")
+                Text("Bored").tag("bored")
             }
             .pickerStyle(.segmented)
+            .onChange(of: message) { _, newValue in
+                switch newValue {
+                case "happy": message = "Be happy and joyuos"
+                case "sad": message = "Life can be a struggle at times"
+                case "bored": message = "Look for your purpose"
+                default: break
+                }
+            }
         }
         .padding() // MARK: default padding: 16
     }
