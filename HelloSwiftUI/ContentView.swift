7d336fb7c62f5588d304bdeb83cfdf8c80d8a096
Min-Gyeong Hwang <freemin217@gmail.com>
2024-04-08 15:10:24 +0900

    Label with custom icon from Apple Assets

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,6 +11,13 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Label("Text", systemImage: "clock.circle").font(.title)
+            Label{
+                Text("No modifiers")
+            } icon: {
+                Image("clock-solid")
+                    .resizable()
+                    .frame(width: 20.0, height: 20.0)
+            }
         }
     }
 }
