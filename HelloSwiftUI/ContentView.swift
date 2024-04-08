de7b70156b8850bc5ac70369b9c04d6855972b0c
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 14:03:10 +0900

    VStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,40 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isOn = true
-    @State private var value = 0
-    @State private var sliderValue = 0.5
+
     var body: some View {
-        VStack (alignment: .trailing, spacing: 30) {
-            Text("Default padding of 16 points")
-                .padding()
+        VStack (spacing: 20){
+//            ZStack {
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.gray)
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.purple)
+//                    .position(x: 255, y: 125)
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.pink)
+//                    .offset(x: 74, y: 125)
+//            }
+            Text("1111")
+                .font(.system(size: 40))
+                .background(Color.red)
+            Text("2222")
+                .font(.system(size: 40))
+                .background(Color.orange)
+            Text("3333")
+                .font(.system(size: 40))
+                .background(Color.green)
+            Text("4444")
+                .font(.system(size: 40))
                 .background(Color.blue)
-            Text("Default padding of 45 points")
-                .padding(45)
-                .background(Color.blue)
-            Text("Default padding of 3 points")
-                .padding(3)
-                .background(Color.blue)
-            Text("Top padding only")
-                .padding(.top)
-                .background(Color.blue)
-            Spacer(minLength: 100)
-            Text("Trailing padding only")
-                .padding(.trailing)
-                .background(Color.blue)
-            Text("Bottom padding only")
-                .padding(.bottom)
-                .background(Color.blue)
-            Text("Leading padding only")
-                .padding(.leading, 40)
-                .background(Color.blue)
-            Text("Top and Leading padding")
-                .padding([.top, .leading], 40)
-                .background(Color.blue)
-            Text("Top, Leading, Bottom padding")
-                .padding([.top, .leading, .bottom], 45)
-                .background(Color.blue)
-            
+            Text("5555")
+                .font(.system(size: 40))
+                .background(Color.purple)
         }
     }
 }
