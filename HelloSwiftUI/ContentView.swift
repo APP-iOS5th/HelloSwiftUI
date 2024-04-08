0fa52437d455fc4f5143243cd1a8adeda41394be
Jin Park <jinpark1504@gmail.com>
2024-04-08 15:00:24 +0900

    system image

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,29 +14,8 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            let myString = "Displays a string variable"
-            Text("This is my age \(myString). \nSince I am retired, \nI am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-//                .multilineTextAlignment(.trailing)
-                .truncationMode(.tail)
-                .lineLimit(3)
-            Text("Bold Text")
+            Label("Text", systemImage: "clock.circle")
                 .font(.title)
-                .bold()
-            
-            Text("Italic")
-                .font(.title2)
-                .italic()
-            Text("Underline")
-                .font(.headline)
-                .underline()
-            Text("Strikethrough")
-                .font(.subheadline)
-                .strikethrough()
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypewriter", size: 24))
-            Text("Line Spacing\nSecond Line")
-                .font(.body)
-                .lineSpacing(20)
         }
     }
 }
