9e52c15b467155aaa2d9ab1b73329a5bb322ae9d
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 16:41:46 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,32 +7,11 @@
 
 import SwiftUI
 
-extension Color {
-    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.6)
-}
 
 struct ContentView: View {
     var body: some View {
         VStack {
             
-            //Asset Color scheme 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color("custom_brown"))
-            
-            //지역변수 사용
-            let customColor = Color(red: 1, green: 0, blue: 0)
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(customColor)
-            
-            //전역변수 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(Color.myCustomColor)
         }
     }
 }
