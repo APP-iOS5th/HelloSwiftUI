4923a0f8a28f612326a4d5775008b0aaec1256c8
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-08 15:00:23 +0900

    SF Symbols

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,36 +14,8 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            let myString = "Displays a string variable"
-                        Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-                            .multilineTextAlignment(.trailing)
-                            .truncationMode(.tail)
-                            .lineLimit(3)
-            
-            Spacer()
-            
-            Text("Bold Text")
+            Label("Text", systemImage: "clock.circle")
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .bold()
-            
-            Text("Italic")
-                .font(.title2)
-                .italic()
-            
-            Text("Underlined")
-                .font(.headline)
-                .underline()
-                
-            Text("Strikethrough")
-                .font(.subheadline)
-                .strikethrough()
-            
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypewriter", size: 24))
-            
-            Text("Line Spacing\nSecond Line")
-                .font(.body)
-                .lineSpacing(20)
         }
     }
 }
