69f073489c5ecea474f03dd0eca27e4d2da85fa1
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 17:23:09 +0900

    picker

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedColor = Color.gray
 
     var body: some View {
-        VStack {
-            
+        VStack (spacing: 23){
+            Rectangle().fill(selectedColor)
+            Picker("Favorite Color", selection: $selectedColor, content: {
+                Text("Red").tag(Color.red)
+                Text("Green").tag(Color.green)
+                Text("Blue").tag(Color.blue)
+            })
+            .pickerStyle(SegmentedPickerStyle())
         }
     }
 }
