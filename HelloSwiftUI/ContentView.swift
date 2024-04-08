e82881fa3d33239a7f1629846abb26a3ae493b56
DopeKS <kyusang.dev@gmail.com>
2024-04-08 15:14:25 +0900

    SystemImage with SF Symbols ver2

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
+            Label("Text", systemImage: "clock.circle").font(.title)
         }
     }
 }
