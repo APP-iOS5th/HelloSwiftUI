a8220e862883bb1cc62d11b1bfbbd874c9498193
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 17:10:14 +0900

    button toggle

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State var colorMe = false
     var body: some View {
         VStack {
-
+            Rectangle()
+                .fill(colorMe ? Color.green : Color.gray)
+                .frame(width: 250, height: 100)
+            Button("여기를 클릭하세요") {
+                colorMe.toggle()
+            }
+            Button {
+                colorMe.toggle()
+            } label: {
+                Text("Click here")
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
+            Button {
+                colorMe.toggle()
+            } label: {
+                Image("sample_cat_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 5))
+            }
         }
     }
 }
