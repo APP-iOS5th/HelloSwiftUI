bcc46ac1f930d75b57a24c2070c744b24092c67f
seonggeun an <eventscripts@nate.com>
2024-04-08 16:36:07 +0900

    Color, Custom, Asset

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,11 +7,32 @@
 
 import SwiftUI
 
+extension Color {
+    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-         
+            //Asset Color scheme 사용
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            //  .foregroundStyle(Color("커스텀색상파일명"))
             
+            // 지역상수 사용
+            let customColor = Color(red:1, green: 0, blue: 0)
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(customColor)
+            
+            // 전역상수 사용
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color.myCustomColor)
         }
     }
 }
