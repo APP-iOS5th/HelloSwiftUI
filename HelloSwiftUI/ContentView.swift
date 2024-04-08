9531854a15e2738d22b4c5d545296e10a5718840
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 16:22:39 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,35 +11,9 @@ struct ContentView: View {
     @State private var count = 0
     
     var body: some View {
-        HStack {
-            VStack {
-                Image("sample_cat_square")
-                    .resizable()
-                    .frame(width: 250, height: 250)
-                    .aspectRatio(contentMode: .fill)
-                    .overlay(Rectangle().stroke(Color.green, lineWidth: 20)) // 테두리
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 250, height: 250)
-                    .aspectRatio(contentMode: .fill)
-                    .clipShape(Circle())
-                    .shadow(color: .orange, radius: 10, x: 50, y: 30) //그림자 좌표 지정
-                Image("sample_cat_square")
-                    .resizable()
-                    .frame(width: 250, height: 250)
-                    .aspectRatio(contentMode: .fill)
-                    .shadow(color: .red, radius: 20) // 그림자 원본뒤에
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 250, height: 250)
-                    .aspectRatio(contentMode: .fill)
-                    .clipShape(Circle()) //원형으로 깎음
-                    .opacity(0.5) //투명도
-            }
-            .padding()
+        VStack {
+            
         }
-        .padding()
-        .background(Color.gray)
     }
 }
 
