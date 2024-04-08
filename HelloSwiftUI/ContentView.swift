9f8f7b380639b60153993aba8cb131cc66d58229
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 14:28:33 +0900

    Text Formatting

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,9 +7,41 @@
 
 import SwiftUI
 
+//텍스트 작업하기
 struct ContentView: View {
     var body: some View {
         VStack {
+            let myString = "Displays a string variable"
+            //Text("Hello World, \(myString)")
+            Text("This is my age \(myString).\n Since I am retired,\n I am now eligible for a pension and Social Security so\n I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/) //정렬
+                .truncationMode(.tail)
+                .lineLimit(3)
+            
+            Spacer()
+            Text("Bold Text")
+                .font(.title)
+                .bold()
+            
+            Text("Italic")
+                .font(.title2)
+                .italic()
+            
+            Text("Underlined")
+                .font(.headline)
+                .underline()
+            
+            Text("Strikethrough")
+                .font(.subheadline)
+                .strikethrough()
+            
+            Text("Custom font Text")
+                .font(.custom("AmericanTypewriter", size: 24))
+                
+            Text("Line Spacing\nSecond Line")
+                .font(.body)
+                .lineSpacing(20)
+            
             
         }
     }
