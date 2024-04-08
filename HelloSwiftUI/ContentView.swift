5607a4442103ac50b3857bb46e78c045b6855d18
Min-Gyeong Hwang <freemin217@gmail.com>
2024-04-08 16:22:52 +0900

    Image clipShape, shadow, overlay, opacity

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,40 +10,55 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-//            Capsule().fill(Color.yellow)
-//            Circle().fill(Color.blue)
-//            Ellipse().fill(Color.brown)
-//            Rectangle().fill(Color.red)
-//            RoundedRectangle(cornerRadius: 25).fill(Color.green)
+//            이미지 모양 변경 (사각형, 원형)
+//            Image("toby")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//            Image("toby2")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .clipShape(Circle())
             
-//            Spacer()
-//            Ellipse().fill(
-//                RadialGradient(gradient: Gradient(colors: [.blue, .yellow]),
-//                               center: .top,
-//                               startRadius: 10,
-//                               endRadius: 65
-//                              ))
-//            Spacer()
-//            Ellipse().fill(
-//                RadialGradient(gradient: Gradient(colors: [.blue, .yellow]),
-//                               center: .center,
-//                               startRadius: 10,
-//                               endRadius: 65
-//                              ))
-//            Spacer()
-//            Ellipse().fill(
-//                AngularGradient(gradient: Gradient(colors: [.blue, .yellow]),
-//                                center: .center
-//                               ))
-//            Spacer()
-            Image(systemName:  "tortoise.fill")
-            Image(systemName:  "tortoise.fill").font(.largeTitle)
-            Image(systemName:  "tortoise.fill").font(.custom("", size: 50))
-            Spacer()
-            Image("clock-solid")
+//            이미지 shadow 효과
+//            Image("toby")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .shadow(color: .red, radius: 46, x: 0, y: 0)
+//            Image("toby2")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .clipShape(Circle())
+//                .shadow(color: .green, radius: 46, x: 90, y: 50)
+//            이미지 shadow 및 overlay 효과
+//            Image("toby")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .shadow(color: .red, radius: 46, x: 0, y: 0)
+//                .overlay(Rectangle().stroke(Color.blue, lineWidth:10))
+//            Image("toby2")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .clipShape(Circle())
+//                .shadow(color: .green, radius: 46, x: 90, y: 50)
+//                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
+            
+//          이미지 투명도 opacity 조절
+            Image("toby")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 150, height: 50)
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .opacity(0.5)
+            Image("toby2")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .opacity(0.25)
         }
     }
 }
