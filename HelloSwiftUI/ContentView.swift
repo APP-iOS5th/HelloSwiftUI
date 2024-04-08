2fe70db1e851685d497ddbfc728aa96d9312cf9a
Seonghoon Jung <seonghoonj@icloud.com>
2024-04-08 12:10:51 +0900

    SwiftUI_1

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isOn = true
+    @State private var value = 0
+    @State private var slidervalue = 0.5
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Slider(value: $slidervalue, in: 0...1)
+                .accentColor(.purple)
+            
+            Stepper("값 \(value))", value: $value, in: 0...10)
+            
+            Toggle("Toggle message on/off", isOn: $isOn)
+            Text("위")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            HStack {
+                Text("왼쪽")
+                Text("오른쪽")
+            }
+            .background(Color.purple)
+            ZStack {
+                Text("   배경")
+                Text("전경   ")
+            }
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                print("button1 click")
+            }
+            Button(action: {
+                print("button2 click")
+            }, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
         }
-        .padding()
     }
 }
 
