f07b0f956d94a82ddc1f6348f9327739dd7aacda
rmfls0606 <rmfls0606@naver.com>
2024-04-08 14:21:32 +0900

    text modifier Example

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,16 +11,35 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            ZStack{
-                Text("Top")
-                    .font(.system(size: 40))
-                    .background(.yellow)
-                
-                Text("Top")
-                    .font(.system(size: 40))
-                    .background(.yellow)
-                    .offset(x: 74, y: 125)
-            }
+            let myString = "Displays aa string variable"
+//            Text("Hello world \(myString)")
+            Text("This is my age\(myString). Sice I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                .truncationMode(.middle)// 생략 부분 앞쪽 생략, 중간 생략, 마지막 생략 -> default .tail
+                .lineLimit(3)
+            
+            Text("Bold Text")
+                .font(.title)
+                .bold()
+            
+            Text("Italic")
+                .font(.title2)
+                .italic()
+            
+            Text("Underlined")
+                .font(.headline)
+                .underline()
+            
+            Text("Strikethrough")
+                .font(.subheadline)
+                .strikethrough()
+            
+            Text("Custom Font Text")
+                .font(.custom("AmericanTypewriter", size: 24))
+            
+            Text("Line Spacing")
+                .font(.body)
+                .lineSpacing(10)
         }
     }
 }
