8380f8c7a22de18be5672ee686746f0bac38d01c
Hyunseo Yook <hhh3332@naver.com>
2024-04-08 14:26:13 +0900

    Text Fomatting

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,32 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-//            ZStack{
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.cyan)
-//                
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.cyan)
-//                    .position(x: 255, y: 127)
-//                
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.cyan)
-//                    .offset(x: 74, y: 125)
-//            }
+            let myString = "Display a string variable"
+            Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+                .multilineTextAlignment(.trailing)
+                .truncationMode(/*@START_MENU_TOKEN@*/.tail/*@END_MENU_TOKEN@*/)
+                .lineLimit(3)
             
-            Text("First")
-                .font(.system(size: 40))
-                .background(Color.yellow)
-            Text("Second View")
-                .font(.system(size: 40))
-                .background(Color.yellow)
+            Spacer()
+            Text("Bold Text")
+                .font(.title)
+                .bold()
+            
+            Text("Italic")
+                .font(.title2)
+                .italic()
+            
+            Text("UnderLined")
+                .font(.headline)
+                .underline()
+            
+            Text("Strikethrough")
+                .font(.subheadline)
+                .strikethrough()
+            
+            Text("Custom Font Text")
+                .font(.custom("AmericanTypewriter", size: 24))
+                
+            Text("Line Spacing\nSecond Line")
+                .font(.body)
+                .lineSpacing(20)
+     
         }
-        // VStack offset 설정
-        .offset(x: 25, y: 125)
-    
     }
 }
 
