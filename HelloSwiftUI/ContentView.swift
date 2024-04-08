05bb127f670997a8db4e9ae834b8c53dccf68045
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-08 14:00:15 +0900

    ZStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -2,42 +2,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .trailing, spacing: 24){
-            Text("Default padding of 16 points")
-                .padding()
-                .background(Color.yellow)
-            Text("Default padding of 45 points")
-                .padding(45)
-                .background(Color.yellow)
-            Text("Default padding of 3 points")
-                .padding(3)
-                .background(Color.yellow)
-            
-            Spacer()
-            
-            HStack (alignment: .bottom, spacing: 24){
-                Text("Top padding only")
-                    .padding(.top)
+        VStack {
+            //            ZStack {
+            //                Text("Top")
+            //                    .font(.system(size: 40))
+            //                    .background(Color.yellow)
+            //
+            //                Text("Top")
+            //                    .font(.system(size: 40))
+            //                    .background(Color.blue)
+            //                    .offset(x: 255, y: 127)
+            //
+            //                Text("Top")
+            //                    .font(.system(size: 40))
+            //                    .background(Color.red)
+            //                    .offset(x: 74, y: 125)
+            //            }
+            VStack {
+                Text("First")
+                    .font(.system(size: 40))
                     .background(Color.yellow)
-                Text("Trailing padding only")
-                    .padding(.trailing)
-                    .background(Color.yellow)
-                Text("Bottom padding only")
-                    .padding(.bottom)
-                    .background(Color.yellow)
+                
+                Text("Second View")
+                    .font(.system(size: 40))
+                    .background(Color.blue)
             }
-            
-            Spacer(minLength: 100)
-            
-                Text("Leading padding only")
-                    .padding(.leading, 45)
-                    .background(Color.yellow)
-                Text("Top, Leading padding")
-                    .padding([.top, .leading], 45)
-                    .background(Color.yellow)
-                Text("Top, Leading and Bottom")
-                    .padding([.top,.leading, .bottom], 45)
-                    .background(Color.yellow)
+            .offset(x: 25, y: 125)
         }
     }
 }
