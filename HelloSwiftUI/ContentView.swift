54577bd426b7b84ed3e41d42aac8062b51a3bd5e
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-08 17:23:11 +0900

    SegmentPickerStyle

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,36 +12,17 @@ extension Color {
 }
 
 struct ContentView: View {
-    @State var colorMe = false
+    @State private var selectedColor = Color.gray
     var body: some View {
-        VStack {
-            Rectangle()
-                .fill(colorMe ? Color.green : Color.gray)
-                .frame(width: 250, height: 100)
+        VStack (spacing: 28) {
+            Rectangle().fill(selectedColor)
+            Picker("Favorite Color", selection: $selectedColor, content: {
+                Text("Red").tag(Color.red)
+                Text("Green").tag(Color.green)
+                Text("Blue").tag(Color.blue)
+            })
+            .pickerStyle(SegmentedPickerStyle())
             
-            Button("여기를 클릭하세요") {
-                colorMe.toggle()
-            }
-            
-            Button {
-                colorMe.toggle()
-            } label: {
-                Text("Click Here")
-                    .font(.largeTitle)
-                    .foregroundStyle(Color.green)
-                    .padding()
-                    .border(Color.red, width: 6)
-            }
-            
-            Button {
-                colorMe.toggle()
-            } label: {
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
-            }
         }
   
     }
