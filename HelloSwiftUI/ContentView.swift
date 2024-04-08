961530a6991e0172a53e26eb7465cceff84bde47
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 13:15:10 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,40 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    //SwiftUI Slider, Stepper, @State
-    @State private var isOn = true
-    @State private var value = 0
-    @State private var sliderValue = 0.5
-    
     var body: some View {
-        VStack { //Vertical
-            Slider(value: $sliderValue, in: 0...1)
-                .accentColor(/*@START_MENU_TOKEN@*/.purple/*@END_MENU_TOKEN@*/)
+        VStack {
             
-            Stepper("값 \(value)", value: $value, in: 0...10)
-            
-            Toggle("Toggle message on/off", isOn:$isOn)
-            Text("위")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-            HStack { //Horizontal
-                Text("왼쪽")
-                Text("오른쪽")
-            }
-            .background(Color.purple)
-            ZStack {
-                Text("배경")
-                Text("전경")
-            }
-            Button("Button"){
-                print("button1 click")
-            }
-            
-            Button(action: {
-                print("button2 click")
-            }, label: {
-                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-            })
         }
     }
 }
