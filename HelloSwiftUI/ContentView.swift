90f7a93b79f77c8dc608820c17c8fc35a810a5da
seonggeun an <eventscripts@nate.com>
2024-04-08 16:18:51 +0900

    Image modifier

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,7 +10,60 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-                       
+            //이미지 크기 조정, 크롭(자르기, 채우기)
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+            
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .clipShape(Circle())
+                
+            //이미지 그림자 효과
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .shadow(color: .red, radius: 46, x: 0, y: 0)
+            
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .clipShape(Circle())
+                .shadow(color:.green, radius: 46, x: 90, y: 50)
+            
+            //이미지 테두리 오버레이
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .shadow(color: .red, radius: 46, x: 0, y: 0)
+                .overlay(Rectangle().stroke(Color.blue, lineWidth: 10))
+            
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .clipShape(Circle())
+                .shadow(color:.green, radius: 46, x: 90, y: 50)
+                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
+            
+            //이미지 투명도 조절
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                
+            
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                
             
         }
     }
