95052626b75b322ca90c791f898da44449151b70
seonggeun an <eventscripts@nate.com>
2024-04-08 14:21:28 +0900

    Text Formatting

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,7 +11,37 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
+            let myString = "Displays a string variable"
+            // Text("Hello World, \(myString)")
+            Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+                .truncationMode(.tail) //  문자열 정렬 핸들링??..
+                .lineLimit(3) // 최대출력 줄수 설정 이후는 ...표시
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
             
+            Spacer()
+            
+            Text("Bold Text")
+                .font(.title)
+                .bold()
+            
+            Text("Italic")
+                .font(.title2)
+                .italic()
+            
+            Text("Underlined")
+                .font(.headline)
+                .underline()
+            
+            Text("Strikethrough")
+                .font(.subheadline)
+                .strikethrough()
+            
+            Text("Custom Font Text")
+                .font(.custom("AmericanTypeWriter", size: 24))
+            
+            Text("Line Spacing")
+                .font(.body)
+                .lineSpacing(10)
         }
     }
 }
