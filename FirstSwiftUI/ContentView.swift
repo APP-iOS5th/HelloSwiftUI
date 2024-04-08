e976cd01bac1fc5cb57b96936f403eb8100e314a
avan31017 <avan31017@gmail.com>
2024-04-08 15:10:58 +0900

    Image

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -13,35 +13,20 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-//            Text("Hello World & \(myString)")
-            Text("Displays a string variable Displays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a string variableDisplays a str1ng variable")
-                .multilineTextAlignment(.trailing) // 정렬
-                .lineLimit(3)
-                .truncationMode(.middle)
-            
-            
-            Text("Bold Text")
-                .font(.title) // 크기
-                .fontWeight(.bold)
-            
-            Text("Italic")
-                .font(.title2)
-                .italic()
-            
-            Text("UnderLined")
-                .font(.headline)
-                .underline()
-            
-            Text("Strikethrough")
-                .font(.subheadline)
-                .strikethrough()
-            
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypewriter", size: 24))
-            
-            Text("Line Spacing")
-                .font(.body)
-                .lineSpacing(10)
+            Image(systemName: "clock.circle")
+            Label("Txt", systemImage: "person.fill")
+            Image("bowl-food-solid")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+            Label {
+                Text("This is rice")
+            } icon: {
+                Image("bowl-food-solid")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20)
+            }
         }
     }
 }
