89d6e75535320a87ea21a9dfcb82d6ca230c1b48
kyhlsd <kmyghn@gmail.com>
2024-04-08 15:13:20 +0900

    Label with custom Icon

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,38 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack() {
-            let myString = "Displays a string variable"
-            Text("Hello World, \(myString)")
-            Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                .truncationMode(/*@START_MENU_TOKEN@*/.tail/*@END_MENU_TOKEN@*/)
-                .lineLimit(3)
-            
-            Spacer()
-            
-            Text("Bold Text")
-                .font(.title)
-                .bold()
-            
-            Text("Italic Text")
-                .font(.title2)
-                .italic()
-            
-            Text("Underlined Text")
-                .font(.headline)
-                .underline()
-            
-            Text("Strikethrough Text")
-                .font(.subheadline)
-                .strikethrough()
-            
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypewriter", size: 24))
-                
-            Text("Line Spacing\n Second line")
-                .font(.body)
-                .lineSpacing(10)
-            
+            Label("Text", systemImage: "clock.circle").font(.title)
+            Label {
+                Text("No modifiers")
+            } icon: {
+                Image("clock-solid")
+                    .resizable()
+                    .frame(width: 20.0, height: 20.0)
+            }
         }
     }
 }
