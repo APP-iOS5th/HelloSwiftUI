34248d4058220dd0efed20b8cf1e474cc677979c
seokyung402 <wni402@naver.com>
2024-04-08 14:06:04 +0900

    Position with VStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,45 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isOn = true
-    @State private var value = 0
-    @State private var sliderValue = 0.5
-
     var body: some View {
-        VStack (alignment: .trailing, spacing: 24) {
-            Text("Default padding of 16 points")
-                .padding()
+        VStack {
+//            ZStack {
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.yellow)
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.yellow)
+//                    .position(x: 255, y: 127)
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.yellow)
+//                    .offset(x: 74, y: 125)
+//            }
+            Text("First")
+                .font(.system(size: 40))
                 .background(Color.yellow)
-            Text("Default padding of 45 points")
-                .padding(45)
-                .background(Color.yellow)
-            Text("Default padding of 3 points")
-                .padding(3)
-                .background(Color.yellow)
-            Spacer()
-            HStack (alignment: .bottom){
-                Text("Top padding only")
-                    .padding(.top)
-                    .background(Color.yellow)
-                Text("Trailing padding only")
-                    .padding(.trailing)
-                    .background(Color.yellow)
-                Text("Bottom padding only")
-                    .padding(.bottom)
-                    .background(Color.yellow)
-            }
-            Spacer(minLength: 100)
-            Text("Leading padding only")
-                .padding(.leading)
-                .background(Color.yellow)
-            Spacer()
-            Text("Top, Leading padding only")
-                .padding([.top, .leading], 45)
-                .background(Color.yellow)
-            Text("Top, Leading, Bottom padding only")
-                .padding([.top, .leading, .bottom], 45)
+            Text("Second")
+                .font(.system(size: 40))
                 .background(Color.yellow)
         }
+        //VStack offset
+        .offset(x: 25, y: 125)
     }
 }
 
