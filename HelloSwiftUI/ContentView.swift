c358a0e0ab97ff00b67a809edc258eb392c00b27
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 13:21:52 +0900

    Padding

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,6 +13,33 @@ struct ContentView: View {
     @State private var sliderValue = 0.5
     var body: some View {
         VStack {
+            Text("Default padding of 16 points")
+                .padding()
+                .background(Color.blue)
+            Text("Default padding of 45 points")
+                .padding(45)
+                .background(Color.blue)
+            Text("Default padding of 3 points")
+                .padding(3)
+                .background(Color.blue)
+            Text("Top padding only")
+                .padding(.top)
+                .background(Color.blue)
+            Text("Trailing padding only")
+                .padding(.trailing)
+                .background(Color.blue)
+            Text("Bottom padding only")
+                .padding(.bottom)
+                .background(Color.blue)
+            Text("Leading padding only")
+                .padding(.leading, 40)
+                .background(Color.blue)
+            Text("Top and Leading padding")
+                .padding([.top, .leading], 40)
+                .background(Color.blue)
+            Text("Top, Leading, Bottom padding")
+                .padding([.top, .leading, .bottom], 45)
+                .background(Color.blue)
             
         }
     }
