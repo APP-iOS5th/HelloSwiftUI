db64f385fa03c9b8d5385f81cd8f22fd3b659a2a
Jinx <jinxwift@gmail.com>
2024-04-08 13:23:29 +0900

    v stack 패딩 얼라인먼트

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,53 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isOn = true
-    @State private var value = 0
-    @State private var sliderValue = 0.5
-    
     var body: some View {
-        VStack {
-            Slider(value: $sliderValue, in: 0...1)
-                .tint(Color.red)
+        VStack (alignment: .leading, spacing: 24) {
             
-            Stepper("값 \(value)", value: $value, in: 0...10)
+            Text("Default padding of 16 points")
+                .padding()
+                .background(Color.yellow)
+            Text("Default padding of 45 points")
+                .padding(45)
+                .background(Color.yellow)
+            Text("Default padding of 3 points")
+                .padding(3)
+                .background(Color.yellow)
+            Text("Top padding only")
+                .padding(.top)
+                .background(Color.yellow)
+            Text("Trailing padding only")
+                .padding(.trailing)
+                .background(Color.yellow)
+            Text("Bottom padding only")
+                .padding(.bottom)
+                .background(Color.yellow)
+            Text("Leading padding only")
+                .padding(.leading)
+                .background(Color.yellow)
+            Text("Top, Leading and Bottom")
+                .padding([.top,.leading,.bottom], 45)
+                .background(Color.yellow)
             
-            Toggle("Toggle message on/off", isOn: $isOn)
-            Text("위")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-
-            HStack {
-                Text("왼쪽")
-                Text("오른쪽")
-            }
-            .background(Color.yellow)
-            ZStack {
-                Text("배경")
-                Text("전경")
-            }
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                print("button1 click")
-            }
-                Button(action: {
-                    print("button2 click")
-                }, label: {
-                    Text("Button")
-                })
-            }
-//        .background(Color.yellow)
         }
     }
     
-    #Preview {
-        ContentView()
-    }
-    
-    /*
-     Swift 3.0 이전 프리뷰
-     struct ContentView_Previews: PreviewProvider {
-     static var previews: some View {
-     ContentView()
-     }
-     }
-     */
+}
+
+
+#Preview {
+    ContentView()
+}
+
+
+/*
+ Swift 3.0 이전 프리뷰
+ struct ContentView_Previews: PreviewProvider {
+ static var previews: some View {
+ ContentView()
+ }
+ }
+ */
+
