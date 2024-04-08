feafb81ac144631659528b59895c531003209109
DopeKS <kyusang.dev@gmail.com>
2024-04-08 15:36:39 +0900

    Iamge, Image

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -17,30 +17,38 @@ struct ContentView: View {
 //            Rectangle().fill(Color.secondary)
 //            RoundedRectangle(cornerRadius:  31).fill(Color.green)
             
-            Spacer()
-            Ellipse().fill(
-                RadialGradient(
-                    gradient: Gradient(colors:[.blue,.yellow]),
-                    center: .top,
-                    startRadius: 10,
-                    endRadius: 65
-                ))
-            Spacer()
-            Ellipse().fill(
-                RadialGradient(
-                    gradient: Gradient(colors:[.blue,.yellow]),
-                    center: .center,
-                    startRadius: 10,
-                    endRadius: 65
-                ))
-            Spacer()
-            Ellipse().fill(
-                AngularGradient(
-                    gradient: Gradient(colors:[.blue,.yellow]),
-                    center: .center
-                ))
-            Spacer()
+//            Spacer()
+//            Ellipse().fill(
+//                RadialGradient(
+//                    gradient: Gradient(colors:[.blue,.yellow]),
+//                    center: .top,
+//                    startRadius: 10,
+//                    endRadius: 65
+//                ))
+//            Spacer()
+//            Ellipse().fill(
+//                RadialGradient(
+//                    gradient: Gradient(colors:[.blue,.yellow]),
+//                    center: .center,
+//                    startRadius: 10,
+//                    endRadius: 65
+//                ))
+//            Spacer()
+//            Ellipse().fill(
+//                AngularGradient(
+//                    gradient: Gradient(colors:[.blue,.yellow]),
+//                    center: .center
+//                ))
+//            Spacer()
             
+            Image(systemName: "tortoise.fill")
+            Image(systemName: "tortoise.fill").font(.title)
+            Image(systemName: "tortoise.fill").font(.custom("", size: 50))
+            Spacer()
+            Image("clock-solid")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150, height: 50)
         }
     }
 }
