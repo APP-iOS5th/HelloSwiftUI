3b3bdd7aad283f66d876e3e23845904e6a7d7616
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 14:05:54 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,34 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            /*
-             //offset은 자기가 그려지는 위치기준
-             //position은 부모기준으로 나를 어디에 위치시킬거냐
-            ZStack {
-                Text("Top")
-                    .font(.system(size: 40))
-                    .background(Color.blue)
-                Text("Top")
-                    .font(.system(size: 40))
-                    .background(Color.blue)
-                    .position(x:255 ,y:127)
-                Text("Top")
-                    .font(.system(size: 40))
-                    .background(Color.blue)
-                    .offset(x: 74, y: 124)
-                
-            }
-            */
             
-            Text("First")
-                .font(.system(size: 40))
-                .background(Color.blue)
-            Text("Second View")
-                .font(.system(size: 40))
-                .background(Color.blue)
         }
-        //VStack offset설정
-        .offset(x: 25, y: 125)
     }
 }
 
