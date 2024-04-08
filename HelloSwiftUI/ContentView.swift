8d790cdefba303d303bf108883737f243649a1c6
jinwoong <baseball0020@gmail.com>
2024-04-08 15:31:30 +0900

    image modifiers

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,27 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            let myString = "Displays a string variable"
-            Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-                .multilineTextAlignment(.trailing)
-                .truncationMode(.tail)
-                .lineLimit(3)
+            Image(systemName: "tortoise.fill")
+            Image(systemName: "tortoise.fill").font(.largeTitle)
+            Image(systemName: "tortoise.fill").font(.custom("", size: 50))
             Spacer()
-            Text("Bold Text")
-                .font(.title)
-                .bold()
-            Text("Itailc Text")
-                .font(.title2)
-                .italic()
-            Text("Underlined Text")
-                .font(.headline)
-                .underline()
-            Text("Strikethrough Text")
-                .font(.body)
-                .strikethrough()
-            Text("First Line\nSecond Line")
-                .font(.body)
-                .lineSpacing(20)
+            Image("clock-solid")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150, height: 50)
         }
     }
 }
