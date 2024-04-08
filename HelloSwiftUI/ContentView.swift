98b86c9f12944179a5e2cbc8b81a64a504540f93
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 15:28:58 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,32 +11,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            //            Capsule().fill(Color.yellow)
-            //            Circle().fill(Color.blue)
-            //            Ellipse().fill(Color.brown)
-            //            Rectangle().fill(Color.red)
-            //            RoundedRectangle(cornerRadius: 20).fill(Color.green)
-            
-            Spacer()
-            Ellipse().fill(
-                RadialGradient(
-                    gradient: Gradient(colors: [.blue, .yellow]),
-                        center: .top,
-                        startRadius: 10,
-                        endRadius: 65
-            ))
-            Ellipse().fill(
-                RadialGradient(
-                    gradient: Gradient(colors: [.blue, .yellow]),
-                        center: .center,
-                        startRadius: 10,
-                        endRadius: 65
-            ))
-            Ellipse().fill(
-                AngularGradient(
-                    gradient: Gradient(colors: [.blue, .yellow]),
-                        center: .center
-            ))
+
         }
     }
 }
