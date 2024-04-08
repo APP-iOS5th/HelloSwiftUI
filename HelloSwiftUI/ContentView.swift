25dfeef9d65c8babde2ec6dc0c7b43e83358ead0
jihye <ganzi410@gmail.com>
2024-04-08 13:33:26 +0900

    VStack, HStack alignment & Spacer

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
     @State private var sliderValue = 0.5
     
     var body: some View {
-        VStack {
+        // vstack alignment - leading, trailing, center
+        VStack (alignment: .leading, spacing: 24) {
             Text("Default padding of 16 points")
                 .padding()
                 .background(.yellow)
@@ -23,19 +24,23 @@ struct ContentView: View {
             Text("Default padding of 3 points")
                 .padding(3)
                 .background(.yellow)
-            
-            Text("Top padding only")
-                .padding(.top)
-                .background(Color.teal)
-            Text("Trailing padding only")
-                .padding(.trailing)
-                .background(Color.teal)
-            Text("Botton padding only")
-                .padding(.bottom)
-                .background(Color.teal)
+            Spacer()
+            // hstack alignment - top, bottom,  center
+            HStack (alignment: .bottom, spacing: 24) {
+                Text("Top padding only")
+                    .padding(.top)
+                    .background(Color.teal)
+                Text("Trailing padding only")
+                    .padding(.trailing)
+                    .background(Color.teal)
+                Text("Botton padding only")
+                    .padding(.bottom)
+                    .background(Color.teal)
+            }
+            Spacer()
             Text("Leading padding of 45 points only")
                 .padding(.leading, 45)
-                .background(Color.teal)
+                .background(Color.yellow)
             
             Text("Top, Leading paddings")
                 .padding([.top, .leading], 45)
