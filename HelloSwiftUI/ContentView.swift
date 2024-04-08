bdfe2b1df5e06897e7473cc0bc4d7a32d6725e5e
DopeKS <kyusang.dev@gmail.com>
2024-04-08 15:34:13 +0900

    Image, gradient-filled ellipses

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,11 +11,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Capsule().fill(Color.indigo)
-            Circle().fill(Color.pink)
-            Ellipse().fill(Color.purple)
-            Rectangle().fill(Color.secondary)
-            RoundedRectangle(cornerRadius:  31).fill(Color.green)
+//            Capsule().fill(Color.indigo)
+//            Circle().fill(Color.pink)
+//            Ellipse().fill(Color.purple)
+//            Rectangle().fill(Color.secondary)
+//            RoundedRectangle(cornerRadius:  31).fill(Color.green)
+            
+            Spacer()
+            Ellipse().fill(
+                RadialGradient(
+                    gradient: Gradient(colors:[.blue,.yellow]),
+                    center: .top,
+                    startRadius: 10,
+                    endRadius: 65
+                ))
+            Spacer()
+            Ellipse().fill(
+                RadialGradient(
+                    gradient: Gradient(colors:[.blue,.yellow]),
+                    center: .center,
+                    startRadius: 10,
+                    endRadius: 65
+                ))
+            Spacer()
+            Ellipse().fill(
+                AngularGradient(
+                    gradient: Gradient(colors:[.blue,.yellow]),
+                    center: .center
+                ))
+            Spacer()
+            
         }
     }
 }
