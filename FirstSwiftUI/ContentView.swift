64286041c590bec72ff398e71bee55ba50b4745d
avan31017 <avan31017@gmail.com>
2024-04-08 16:20:02 +0900

    Cut & Border & Shadow -> Image

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -11,59 +11,52 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("Various of Circle")
+            Text("Cut Image")
             HStack {
-                Capsule()
-                    .fill(.yellow)
-                    .frame(width: 100, height: 150)
-                Circle()
-                    .fill(.blue)
-                    .frame(width: 100)
-                Ellipse()
-                    .fill(.green)
-                    .frame(width: 100, height: 150)
-            }
-            HStack {
-                Ellipse()
-                    .fill(
-                        RadialGradient(gradient: Gradient(colors: [.pink, .brown]), center: .top, startRadius: 10, endRadius: 50)
-                    )
-                    .frame(width: 100, height: 150)
-                Ellipse().fill(
-                    RadialGradient(
-                        gradient: Gradient(colors:[.blue,.yellow]),
-                        center: .center,
-                        startRadius: 10,
-                        endRadius: 65
-                    ))
-                .frame(width: 100, height: 150)
-                Ellipse().fill(
-                    AngularGradient(
-                        gradient: Gradient(colors:[.blue,.yellow]),
-                        center: .center
-                    ))
-                .frame(width: 100, height: 150)
-            }
-            .padding(.bottom, 30)
-            Text("Various of Rectangle")
-            HStack {
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: 100, height: 100)
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(.purple)
-                    .frame(width: 100, height: 100)
-            }
-            .padding(.bottom, 30)
-            Text("Image example")
-            HStack {
-                Image(systemName: "tortoise.fill")
-                Image(systemName: "tortoise.fill").font(.largeTitle)
-                Image(systemName: "tortoise.fill").font(.custom("", size: 50))
-                Image("bowl-food-solid")
+                Image("gecko")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 150, height: 50)
+                    .frame(width: 120)
+                Image("gecko")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120)
+                    .clipShape(Circle())
+                Image("gecko")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120)
+                    .clipShape(Circle())
+                    .shadow(color: .blue, radius: 10, x: 5, y:5)
+            }
+            .padding(.bottom, 30)
+            Text("Border/Shadow Cutted Image")
+            HStack {
+                Image("gecko")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120)
+                    .opacity(0.2)
+                Image("gecko")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120)
+                    .clipShape(Circle())
+                    .shadow(color: .blue, radius: 10, x: 5, y:5)
+                    .overlay {
+                        Rectangle().stroke(.yellow, lineWidth: 5)
+                    }
+                Image("gecko")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120)
+                    .clipShape(Circle())
+                    .shadow(color: .blue, radius: 10, x: 5, y:5)
+                    .overlay {
+                        Circle().stroke(.blue, lineWidth: 5)
+                            .opacity(0.3)
+                    }
+                    
             }
         }
     }
