2ce32a002996ab1fcc351b2657ff5f156e02236a
yehjinjang <j95033176@gmail.com>
2024-04-08 16:28:54 +0900

    image effects

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -3,56 +3,68 @@
 //  HelloSwiftUI
 //
 //  Created by 장예진 on 4/8/24.
-//
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View{
+    var body: some View {
         VStack {
-            // sf symbol pic usage
-//            Label("what time is it?",
-//                  systemImage: "clock.circle").font(.title)
-//            Label{
-//                Text("No modifiers")
-//            } icon: {
-//                Image("clock-solid")
-//                    .resizable()
-//                    .frame(width:20.0, height: 20.0)
-//            }
-            //도형
-//            //capsule(알약모양 = 타원형)
-//            Capsule().fill(Color.yellow)
-//            Circle().fill(Color.brown)
-//            Ellipse().fill(Color.pink)
-//            Rectangle().fill(Color.red)
-//            RoundedRectangle(cornerRadius: 50).fill(Color.green)
-//            Capsule().fill(Color.yellow)
-//            
-//            Spacer()
-//            Ellipse().fill(
-//                RadialGradient(
-//                    gradient: Gradient(colors:[.blue,.yellow]), center: .top, startRadius: 10, endRadius: 65))
-//            Spacer()
+//            이미지 크기 조정, 크롭(자르기, 원형)
+//            Image("sample_dog_square")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
 //
-//            Ellipse().fill(
-//                RadialGradient(
-//                    gradient: Gradient(colors:[.blue,.yellow]), center: .center, startRadius: 10, endRadius: 65))
-//            Spacer()
+//            Image("sample_cat_square")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .clipShape(Circle())
+         
+//          이미지 그림자 효과
+//            Image("sample_dog_square")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .shadow(color: .red, radius: 46, x: 0, y: 0)
 //
-//            Ellipse().fill(
-//                RadialGradient(
-//                    gradient: Gradient(colors:[.blue,.yellow]), center: .top))
-            Spacer()
-            Image(systemName: "tortoise.fill")
-            Image(systemName: "tortoise.fill").font(.largeTitle)
-            Image(systemName: "tortoise.fill").font(.custom("",size: 50))
-            Spacer()
-            Image("clock-solid")
+//            Image("sample_cat_square")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .clipShape(Circle())
+//                .shadow(color: .green, radius: 46, x: 90, y: 50)
+
+//          이미지 테두리 오버레이
+//            Image("sample_dog_square")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .shadow(color: .red, radius: 46, x: 0, y: 0)
+//                .overlay(Rectangle().stroke(Color.blue, lineWidth: 10))
+//
+//            Image("sample_cat_square")
+//                .resizable()
+//                .frame(width: 250, height: 250)
+//                .aspectRatio(contentMode: .fill)
+//                .clipShape(Circle())
+//                .shadow(color: .green, radius: 46, x: 90, y: 50)
+//                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
+
+//          이미지 투명도 조절
+            Image("sample_dog_square")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width:150,height: 50)
-            //fit과 fill의 차이 알기
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .opacity(0.5)
+            
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .opacity(0.25)
+
+            
         }
     }
 }
@@ -61,12 +73,11 @@ struct ContentView: View {
     ContentView()
 }
 
-
 /*
- swift 3.0 이전 preview
-struct contentview_previews : PreviewProvider {
-    static var previews: some view{
-        ContentView()
-    }
+ Swift 3.0 이전 프리뷰
+ struct ContentView_Previews: PreviewProvider {
+ static var previews: some View {
+ ContentView()
  }
-*/
+ }
+ */ 
