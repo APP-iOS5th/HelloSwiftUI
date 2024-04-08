7a5c4e3def6ac03003ebe8675226babd9c997fc9
jmbae <jmbae@codegrove.co.kr>
2024-04-08 17:14:35 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -15,31 +15,7 @@ struct ContentView: View {
     @State var colorMe = false
     var body: some View {
         VStack {
-            Rectangle()
-                .fill(colorMe ? Color.green : Color.gray)
-                .frame(width: 250, height: 100)
 
-            Button("여기를 클릭하세요") {
-                colorMe.toggle()
-            }
-            Button {
-                colorMe.toggle()
-            } label: {
-                Text("Click here")
-                    .font(.largeTitle)
-                    .foregroundColor(.green)
-                    .padding()
-                    .border(Color.red, width: 6)
-            }
-            Button {
-                colorMe.toggle()
-            } label: {
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
-            }
         }
     }
 }
