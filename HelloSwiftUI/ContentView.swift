397f0d5258ca099def519c9ef62265c83f524422
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-08 13:17:03 +0900

    padding

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,41 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isOn = true
-    @State private var value = 0
-    @State private var sliderValue = 0.5
-    
     var body: some View {
         VStack {
-            Slider(value: $sliderValue, in: 0...1)
-                .tint(.brown)
             
-            Stepper("값 \(value)", value: $value, in: 0...10)
-            
-            Toggle("Toggle message on/off", isOn: $isOn)
-            Text("위")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .background(Color.purple)
+            Text("Default padding of 16 points")
                 .padding()
-            HStack {
-                Text("왼쪽")
-                Text("오른쪽")
-            }
-            ZStack {
-                Text("배경")
-                Text("전경")
-            }
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                print("button1 click")
-            }
-            Button(action: {
-                print("button2 click")
-            }, label: { //action 은 클릭했을 때 하는 일
-                Text("Button")
-            })
+                .background(Color.yellow)
+            
+            Text("Default padding of 45 points")
+                .padding(45)
+                .background(Color.yellow)
+                
+            Text("Default padding of 3 points")
+            
+            Text("Top padding only")
+                .padding(.top)
+                .background(Color.yellow)
+            
+            Text("Trailing padding only")
+                .padding(.trailing)
+                .background(Color.yellow)
+            
+            Text("Bottom padding only")
+                .padding(.bottom)
+                .background(Color.yellow)
+            
+            Text("Leading padding only")
+                .padding(.leading, 30)
+                .background(Color.yellow)
+            
+            Text("Top, Leading padding")
+                .padding([.top, .leading], 45)
+                .background(Color.yellow)
+            Text("Top, Leading and Bottom")
+                .padding([.top, .leading, .bottom])
+                .background(Color.yellow)
         }
-        //.background(Color.purple)
+        
     }
 }
 
@@ -51,10 +53,10 @@ struct ContentView: View {
 }
 
 /*
-    Swift 3.0 이전 프리뷰
+ Swift 3.0 이전 프리뷰
  struct ContentView_Previews: PreviewProvider {
-    static var previews: som View {
-        ContentView()
-    }
-}
+ static var previews: som View {
+ ContentView()
+ }
+ }
  */
