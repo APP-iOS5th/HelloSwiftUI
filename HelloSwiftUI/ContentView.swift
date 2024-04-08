a4231ee95d594caea20792860fcfbe7cdd6c901e
DopeKS <kyusang.dev@gmail.com>
2024-04-08 10:04:05 +0900

    #Preview 수정자 설명 추가 (이번버전)

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,11 +14,22 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                
         }
         .padding()
     }
 }
 
+/// 계속 개발중, 6월이 지나면 어떻게 바뀔지 모름.
 #Preview {
     ContentView()
 }
+
+/**
+ Swift 3.0 이전 프리뷰
+ struct ContentView_Previews:PreviewProvider {
+    static var previews: some View {
+            ContentView()
+    }
+ }
+ */
