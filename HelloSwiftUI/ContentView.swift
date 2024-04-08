348a2951ee0d3d45db8a7b6568bbb700deb43781
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 16:21:51 +0900

    Image modifiers

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,7 +10,64 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-    
+            /*
+             //이미지 크기 조정, 크롬(자르기, 원형)
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+            
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            */
+            
+            /*
+            //이미지 그림자 효과
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .shadow(color: .red, radius: 46, x: 0, y: 0)
+            
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .shadow(color:.green, radius: 46, x: 90, y: 50)
+            */
+            /*
+            //이미지 테두리 오버레이
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .shadow(color: .red, radius: 46, x: 0, y: 0)
+                .overlay(Rectangle().stroke(Color.blue, lineWidth: 10))
+            
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .shadow(color:.green, radius: 46, x: 90, y: 50)
+                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
+             */
+            //이미지 투명도 조절
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .opacity(0.5)
+            
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .opacity(0.25)
         }
     }
 }
