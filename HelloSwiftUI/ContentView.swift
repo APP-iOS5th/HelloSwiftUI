389a28eb1b15d1ac8f40bb0dcfddf910d1ed2f5c
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 17:14:52 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,37 +9,9 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var colorMe = true
     var body: some View {
         VStack {
             
-            Rectangle()
-                .fill(colorMe ? Color.green : Color.gray)
-                .frame(width: 250, height: 100)
-            //후행클로저
-            Button("여기를 클릭하세요") {
-                print("클릭시 실행코드 1")
-            }
-            
-            Button {
-                print("클릭시 실행코드 2")
-            } label: {
-                 Text("Click here")
-                    .font(.largeTitle)
-                    .foregroundColor(.green)
-                    .padding()
-                    .border(Color.red, width: 6)
-            }
-            
-            Button {
-                print("클릭시 실행코드 3")
-            } label:{
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
-            }
         }
     }
 }
