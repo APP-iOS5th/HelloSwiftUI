917885dacf50b473ec0438b7020836ff5930718c
Hyunseo Yook <hhh3332@naver.com>
2024-04-08 14:05:08 +0900

    Position with VStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,46 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .trailing, spacing: 24){
-           Text("Default padding of 16 points")
-                .padding()
+        VStack {
+//            ZStack{
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.cyan)
+//                
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.cyan)
+//                    .position(x: 255, y: 127)
+//                
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.cyan)
+//                    .offset(x: 74, y: 125)
+//            }
+            
+            Text("First")
+                .font(.system(size: 40))
                 .background(Color.yellow)
-            
-            Text("Default padding of 45 points")
-                 .padding(45)
-                 .background(Color.yellow)
-            
-            Text("Default padding of 3 points")
-                 .padding(3)
-                 .background(Color.yellow)
-            Spacer()
-            HStack (alignment: .bottom, spacing: 24) {
-                
-                Text("Top padding only")
-                    .padding(.top)
-                    .background(Color.yellow)
-                
-                Text("Trailing padding only")
-                    .padding(.trailing)
-                    .background(Color.yellow)
-                
-                Text("Bottom padding only")
-                    .padding(.bottom)
-                    .background(Color.yellow)
-            }
-            Spacer()
-            Text("Leading padding only")
-                .padding(.leading, 45)
-                 .background(Color.yellow)
-            
-            Text("Top, Leading padding")
-                .padding([.top, .leading], 45)
-                 .background(Color.yellow)
-
-            Text("Top, Leading and Bottom")
-                .padding([.top, .leading, .bottom], 45)
-                 .background(Color.yellow)
+            Text("Second View")
+                .font(.system(size: 40))
+                .background(Color.yellow)
         }
+        // VStack offset 설정
+        .offset(x: 25, y: 125)
+    
     }
 }
 
