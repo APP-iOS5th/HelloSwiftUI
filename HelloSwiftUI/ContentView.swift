56086fa94dad3bb5242e8ffb873dc53a68d90fce
YACHAE <104hsh76@gmail.com>
2024-04-08 15:12:45 +0900

    addClockImage

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,29 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .trailing, spacing: 24) {
-//            ZStack {
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.yellow)
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.yellow)
-//                    .position(x: 255, y: 127)
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.yellow)
-//                    .offset(x: 74, y: 125)
-//            }
-            Text ("Frist")
-                .font(.system(size: 40))
-                .background(.yellow)
-            Text ("Second View")
-                .font(.system(size: 40))
-                .background(.yellow)
+        VStack {
+            Label("Text", systemImage: "clock.circle").font(.title)
+            Label {
+                Text("No modifires")
+            } icon: {
+                Image("clock-solid")
+                    .resizable()
+                    .frame(width: 20.0, height: 20.0)
+            }
         }
-        // VStack offset 설정
-        .offset(x: 25, y: 125)
     }
 }
 
