ee626f7cfa6a7b824e4e928f2cd9b6420b2403b1
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 17:37:50 +0900

    tag modifier

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,17 +9,27 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var selectedColor = Color.gray
+    @State private var message = ""
     
     var body: some View {
         VStack(spacing: 28) {
-            Rectangle().fill(selectedColor)
-            Picker("Favorite Color", selection: $selectedColor, content: {
-                Text("Red").tag(Color.red)
-                Text("Green").tag(Color.green)
-                Text("Blue").tag(Color.blue)
+            Text(message)
+            Picker("Favorite Color", selection: $message, content: {
+                Text("Happy").tag("happy")
+                Text("Sad").tag("sad")
+                Text("Bored").tag("bored")
             })
             .pickerStyle(SegmentedPickerStyle())
+            .onChange(of: message) {
+                newValue in
+                switch newValue {
+                case "happy": message = "Be happy and joyous"
+                case "sad": message = "Life can be a struggle at times"
+                case "bored": message = "Look for your purpose"
+                default:
+                    break
+                }
+            }
         }
     }
 }
