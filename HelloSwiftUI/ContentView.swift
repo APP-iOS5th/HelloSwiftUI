5e306e84ca9971c4c20ef2a24380e01b88051734
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 15:20:31 +0900

    Gradient

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
     var body: some View {
         VStack {
-            Capsule().fill(Color.yellow)
-            Circle().fill(Color.blue)
-            Ellipse().fill(Color.brown)
-            Rectangle().fill(Color.red)
-            RoundedRectangle(cornerRadius: 20).fill(Color.green)
+            //            Capsule().fill(Color.yellow)
+            //            Circle().fill(Color.blue)
+            //            Ellipse().fill(Color.brown)
+            //            Rectangle().fill(Color.red)
+            //            RoundedRectangle(cornerRadius: 20).fill(Color.green)
+            
+            Spacer()
+            Ellipse().fill(
+                RadialGradient(
+                    gradient: Gradient(colors: [.blue, .yellow]),
+                        center: .top,
+                        startRadius: 10,
+                        endRadius: 65
+            ))
+            Ellipse().fill(
+                RadialGradient(
+                    gradient: Gradient(colors: [.blue, .yellow]),
+                        center: .center,
+                        startRadius: 10,
+                        endRadius: 65
+            ))
+            Ellipse().fill(
+                AngularGradient(
+                    gradient: Gradient(colors: [.blue, .yellow]),
+                        center: .center
+            ))
         }
     }
 }
@@ -26,8 +47,8 @@ struct ContentView: View {
 /*
  swift 3.0이전
  struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        contentView()
-    }
+ static var previews: some View {
+ contentView()
+ }
  }
  */
