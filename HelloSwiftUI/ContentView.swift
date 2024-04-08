41778a6560d9cbb9613afc55892d3c8963a3d881
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-08 16:34:00 +0900

    Color setting

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,64 +7,33 @@
 
 import SwiftUI
 
+extension Color {
+    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            //이미지 크기 조정, 크롭(자르기)
-            //            Image("sample_dog_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //
-            //            Image("sample_cat_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-            //                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            //
-            //            //이미지 그림자 효과
-            //            Image("sample_dog_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //                .shadow(color: .red, radius: 46, x: 0, y: 0)
-            //
-            //            Image("sample_cat_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-            //                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            //                .shadow(color: .green, radius: 46, x: 90, y: 50)
-            
-            //            Image("sample_dog_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //                .shadow(color: .red, radius: 46, x: 0, y: 0)
-            //                .overlay(Rectangle().stroke(Color.blue, lineWidth: 10))
-            //
-            //            Image("sample_cat_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-            //                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            //                .shadow(color: .green, radius: 46, x: 90, y: 50)
-            //                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
-            //이미지 투명도 조절
-            Image("sample_dog_square")
+            //Asset Color scheme 사용
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .opacity(0.5)
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color("custom_brown"))
             
+            // 지역변수(상수) 사용
+            let customColor = Color(red: 1, green: 0, blue: 0)
             
-            Image("sample_cat_square")
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                .opacity(0.25)
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color(customColor))
             
-            
+            // 전역변수(상수) 사용
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color.myCustomColor)
+        
         }
   
     }
