62ebe1c127fdff9a2441a6eb380f425658f67aea
DopeKS <kyusang.dev@gmail.com>
2024-04-08 16:30:25 +0900

    이미지 크기 조정, 크롭(자르기, 원형)

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,7 +11,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            /// 이미지 크기 조정, 크롭(자르기, 원형)
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
             
+            Image("sample_cat_square")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .aspectRatio(contentMode: .fill)
+                .clipShape(Circle())
         }
     }
 }
