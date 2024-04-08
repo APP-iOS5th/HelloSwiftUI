ea6a972f825831bd8fb0f5278f024f1200bb1b77
DopeKS <kyusang.dev@gmail.com>
2024-04-08 17:21:13 +0900

    Button @ViewBuilder

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,7 +11,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Button("여기를 클릭하세요") {
+                print("클릭 시 실행코드 1")
+            }
             
+            Button {
+                print("클릭 시 실행코드 2")
+            } label: {
+                Text("Click here")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+                    .border(Color.orange, width: 6)
+            }
+            
+            Button {
+                print("클릭 시 실행코드 3")
+            } label: {
+                Image("sample_dog_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 6))
+            }
         }
     }
 }
