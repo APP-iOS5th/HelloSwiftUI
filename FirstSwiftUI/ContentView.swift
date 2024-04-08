5e5982c5f108d1f344a4341036aa8afd8d81ee23
avan31017 <avan31017@gmail.com>
2024-04-08 15:27:50 +0900

    Shape

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -9,23 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let myString = "Displays a string variable"
-    
     var body: some View {
         VStack {
-            Image(systemName: "clock.circle")
-            Label("Txt", systemImage: "person.fill")
-            Image("bowl-food-solid")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100)
-            Label {
-                Text("This is rice")
-            } icon: {
-                Image("bowl-food-solid")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 20)
+            Text("Various of Circle")
+            HStack {
+                Capsule()
+                    .fill(.yellow)
+                    .frame(width: 100, height: 150)
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 100)
+                Ellipse()
+                    .fill(.green)
+                    .frame(width: 100, height: 150)
+            }
+            HStack {
+                Ellipse()
+                    .fill(
+                        RadialGradient(gradient: Gradient(colors: [.pink, .brown]), center: .top, startRadius: 10, endRadius: 50)
+                    )
+                    .frame(width: 100, height: 150) 
+            }
+            .padding(.bottom, 30)
+            Text("Various of Rectangle")
+            HStack {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 100, height: 100)
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.purple)
+                    .frame(width: 100, height: 100)
             }
         }
     }
