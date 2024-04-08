84a5c270a3046d24688ab452fda64595a16ea0bb
rmfls0606 <rmfls0606@naver.com>
2024-04-08 15:15:25 +0900

    image Example

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,35 +11,14 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            let myString = "Displays aa string variable"
-//            Text("Hello world \(myString)")
-            Text("This is my age\(myString). Sice I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                .truncationMode(.middle)// 생략 부분 앞쪽 생략, 중간 생략, 마지막 생략 -> default .tail
-                .lineLimit(3)
-            
-            Text("Bold Text")
-                .font(.title)
-                .bold()
-            
-            Text("Italic")
-                .font(.title2)
-                .italic()
-            
-            Text("Underlined")
-                .font(.headline)
-                .underline()
-            
-            Text("Strikethrough")
-                .font(.subheadline)
-                .strikethrough()
-            
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypewriter", size: 24))
-            
-            Text("Line Spacing")
-                .font(.body)
-                .lineSpacing(10)
+            Label("Text", systemImage: "clock.circle").font(.title)
+            Label{
+                Text("No Modifiers")
+            } icon: {
+                Image(systemName: "clock.circle")
+                    .resizable()
+                    .frame(width: 20.0, height: 20.0)
+            }
         }
     }
 }
