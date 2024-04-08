a306199bb5e77c620f2969aedc6b490c8936bdcc
DopeKS <kyusang.dev@gmail.com>
2024-04-08 14:30:50 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,36 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            let myString = "Displays a string variable"
-            //Text("Hello World, \(myString)")
-            Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-                .multilineTextAlignment(.trailing) //여러 줄로 이루어진 텍스트의 정렬 방향을 설정.(leading, center, trailing)
-                .truncationMode(.tail) //텍스트 요소에서 텍스트가 요소의 공간을 벗어날 때의 처리 방식을 지정 (head, middle, tail, truncateHead, truncateMiddle, truncateTail)
-                .lineLimit(3) //텍스트 요소에서 표시할 최대 줄 수를 지정.(정수 값, nil)
             
-            Spacer()
-            Text("Bold Text") //굵은 글씨
-                .font(.title)
-                .bold()
-            
-            Text("Italic") //이텔리체
-                .font(.title2)
-                .italic()
-            
-            Text("Underlined") //밑줄
-                .font(.headline)
-                .underline()
-            
-            Text("Strikethrough") //취소선
-                .font(.subheadline)
-                .strikethrough()
-            
-            Text("Custom Font Text") //다른 폰트
-                .font(.custom("AmericanTypewriter", size: 24))
-            
-            Text("Line Spacing \n Second Line") //줄간격, 행간이라고 하껄
-                .font(.body)
-                .lineSpacing(10)
         }
     }
 }
