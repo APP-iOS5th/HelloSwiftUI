d2c0209f7cf2ea6044f2ef5c372c8a43353a4278
wotjs020708 <wotjs020708@gmail.com>
2024-04-08 17:22:16 +0900

    SegmentPickerStyle

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,38 +7,17 @@
 
 import SwiftUI
 
-extension Color {
-    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
-}
-
 struct ContentView: View {
-    @State var colorMe = false
+    @State private var selectedColor = Color.gray
     var body: some View {
-        VStack{
-            Rectangle()
-                .fill(colorMe ? Color.green : Color.gray)
-                .frame(width: 250, height: 100)
-            Button("요기를 클릭하세요"){
-                colorMe.toggle()
-            }
-            Button{
-                colorMe.toggle()
-            } label: {
-                Text("Click here")
-                    .font(.largeTitle)
-                    .foregroundStyle(Color.green)
-                    .padding()
-                    .border(Color.red, width: 6)
-            }
-            Button{
-                colorMe.toggle()
-            } label: {
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
-            }
+        VStack (spacing: 28){
+            Rectangle().fill(selectedColor)
+            Picker("Favorite Color", selection: $selectedColor, content: {
+                Text("Red").tag(Color.red)
+                Text("Green").tag(Color.green)
+                Text("Blue").tag(Color.blue)
+            })
+            .pickerStyle(SegmentedPickerStyle())
         }
     }
 }
