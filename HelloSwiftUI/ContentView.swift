8e3ff4dff780a62a32ec422ed3aadfb609cb5eef
SungWoon Lee <lsw.netline@gmail.com>
2024-04-08 16:18:49 +0900

    opacity, shadow

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,44 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack  {
+            //이미지 크기 조정, 크롭(자르기, 원형)
+//            Image("sample_dog_square")
+//                .resizable()
+//                .frame(width:250, height:250)
+//                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+//            Image("sample_dog_square")
+//                .resizable()
+//                .frame(width:250, height:250)
+//                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+//                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             
-            //            Capsule().fill(Color.yellow)
-            //            Circle().fill(Color.blue)
-            //            Ellipse().fill(Color.brown)
-            //            Rectangle().fill(Color.red)
-            //            RoundedRectangle(cornerRadius: 30).fill(Color.green)//cornerRadius는 모서리 곡률
+            // 이미지 그림자 효과
+//            Image("sample_dog_square")
+//                .resizable()
+//                .frame(width:250, height:250)
+//                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+//                .shadow(color: .red, radius: 10, x:0, y:0)
+//            Image("sample_dog_square")
+//                .resizable()
+//                .frame(width:250, height:250)
+//                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+//                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+//                .shadow(color: .green, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x:90, y:90)
+//            
             
-            //            Spacer()
-            //            Ellipse().fill(
-            //                RadialGradient (
-            //                    gradient: Gradient(colors:[.blue, .yellow]),
-            //                    center: .top,
-            //                    startRadius: 10,
-            //                endRadius: 65))
-            //            Spacer()
-            //            Ellipse().fill(
-            //                RadialGradient (
-            //                    gradient: Gradient(colors:[.blue, .yellow]),
-            //                    center: .top,
-            //                    startRadius: 10,
-            //                endRadius: 65))
-            //            Spacer()
-            //            Ellipse().fill(
-            //                RadialGradient (
-            //                    gradient: Gradient(colors:[.blue, .yellow]),
-            //                    center: .top,
-            //                    startRadius: 10,
-            //                endRadius: 65))
-            //            Spacer()
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width:250, height:250)
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
             
-            Image(systemName: "tortoise.fill")
-            Image(systemName: "tortoise.fill").font(.largeTitle)
-            Image(systemName: "tortoise.fill").font(.custom("", size: 50))
-            //내장 아이콘 가져오기.
-            
-            Image("clock-solid").resizable().aspectRatio(contentMode: .fill).frame(width: 150, height: 50) //fill은 그냥 넘어가도 되고, fit은 맞게 들어가게 하는 것.
-            // asset에 추가한 아이콘 사용하기.
-            
+            Image("sample_dog_square")
+                .resizable()
+                .frame(width:250, height:250)
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .shadow(color: .green, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x:90, y:90)
             
         }
     }
