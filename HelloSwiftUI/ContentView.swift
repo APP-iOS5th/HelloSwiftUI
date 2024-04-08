43877d8c42a33b224af552cb3dcfdd3f872c09bb
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-08 17:23:15 +0900

    Picker

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -5,34 +5,16 @@ extension Color {
 }
 
 struct ContentView: View {
-    @State var colorMe = false
+    @State private var  selectedColor = Color.gray
     var body: some View {
-        VStack {
-            Rectangle()
-                .fill(colorMe ? Color.green : Color.gray)
-                .frame(width: 250, height: 100)
-            
-            Button("여기를 클릭하세요.") {
-                colorMe.toggle()
-            }
-            Button {
-                colorMe.toggle()
-            } label: {
-                Text("Click here")
-                    .font(.largeTitle)
-                    .foregroundStyle(.green)
-                    .padding()
-                    .border(Color.red, width: 6)
-            }
-            Button {
-                colorMe.toggle()
-            } label: {
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
-            }
+        VStack (spacing: 30){
+            Rectangle().fill(selectedColor)
+            Picker("Favortie Color", selection: $selectedColor,content: {
+                Text("Red").tag(Color.red)
+                Text("Blue").tag(Color.blue)
+                Text("Green").tag(Color.green)
+            })
+            .pickerStyle(SegmentedPickerStyle())
         }
     }
 }
