7c0df2c94f660eafe84ab104f0391b5831f528ca
SungWoon Lee <lsw.netline@gmail.com>
2024-04-08 13:26:49 +0900

    spacing

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,18 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View { @State private var isOn = true
+    @State private var value = 0
+    @State private var sliderValue = 0.5
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        VStack (alignment: .trailing, spacing: 24) {
+            
+            Text("  Default padding of 16 points")
+                .padding()
+                .background(Color.yellow)
+            Text("  Default padding of 45 points")
+                .padding(45)
+                .background(Color.yellow)
+            Text("  Default padding of 3 points")
+                .padding(3)
+                .background(Color.yellow)
 
+            HStack (alignment: .bottom, spacing: 24) {
+                Text("Top padding only")
+                    .padding(.top)
+                    .background(Color.yellow)
+                Text("Top padding only")
+                    .padding(.trailing)
+                    .background(Color.yellow)
+                Text("Bottom padding only")
+                    .padding(.bottom)
+                    .background(Color.yellow)
+                Text("Top padding only")
+                    .padding([.top, .leading], 45)
+                .background(Color.yellow) }
+            
+            Text("Top, Leading padding")
+                .padding([.top, .leading], 45)
+                .background(Color.yellow)
+            Text("Top, Leading and bottom padding")
+                .padding([.top, .leading, .bottom], 45)
+                .background(Color.yellow)
+        }
+    }
+    }
 #Preview {
     ContentView()
 }
+
+/*
+ 
+ Swift 3.0 이전 프리뷰
+ struct ContentView)Previews: PreviewProvider {
+ static var previews: some View {
+ ContentView()
+ }
+ }
+ 
+ */
--- HelloSwiftUI/HelloSwiftUIApp.swift
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct HelloSwiftUIApp: App {
     var body: some Scene {
+
         WindowGroup {
             ContentView()
         }
