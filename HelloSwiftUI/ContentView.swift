daea01b1a4c9408082093c7f8e2cfafe8b777188
Jinx <jinxwift@gmail.com>
2024-04-08 17:05:26 +0900

    button viewbuilder

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -279,29 +279,58 @@ extension Color {
 struct ContentView: View {
     var body: some View {
         VStack {
-            
-            // asset color scheme 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color("custom_brown"))
-            
-            // 지역 상수 사용
-            let customColor = Color(red: 1, green: 0, blue: 0)
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(customColor)
-            
-            // 전역 상수 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color.myCustomColor)
-            
+            Button("여기를 클릭하세요") {
+                print("클릭 시 실행코드 1")
+            }
+            Button {
+                print("클릭 시 실행코드 2")
+            } label: {
+                Text("Click here")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
+            Button {
+                print("클릭 시 실행코드 3")
+            } label: {
+                Image("sample_dog_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
+            }
         }
     }
 }
+//
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            
+//            // asset color scheme 사용
+//            Image(systemName: "heart.fill")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .foregroundStyle(Color("custom_brown"))
+//            
+//            // 지역 상수 사용
+//            let customColor = Color(red: 1, green: 0, blue: 0)
+//            Image(systemName: "heart.fill")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .foregroundStyle(customColor)
+//            
+//            // 전역 상수 사용
+//            Image(systemName: "heart.fill")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .foregroundStyle(Color.myCustomColor)
+//            
+//        }
+//    }
+//}
+
 
 #Preview {
     ContentView()
