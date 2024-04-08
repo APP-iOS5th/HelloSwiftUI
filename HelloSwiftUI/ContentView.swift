e0b7500aef310b2ba586c8f6d93c1a235da19e39
Min-Gyeong Hwang <freemin217@gmail.com>
2024-04-08 16:35:53 +0900

    Color 사용 방법

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,58 +7,32 @@
 
 import SwiftUI
 
+extension Color{
+    static let myCustomColor = Color(red:0.5, green: 0.8, blue: 0.3)
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-//            이미지 모양 변경 (사각형, 원형)
-//            Image("toby")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//            Image("toby2")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .clipShape(Circle())
-            
-//            이미지 shadow 효과
-//            Image("toby")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .shadow(color: .red, radius: 46, x: 0, y: 0)
-//            Image("toby2")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .clipShape(Circle())
-//                .shadow(color: .green, radius: 46, x: 90, y: 50)
-//            이미지 shadow 및 overlay 효과
-//            Image("toby")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .shadow(color: .red, radius: 46, x: 0, y: 0)
-//                .overlay(Rectangle().stroke(Color.blue, lineWidth:10))
-//            Image("toby2")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .clipShape(Circle())
-//                .shadow(color: .green, radius: 46, x: 90, y: 50)
-//                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
-            
-//          이미지 투명도 opacity 조절
-            Image("toby")
+            //Asset Color scheme 사용
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .opacity(0.5)
-            Image("toby2")
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color("custom_brown"))
+            
+            //지역 상수 사용
+            let customColor = Color(red: 1, green: 0, blue: 0)
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .opacity(0.25)
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color(customColor))
+            // 전역 상수 사용
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color.myCustomColor)
+            
+            
         }
     }
 }
