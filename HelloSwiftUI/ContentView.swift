66abd4e75b01fb41cbaad4134227344ead41b0a0
JongWon Jeong <jjwon214@gmail.com>
2024-04-08 13:14:52 +0900

    Padding

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,34 +14,40 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Slider(value: $sliderValue, in: 0...1)
-                .tint(.brown)
+            
+            Text("Default padding of 16 points")
+                .padding()
+                .background(.yellow)
+            
+            Text("Default padding of 45 points")
+                .padding(45)
+                .background(.yellow)
+            
+            Text("Default padding of 3 points")
+                .padding(3)
+                .background(.yellow)
+            
+            
+            Text("Top padding only")
+                .padding(.top)
+                .background(.yellow)
+            
+            Text("trailing padding only")
+                .padding(.trailing)
+                .background(.yellow)
+            
+            Text("bottom padding only")
+                .padding(.bottom)
+                .background(.yellow)
+            
+            Text("top, leading padding ")
+                .padding([.top, .leading], 45)
+                .background(.yellow)
+            
+            Text("top, leading and bottom padding ")
+                .padding([.top, .leading, .bottom], 45)
+                .background(.yellow)
                 
-            
-            Stepper("값 \(value)", value: $value, in: 0...10)
-            
-            Toggle("Toggle message on/off", isOn: $isOn)
-            Text("위")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-            
-            HStack {
-                Text("왼쪽")
-                Text("오른쪽")
-            }
-            .background(Color.purple)
-            ZStack {
-                Text("배경")
-                Text("전경")
-            }
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                print("button1 click")
-            }
-            Button(action: {
-                print("button2 click")
-            }, label: {
-                Text("Button")
-            })
         }
     }
 }
