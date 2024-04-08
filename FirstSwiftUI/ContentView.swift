4c0efff6594577cf8b87a5ff9b22af6d520eb348
avan31017 <avan31017@gmail.com>
2024-04-08 13:22:17 +0900

    padding

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -15,7 +15,33 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            
+            Text("Default padding of 16 points")
+                .padding()
+                .background(.yellow)
+            Text("Default padding of 45 points")
+                .padding(45)
+                .background(.yellow)
+            Text("Default padding of 3 points")
+                .padding(3)
+                .background(.yellow)
+            Text("Top padding only")
+                .padding(.top)
+                .background(.yellow)
+            Text("Trailing padding only")
+                .padding(.trailing)
+                .background(.yellow)
+            Text("Leading padding only")
+                .padding(.leading)
+                .background(.yellow)
+            Text("Bottom padding only")
+                .padding(.bottom)
+                .background(.yellow)
+            Text("Bottom padding only")
+                .padding([.top, .leading], 45)
+                .background(.yellow)
+            Text("Bottom padding only")
+                .padding([.top, .leading, .bottom], 45)
+                .background(.yellow)
         }
     }
 }
