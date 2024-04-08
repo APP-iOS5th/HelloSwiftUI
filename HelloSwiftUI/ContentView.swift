7894b8b570a8b407345014538852efa3858e9826
wotjs020708 <wotjs020708@gmail.com>
2024-04-08 10:04:17 +0900

    #Privew 설명 추가

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -22,3 +22,12 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+/*
+Swift 3.0이전 프리뷰
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+            ContentView()
+        }
+}
+ */
