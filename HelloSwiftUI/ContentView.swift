f578a3dcec39cdbf0fa2e090a08a1891bfcfc979
YACHAE <104hsh76@gmail.com>
2024-04-08 09:25:53 +0900

    Initial Commit

---
+++ HelloSwiftUI/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by Yachae on 4/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
