9a4ffa0182f2df7f723a1db4e5554d0d0403aa85
Hyunseo Yook <hhh3332@naver.com>
2024-04-08 15:00:43 +0900

    SystemImage with SF Symbols

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,36 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            let myString = "Display a string variable"
-            Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-                .multilineTextAlignment(.trailing)
-                .truncationMode(/*@START_MENU_TOKEN@*/.tail/*@END_MENU_TOKEN@*/)
-                .lineLimit(3)
-            
-            Spacer()
-            
-            Text("Bold Text")
-                .font(.title)
-                .bold()
-            
-            Text("Italic")
-                .font(.title2)
-                .italic()
-            
-            Text("UnderLined")
-                .font(.headline)
-                .underline()
-            
-            Text("Strikethrough")
-                .font(.subheadline)
-                .strikethrough()
-            
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypewriter", size: 24))
-                
-            Text("Line Spacing\nSecond Line")
-                .font(.body)
-                .lineSpacing(20)
+            Label("Text", systemImage: "clock.circle").font(.title)
      
         }
     }
