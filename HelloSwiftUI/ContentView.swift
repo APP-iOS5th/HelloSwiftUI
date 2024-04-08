c76b361d32c01da11d4ccc1930175a8164c1af86
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 16:38:35 +0900

    Color

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,67 +7,32 @@
 
 import SwiftUI
 
+extension Color {
+    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.6)
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            /*
-             //이미지 크기 조정, 크롬(자르기, 원형)
-            Image("sample_dog_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
             
-            Image("sample_cat_square")
+            //Asset Color scheme 사용
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            */
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color("custom_brown"))
             
-            /*
-            //이미지 그림자 효과
-            Image("sample_dog_square")
+            //지역변수 사용
+            let customColor = Color(red: 1, green: 0, blue: 0)
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .shadow(color: .red, radius: 46, x: 0, y: 0)
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(customColor)
             
-            Image("sample_cat_square")
+            //전역변수 사용
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                .shadow(color:.green, radius: 46, x: 90, y: 50)
-            */
-            /*
-            //이미지 테두리 오버레이
-            Image("sample_dog_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .shadow(color: .red, radius: 46, x: 0, y: 0)
-                .overlay(Rectangle().stroke(Color.blue, lineWidth: 10))
-            
-            Image("sample_cat_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                .shadow(color:.green, radius: 46, x: 90, y: 50)
-                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
-             */
-            //이미지 투명도 조절
-            Image("sample_dog_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .opacity(0.5)
-            
-            Image("sample_cat_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .opacity(0.25)
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color.myCustomColor)
         }
     }
 }
