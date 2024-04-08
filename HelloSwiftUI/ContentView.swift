f7c170d0952ce05b915817eedc209ca8ce2cf6d4
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-08 14:29:52 +0900

    text style

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,25 +14,36 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            ZStack {
-                Text("Top")
-                    .font(.system(size: 40))
-                    .background(Color.yellow)
-                Text("Top")
-                    .font(.system(size: 40))
-                    .background(Color.yellow)
-                    .position(x: 255, y: 127)
-                Text("Top")
-                    .font(.system(size: 40))
-                    .background(Color.yellow)
-                    .offset(x: 74, y: 125)
-            }
-            Text("First")
-                .font(.system(size: 40))
-                .background(Color.yellow)
-            Text("Second")
-                .font(.system(size: 40))
-                .background(Color.yellow)
+            let myString = "Displays a string variable"
+                        Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+                            .multilineTextAlignment(.trailing)
+                            .truncationMode(.tail)
+                            .lineLimit(3)
+            
+            Spacer()
+            
+            Text("Bold Text")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+            
+            Text("Italic")
+                .font(.title2)
+                .italic()
+            
+            Text("Underlined")
+                .font(.headline)
+                .underline()
+                
+            Text("Strikethrough")
+                .font(.subheadline)
+                .strikethrough()
+            
+            Text("Custom Font Text")
+                .font(.custom("AmericanTypewriter", size: 24))
+            
+            Text("Line Spacing\nSecond Line")
+                .font(.body)
+                .lineSpacing(20)
         }
     }
 }
