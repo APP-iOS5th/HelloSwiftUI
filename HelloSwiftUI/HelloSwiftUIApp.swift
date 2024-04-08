5dabfcbd6db4bf1b4b7b7c7d9ad658fad5a266c0
Jihye Park <deviloper.park@gmail.com>
2024-04-08 09:26:07 +0900

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by 박지혜 on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
