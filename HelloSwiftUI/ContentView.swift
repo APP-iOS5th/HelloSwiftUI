21663532d449ef137e17f359ac5bc6feac9f6cab
Jin Park <jinpark1504@gmail.com>
2024-04-08 17:14:35 +0900

    button

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,30 +7,36 @@
 
 import SwiftUI
 
-extension Color {
-    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
-}
 struct ContentView: View {
+    @State var colorMe = false
     var body: some View {
         VStack {
-            // Asset Color scheme 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color("custom_brown"))
-
-            // 지역상수 사용
-            let customColor = Color(red: 1, green: 0, blue: 0)
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(customColor)
-
-            // 전역상수 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color.myCustomColor)
+            Rectangle()
+                .fill(colorMe ? Color.green : Color.gray)
+                .frame(width: 250, height: 100)
+            Button("여기를 클릭하세요") {
+                colorMe.toggle()
+            }
+            
+            Button {
+                colorMe.toggle()
+            } label: {
+                Text("Click here")
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
+            
+            Button {
+                colorMe.toggle()
+            } label: {
+                Image("sample_dog_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(.blue, lineWidth: 3))
+            }
         }
     }
 }
