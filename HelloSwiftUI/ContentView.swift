f37dc372459c5add7ddf8f4a3f318e22da7b3923
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 17:06:07 +0900

    button custom

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,7 +11,27 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-           
+            Button("여기를 클릭하세요") {
+                print("클릭시 실행코드 1")
+            }
+            Button {
+                print("클릭시 실행코드 2")
+            } label: {
+                Text("Click here")
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
+            Button {
+                print("클릭시 실행코드 3")
+            } label: {
+                Image("sample_cat_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 5))
+            }
         }
     }
 }
