6ec2a88c9ad3e798621523c33491ae45629ad1d5
DopeKS <kyusang.dev@gmail.com>
2024-04-08 10:18:59 +0900

    Add Button

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,14 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ///Vertical Stack (default: center)
+        ///수직 스택으로, 자식 뷰들을 수직(위에서 아래로) 방향으로 정렬.
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                
+            Text("위")
+            ///Horizontal Stack
+            ///수평 스택으로, 자식 뷰들을 수평(왼쪽에서 오른쪽으로) 방향으로 정렬.
+            HStack {
+                Text("왼쪽")
+                Text("오른쪽")
+            }
+            ///Z-Axis Stack
+            /*깊이 스택으로, 자식 뷰들을 Z축(앞에서 뒤로) 방향으로 쌓음.
+             뷰들을 서로 위에 놓고 싶을 때 사용.
+             예를 들어, 배경 위에 텍스트를 놓고 싶을 때 사용할 수 있음. */
+            ZStack {
+                Text("배경")
+                Text("전경")
+            }
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                print("button1 click")
+            }
+            Button(action: {
+                print("button2 click")
+            }, label: {
+                Text("Button")
+            })
         }
-        .padding()
     }
 }
 
