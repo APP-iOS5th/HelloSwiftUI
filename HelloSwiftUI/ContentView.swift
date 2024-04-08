e6fb6edc125a98464f4759e5c70c2ae15b2c5c36
Ruel <c0407h@gmail.com>
2024-04-08 22:53:28 +0900

    SegmentPickerStyle

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,33 +12,21 @@ extension Color {
 }
 
 struct ContentView: View {
-    @State private var isOn = true
-    @State private var value = 0
-    @State private var sliderValue = 0.0
+    @State private var selectedColor = Color.gray
     
     var body: some View {
-        VStack() {
-            Button("여기를 클릭하세요") {
-                print("클릭시 실행코드 1")
+        VStack(spacing: 28) {
+            Rectangle()
+                .fill(selectedColor)
+            Picker("Favorite Color" ,selection: $selectedColor) {
+                Text("Red")
+                    .tag(Color.red)
+                Text("Green")
+                    .tag(Color.green)
+                Text("Blue")
+                    .tag(Color.blue)
             }
-            Button {
-                print("클릭시 실행코드 2")
-            } label: {
-                Text("Click here")
-                    .font(.largeTitle)
-                    .foregroundColor(.green)
-                    .padding()
-                    .border(Color.red, width: 6)
-            }
-            Button {
-                print("클릭시 실행코드 3")
-            } label: {
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
-            }
+            .pickerStyle(SegmentedPickerStyle())
         }
     }
 }
