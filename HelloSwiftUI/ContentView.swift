be3a2119a4e157b6b12830a869c45896038ed9c6
DopeKS <kyusang.dev@gmail.com>
2024-04-08 17:19:30 +0900

    Color, custom color

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,98 +8,31 @@
 
 import SwiftUI
 
+extension Color {
+    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            /// 이미지 크기 조정, 크롭(자르기, 원형)
-            //            Image("sample_dog_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //
-            //            Image("sample_cat_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //                .clipShape(Circle())
+            /// Asset Color scheme 사용
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color("custom_brown"))
             
-            /// 이미지 그림자 효과
-            //            Image("sample_dog_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //                .shadow(color: .red, radius: 46, x: 0, y: 0)
-            //
-            //            Image("sample_cat_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //                .clipShape(Circle())
-            //                .shadow(color: .green, radius: 46, x: 90, y: 50)
-            //        }
+            /// 지역상수 사용
+            let customColor = Color(red: 1, green: 0, blue: 0)
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(customColor)
             
-            /// 이미지 테두리 오버레이
-            //            Image("sample_dog_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //                .shadow(color: .red, radius: 46, x: 0, y: 0)
-            //                .overlay(Rectangle().stroke(Color.blue, lineWidth: 10))
-            //
-            //            Image("sample_cat_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //                .clipShape(Circle())
-            //                .shadow(color: .green, radius: 46, x: 90, y: 50)
-            //                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
-            
-            /// 이미지 투명도 조절
-            //            Image("sample_dog_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //                .opacity(0.5)
-            //
-            //            Image("sample_cat_square")
-            //                .resizable()
-            //                .frame(width: 250, height: 250)
-            //                .aspectRatio(contentMode: .fill)
-            //                .opacity(0.25)
-            
-            HStack(alignment: .center) {
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .aspectRatio(contentMode: .fill)
-                    .clipShape(Circle())
-                    .padding(.trailing, 15)
-                    .shadow(color: .red, radius: 46, x: 0, y: 0)
-                    .overlay(Circle().stroke(Color.purple, lineWidth: 10))
-                    .opacity(0.5)
-                
-                
-                Image("sample_cat_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .aspectRatio(contentMode: .fill)
-                    .shadow(color: .green, radius: 46, x: 90, y: 50)
-                    .overlay(Rectangle().stroke(Color.blue, lineWidth: 10))
-                    .opacity(0.3)
-            }
-            
-            VStack {
-                Text("Mandoo, Tuna")
-                    .font(.largeTitle)
-                    .bold()
-                    .italic()
-                    .padding(.top, 25)
-                
-                Text("iOS World")
-                    .font(.largeTitle)
-                    .bold()
-                    .italic()
-            }.opacity(0.7).shadow(color: .blue, radius: 46, x: 0, y: 0)
+            /// 전역상수 사용
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color.myCustomColor)
         }
     }
 }
