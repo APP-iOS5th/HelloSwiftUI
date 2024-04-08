0b830b6b1e8d2b9d0047422edf8d5b871eb6cbed
rmfls0606 <rmfls0606@naver.com>
2024-04-08 14:08:02 +0900

    offset Example

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,52 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isOn = true
-    @State private var value = 0
-    @State private var sliderValue = 0.5
     
     var body: some View {
-        VStack(alignment: . trailing, spacing: 24) {
-           Text("Default padding of 16 points")
-                .padding()
-                .background(.yellow)
-           
-            Text("Default padding of 45 points")
-                 .padding(45)
-                 .background(.yellow)
-            
-            Text("Default padding of 3 points")
-                 .padding(3)
-                 .background(.yellow)
-            Spacer()
-            HStack(alignment: .top, spacing: 24){
-                Text("Top padding only")
-                    .padding(.top)
+        VStack{
+            ZStack{
+                Text("Top")
+                    .font(.system(size: 40))
                     .background(.yellow)
                 
-                Text("Trailing padding only")
-                    .padding(.trailing)
+                Text("Top")
+                    .font(.system(size: 40))
                     .background(.yellow)
-                
-                Text("Bottom padding only")
-                    .padding(.bottom)
-                    .background(.yellow)
+                    .offset(x: 74, y: 125)
             }
-            Spacer(minLength: 100)
-            Text("Leading padding only")
-                .padding(.leading, 45)
-                 .background(.yellow)
-            
-            Spacer()
-            Spacer()
-            Text("Top Leading padding only")
-                .padding([.top, .leading], 45)
-                 .background(.yellow)
-            
-            Text("Top Leading and Bottom padding only")
-                .padding([.top, .leading, .bottom], 45)
-                 .background(.yellow)
-        }//: VSTACK
+        }
     }
 }
 
