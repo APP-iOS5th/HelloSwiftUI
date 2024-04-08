8ae4995dfff062a42395c7202e3469be8e8c4a38
seonggeun an <eventscripts@nate.com>
2024-04-08 14:00:33 +0900

    Offset

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,46 +10,22 @@ import SwiftUI
 struct ContentView: View {
    
     var body: some View {
-        VStack (alignment: .trailing, spacing: 24){
-             
-            Text("Default padding of 16 points")
-                .padding()
+        VStack{
+            Text("Top")
+                .font(.system(size: 40))
                 .background(Color.yellow)
-            Text("Default padding of 45 points")
-                .padding(45)
+            Text("Top")
+                .font(.system(size: 40))
                 .background(Color.yellow)
-            Text("Default padding of 3 points")
-                .padding(3)
+                .offset(x: 74, y: 125)
+            Text("Top")
+                .font(.system(size: 40))
                 .background(Color.yellow)
-            
-            //뷰의 남은공간을 다 띄운다?
-            Spacer()
-            
-            HStack (alignment: .bottom, spacing: 24){
-                Text("Top padding only!")
-                    .padding(.top) //위쪽 여백
-                    .background(Color.yellow)
-                Text("trailing padding only!")
-                    .padding(.trailing) //오른쪽 여백
-                    .background(Color.yellow)
-                Text("Bottom padding only!")
-                    .padding(.bottom) //아래쪽 여백
-                    .background(Color.yellow)
-            }
-            Spacer(minLength: 100)
-                Text("Leading padding only!")
-                    .padding(.leading, 45) //왼쪽 여백
-                    .background(Color.yellow)
-            Spacer()
-            Spacer()
-                Text("Top, Leading padding")
-                    .padding([.top, .leading], 45) //위쪽, 왼쪽 여백
-                    .background(Color.yellow)
-                Text("Top, Leading and Bottom")
-                    .padding([.top, .leading, .bottom], 45) //위쪽,왼쪽,아래쪽 여백
-                    .background(Color.yellow)
-            }
+                .offset(x: 74, y: 125)
         }
+        
+       
+    }
 }
 
 #Preview {
