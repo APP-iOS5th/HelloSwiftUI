70b53bf55d51125e13c911e4634764f5250a432f
Hyungjun Kim <hjhj919@naver.com>
2024-04-08 16:45:36 +0900

    color

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,37 +7,27 @@
 
 import SwiftUI
 
+extension Color {
+    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
+}
+
+
 struct ContentView: View {
     var body: some View{
         VStack {
-            Image("sample_dog_square")
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 200, height: 200)
-                .aspectRatio(contentMode: .fill)
-                .shadow( color: .yellow, radius: 30)
-                .overlay(Rectangle().stroke(Color.pink, lineWidth: 5))
-            Image("IMG_1608")
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color("custom_e"))
+            let customColor = Color(red:1, green: 0.2, blue: 0.1)
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 160, height: 200)
-                .aspectRatio(contentMode: .fill)
-                .clipShape(Circle())
-                .shadow(color: .red, radius: 46, x: 0, y: 0)
-                .overlay(Circle().stroke(Color.brown, lineWidth: 4))
-            Image("sample_dog_square")
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(customColor)
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 100, height: 100)
-                .aspectRatio(contentMode: .fill)
-                .shadow( color: .yellow, radius: 30)
-                .overlay(Rectangle().stroke(Color.pink, lineWidth: 5))
-                .opacity(0.4)
-            Image("IMG_1608")
-                .resizable()
-                .frame(width: 180, height: 210)
-                .aspectRatio(contentMode: .fill)
-                .clipShape(Circle())
-                .shadow(color: .red, radius: 46, x: 0, y: 0)
-                .overlay(Circle().stroke(Color.brown, lineWidth: 4))
-                .opacity(0.5)
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(Color(Color.myCustomColor))
         }
     }
 }
