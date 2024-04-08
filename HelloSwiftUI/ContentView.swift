87d7e62b2a7fe698b54a05fd2feb631df45984c1
YACHAE <104hsh76@gmail.com>
2024-04-08 14:06:26 +0900

    Postion with VStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,39 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isOn = true
-    @State private var value = 0
-    @State private var sliderValue = 0.5
     var body: some View {
-        VStack {
-            Slider(value: $sliderValue, in: 0...1)
-                .accentColor(.red)
-            Stepper("값 \(value),", value: $value, in: 0...10)
-            Toggle("Toggle message on/off", isOn: $isOn)
-            Text("위")
-                .background(Color.yellow)
-                .padding()
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            HStack {
-                Text("왼쪽")
-                Text("오른쪽")
-            }
-            ZStack {
-                Text("         배경")
-                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
-                Text("전경         ")
-                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
-            }
-            Button("버튼1") {
-                print("button1 click")
-            }
-            Button(action: { print("button2 click" )}, label: {
-                    Text("버튼2")
-            })
-                   
+        VStack (alignment: .trailing, spacing: 24) {
+//            ZStack {
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.yellow)
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.yellow)
+//                    .position(x: 255, y: 127)
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.yellow)
+//                    .offset(x: 74, y: 125)
+//            }
+            Text ("Frist")
+                .font(.system(size: 40))
+                .background(.yellow)
+            Text ("Second View")
+                .font(.system(size: 40))
+                .background(.yellow)
         }
-        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.gray/*@END_MENU_TOKEN@*/)
+        // VStack offset 설정
+        .offset(x: 25, y: 125)
     }
 }
 
