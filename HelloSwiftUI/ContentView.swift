c4485d874ccad9576611d40033fcdd64807de057
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-08 13:24:58 +0900

    alignment

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack (alignment: .trailing, spacing: 24){
             Text("Default padding of 16 points")
                 .padding()
                 .background(Color.yellow)
@@ -12,24 +12,28 @@ struct ContentView: View {
             Text("Default padding of 3 points")
                 .padding(3)
                 .background(Color.yellow)
-            Text("Top padding only")
-                .padding(.top)
-                .background(Color.yellow)
-            Text("Trailing padding only")
-                .padding(.trailing)
-                .background(Color.yellow)
-            Text("Bottom padding only")
-                .padding(.bottom)
-                .background(Color.yellow)
-            Text("Leading padding only")
-                .padding(.leading, 45)
-                .background(Color.yellow)
-            Text("Top, Leading padding")
-                .padding([.top, .leading], 45)
-                .background(Color.yellow)
-            Text("Top, Leading and Bottom")
-                .padding([.top,.leading, .bottom], 45)
-                .background(Color.yellow)
+            
+            HStack (alignment: .top){
+                Text("Top padding only")
+                    .padding(.top)
+                    .background(Color.yellow)
+                Text("Trailing padding only")
+                    .padding(.trailing)
+                    .background(Color.yellow)
+                Text("Bottom padding only")
+                    .padding(.bottom)
+                    .background(Color.yellow)
+            }
+            
+                Text("Leading padding only")
+                    .padding(.leading, 45)
+                    .background(Color.yellow)
+                Text("Top, Leading padding")
+                    .padding([.top, .leading], 45)
+                    .background(Color.yellow)
+                Text("Top, Leading and Bottom")
+                    .padding([.top,.leading, .bottom], 45)
+                    .background(Color.yellow)
         }
     }
 }
@@ -41,8 +45,8 @@ struct ContentView: View {
 /*
  Swift 3.0 이전 프리뷰
  struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+ static var previews: some View {
+ ContentView()
+ }
  }
  */
