a9c771d7ef0cbbed28cf7ac44335a010a28cb29a
wotjs020708 <wotjs020708@gmail.com>
2024-04-08 10:19:29 +0900

    Stack & Button

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("위")
+            HStack {
+                Text("왼쪽")
+                Text("오른쪽")
+            }
+            ZStack {
+                Text("  배경")
+                Text("전경  ")
+            }
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                print("button1 click")
+            }
+            Button(action:  {
+                print("button2 click")
+            }, label: {
+                Text("Button")
+            })
         }
-        .padding()
     }
 }
 
@@ -24,10 +36,10 @@ struct ContentView: View {
 }
 
 /*
-Swift 3.0이전 프리뷰
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-            ContentView()
-        }
-}
+ Swift 3.0이전 프리뷰
+ struct ContentView_Previews: PreviewProvider {
+ static var previews: some View {
+ ContentView()
+ }
+ }
  */
