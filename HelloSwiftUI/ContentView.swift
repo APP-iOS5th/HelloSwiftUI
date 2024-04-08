fc294b88898325c9faf9a2d7fe5413287264456e
Hyunseo Yook <hhh3332@naver.com>
2024-04-08 16:35:14 +0900

    Color

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,51 +7,33 @@
 
 import SwiftUI
 
+extension Color {
+    static let myCustomColor = Color(red: 0.6, green: 0.9, blue: 0.2)
+}
+
 struct ContentView: View {
     var body: some View {
         VStack{
-//            Image("toby2")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-//            
-//            Image("toby")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-//                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-
+            // Asset Color scheme 사용
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color("custom_pink"))
             
-// 이미지 테두리 오버레이
+            // 지역상수 사용
+            let customColor = Color(red: 1, green: 1, blue: 0.1)
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color(customColor))
             
-//            Image("toby2")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-//                .shadow(color: .yellow, radius: 46 )
-//                .overlay(Rectangle().stroke(Color.brown, lineWidth: 10))
-//            
-//            Image("toby")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-//                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-//                .shadow(color: .blue, radius: 46, x: 90, y: 50 )
-//                .overlay(Circle().stroke(Color.cyan, lineWidth: 20))
+            // 전역상수 사용
+            Image(systemName: "heart.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color.myCustomColor)
             
             
-// 이미지 투명도
-                        Image("toby2")
-                            .resizable()
-                            .frame(width: 250, height: 250)
-                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                            .opacity(0.5)
-            
-                        Image("toby")
-                            .resizable()
-                            .frame(width: 250, height: 250)
-                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                            .opacity(0.25)
                             
         }
     }
