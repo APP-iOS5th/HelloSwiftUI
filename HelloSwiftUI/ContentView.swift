ac8ad127d21f605364052865a2882004969cdb1f
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-08 13:41:20 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,47 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .trailing, spacing: 24) { //spacing: 간격
-            
-            Text("Default padding of 16 point")
-                .padding()
-                .background(Color.yellow)
-            
-            Text("Default padding of 45 point")
-                .padding(45)
-                .background(Color.yellow)
-            
-            Text("Default padding of 3 point")
-                .padding(3)
-                .background(Color.yellow)
-            Spacer()
-            HStack(alignment: .top, spacing: 24){
-                Text("Top padding only")
-                    .padding(.top)
-                    .background(Color.yellow)
-                Text("Trailing padding only")
-                    .padding(.trailing)
-                    .background(Color.yellow)
-                Text("Bottom padding only")
-                    .padding(.bottom)
-                    .background(Color.yellow)
-            }
-            Spacer()
-            Text("Leading padding only")
-                .padding(.leading, 45)
-                .background(Color.yellow)
-            Spacer()
-            Spacer()
-            Text("Top, Leading padding")
-                .padding([.top, .leading], 45)
-                .background(Color.yellow)
-            
-            Text("Topm, Leading and Bottom padding")
-                .padding([.top, .leading, .bottom], 45)
-                .background(Color.yellow)
-            
-            
-            
+        VStack { //spacing: 간격
             
         }
     }
