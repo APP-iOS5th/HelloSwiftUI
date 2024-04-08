3e9255e2d3ca442e1d879d000a3dfe92d0366015
avan31017 <avan31017@gmail.com>
2024-04-08 15:38:54 +0900

    Shape & Image example

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -28,7 +28,21 @@ struct ContentView: View {
                     .fill(
                         RadialGradient(gradient: Gradient(colors: [.pink, .brown]), center: .top, startRadius: 10, endRadius: 50)
                     )
-                    .frame(width: 100, height: 150) 
+                    .frame(width: 100, height: 150)
+                Ellipse().fill(
+                    RadialGradient(
+                        gradient: Gradient(colors:[.blue,.yellow]),
+                        center: .center,
+                        startRadius: 10,
+                        endRadius: 65
+                    ))
+                .frame(width: 100, height: 150)
+                Ellipse().fill(
+                    AngularGradient(
+                        gradient: Gradient(colors:[.blue,.yellow]),
+                        center: .center
+                    ))
+                .frame(width: 100, height: 150)
             }
             .padding(.bottom, 30)
             Text("Various of Rectangle")
@@ -39,6 +53,17 @@ struct ContentView: View {
                 RoundedRectangle(cornerRadius: 10)
                     .fill(.purple)
                     .frame(width: 100, height: 100)
+            }
+            .padding(.bottom, 30)
+            Text("Image example")
+            HStack {
+                Image(systemName: "tortoise.fill")
+                Image(systemName: "tortoise.fill").font(.largeTitle)
+                Image(systemName: "tortoise.fill").font(.custom("", size: 50))
+                Image("bowl-food-solid")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 50)
             }
         }
     }
