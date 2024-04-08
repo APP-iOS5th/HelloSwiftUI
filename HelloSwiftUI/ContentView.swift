c45c9c5aba6d07d53ff3cf6ba85df4b0ea6d778c
Jihye Park <deviloper.park@gmail.com>
2024-04-08 14:12:16 +0900

    Position with VStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,42 +14,28 @@ struct ContentView: View {
     
     var body: some View {
         VStack (alignment: .leading, spacing: 24) {
-            Text("Default padding of 16 points")
-                .padding()
-                .background(Color.yellow)
-            Text("Default padding of 45 points")
-                .padding(45)
-                .background(Color.yellow)
-            Text("Default padding of 3 points")
-                .padding(3)
-                .background(Color.yellow)
-            
-            Spacer()
-            
-            HStack (alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 30){
-                Text("Top padding only")
-                    .padding(.top)
-                    .background(Color.yellow)
-                Text("Trailing padding only")
-                    .padding(.trailing)
-                    .background(Color.yellow)
-                Text("Bottom padding only")
-                    .padding(.bottom)
-                    .background(Color.yellow)
-            }
-            
-            Spacer(minLength: 100)
-            
-            Text("Leading padding only")
-                .padding(.leading, 45)
-                .background(Color.yellow)
-            Text("Top, Leading padding")
-                .padding([.top, .leading], 45)
-                .background(Color.yellow)
-            Text("Top, Leading and Bottom")
-                .padding([.top, .leading, .bottom], 45)
-                .background(Color.yellow)
+//            ZStack {
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.red)
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.yellow)
+//                    .position(x: 255, y: 127)
+//                Text("Top")
+//                    .font(.system(size: 40))
+//                    .background(Color.green)
+//                    .offset(x: 74, y: 127)
+//            }
+            Text("First")
+                .font(.system(size: 40))
+                .background(Color.blue)
+            Text("Second View")
+                .font(.system(size: 40))
+                .background(Color.blue)
         }
+        // VStack offset 설정
+        .offset(x: 25, y:125)
     }
 }
 
