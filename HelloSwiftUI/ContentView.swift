731fb1eea36e9442e44d710dc6e2f6ab889a2012
Jin Park <jinpark1504@gmail.com>
2024-04-08 14:31:23 +0900

    text spacing, alignment and font styles

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,39 +14,33 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            ZStack {
-                Text("Top")
-                    .font(.system(size: 40))
-                    .background(Color.yellow)
-                Text("offset")
-                    .font(.system(size: 40))
-                    .background(Color.yellow)
-                    .offset(x: 45, y: 125)
-                Text("position")
-                    .font(.system(size: 40))
-                    .background(Color.yellow)
-                    .position(x: 45, y: 125)
-                Text("First")
-                    .font(.system(size: 40))
-                    .background(Color.yellow)
-                Text("Second")
-                    .font(.system(size: 40))
-                    .background(Color.yellow)            }
+            let myString = "Displays a string variable"
+            Text("This is my age \(myString). \nSince I am retired, \nI am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+//                .multilineTextAlignment(.trailing)
+                .truncationMode(.tail)
+                .lineLimit(3)
+            Text("Bold Text")
+                .font(.title)
+                .bold()
+            
+            Text("Italic")
+                .font(.title2)
+                .italic()
+            Text("Underline")
+                .font(.headline)
+                .underline()
+            Text("Strikethrough")
+                .font(.subheadline)
+                .strikethrough()
+            Text("Custom Font Text")
+                .font(.custom("AmericanTypewriter", size: 24))
+            Text("Line Spacing\nSecond Line")
+                .font(.body)
+                .lineSpacing(20)
         }
-        .offset(x: 30, y: 100)
-//        .position(x: 30, y: 100)
     }
 }
 
 #Preview {
     ContentView()
 }
-
-/*
-// Swift 3.0 이전 프리뷰
-struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-        ContentView()
-    }
-}
-*/
