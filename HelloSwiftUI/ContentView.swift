bb7751e952e3c3d89ecc3cbfbf52fb51c743c419
kyhlsd <kmyghn@gmail.com>
2024-04-08 17:04:44 +0900

    button

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,44 +7,30 @@
 
 import SwiftUI
 
-extension Color {
-    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
-}
-
 struct ContentView: View {
     var body: some View {
         VStack{
-            Image("sample_dog_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                .shadow(color: .red, radius: 46, x: 0, y: 0)
-                .overlay(Rectangle().stroke(Color.blue, lineWidth: 10))
-                .opacity(0.5)
-            
-            Image("sample_dog_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                .clipShape(Circle())
-                .shadow(color: .green, radius: 46, x:90, y: 50)
-                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
-                .opacity(0.25)
-            
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color("custom_brown"))
-            
-            let customColor = Color(red: 1, green: 0, blue: 0)
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(customColor)
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color.myCustomColor)
+            Button("Click Button"){
+                print("button1 click")
+            }
+            Button {
+                print("button2 click")
+            } label: {
+                Text("Click here")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
+            Button {
+                print("button3 click")
+            } label: {
+                Image("sample_dog_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
+            }
         }
     }
 }
