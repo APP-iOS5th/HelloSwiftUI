0b00e093bab7ba560a4449bc5dff347017ead9ee
Jinx <jinxwift@gmail.com>
2024-04-08 15:00:49 +0900

    system font image 써보기

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -94,42 +94,56 @@
 //    }
 //}
 
+//import SwiftUI
+//
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            let myString = "Displays a string variable"
+//            Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+//                .multilineTextAlignment(.trailing)
+//                .truncationMode(.tail)
+//                .lineLimit(3)
+//            
+//            Spacer()
+//            
+//            Text("Bold Text")
+//                .font(.title)
+//                .bold()
+//            
+//            Text("Italic")
+//                .font(.title2)
+//                .italic()
+//            
+//            Text("Underlined")
+//                .font(.headline)
+//                .underline()
+//            
+//            Text("Strikethrough")
+//                .font(.subheadline)
+//                .strikethrough()
+//            
+//            Text("Custom Font Text")
+//                .font(.custom("AmericanTypewriter", size:24))
+//            
+//            Text("Line Spacing\n Second Line")
+//                .font(.body)
+//                .lineSpacing(10)
+//            
+//        }
+//    }
+//}
+//
+//#Preview {
+//    ContentView()
+//}
+
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View{
     var body: some View {
         VStack {
-            let myString = "Displays a string variable"
-            Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-                .multilineTextAlignment(.trailing)
-                .truncationMode(.tail)
-                .lineLimit(3)
-            
-            Spacer()
-            
-            Text("Bold Text")
-                .font(.title)
-                .bold()
-            
-            Text("Italic")
-                .font(.title2)
-                .italic()
-            
-            Text("Underlined")
-                .font(.headline)
-                .underline()
-            
-            Text("Strikethrough")
-                .font(.subheadline)
-                .strikethrough()
-            
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypewriter", size:24))
-            
-            Text("Line Spacing\n Second Line")
-                .font(.body)
-                .lineSpacing(10)
-            
+            Label("Text", systemImage: "clock.fill").font(.title)
         }
     }
 }
