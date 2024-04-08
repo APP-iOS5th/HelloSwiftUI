df5dbafd2a8118653dca0c63f62f8a26172f76d1
Hyunseo Yook <hhh3332@naver.com>
2024-04-08 13:22:39 +0900

    Padding

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,10 +10,41 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           Text("Default padding of 16 points")
+                .padding()
+                .background(Color.yellow)
+            
+            Text("Default padding of 45 points")
+                 .padding(45)
+                 .background(Color.yellow)
+            
+            Text("Default padding of 3 points")
+                 .padding(3)
+                 .background(Color.yellow)
+
+            Text("Top padding only")
+                .padding(.top)
+                 .background(Color.yellow)
+
+            Text("Trailing padding only")
+                .padding(.trailing)
+                 .background(Color.yellow)
+
+            Text("Bottom padding only")
+                .padding(.bottom)
+                 .background(Color.yellow)
+            
+            Text("Leading padding only")
+                .padding(.leading, 45)
+                 .background(Color.yellow)
+            
+            Text("Top, Leading padding")
+                .padding([.top, .leading], 45)
+                 .background(Color.yellow)
+
+            Text("Top, Leading and Bottom")
+                .padding([.top, .leading, .bottom], 45)
+                 .background(Color.yellow)
         }
         .padding()
     }
