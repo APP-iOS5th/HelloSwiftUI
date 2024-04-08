b1452ffee5d2093f2b171e1f862fb538b6520fd4
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 15:30:56 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,18 +11,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            //폰트 크기로 이미지 크기 조정 가능
-            Spacer()
-            Image(systemName: "tortoise.fill")
-            Image(systemName: "tortoise.fill").font(.largeTitle)
-            Image(systemName: "tortoise.fill").font(.custom("", size: 50))
-            Spacer()
-            
-            Image("apple")
-                .resizable()
-                .aspectRatio(contentMode: .fit) //fit 은 딱 맞게 , fill은 넘쳐도 된다..?
-                .frame(width: 150, height: 50)
-            Spacer()
+
         }
     }
 }
