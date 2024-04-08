6f7706fe69963cef5418ac20fe224fd01f8b1069
jmbae <jmbae@codegrove.co.kr>
2024-04-08 15:41:53 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,43 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            //            Capsule().fill(Color.yellow)
-            //            Circle().fill(Color.blue)
-            //            Ellipse().fill(Color.brown)
-            //            Rectangle().fill(Color.red)
-            //            RoundedRectangle(cornerRadius:  25).fill(Color.green)
-            
-            //            Spacer()
-            //            Ellipse().fill(
-            //                RadialGradient(
-            //                    gradient: Gradient(colors:[.blue,.yellow]),
-            //                    center: .top,
-            //                    startRadius: 10,
-            //                    endRadius: 65
-            //                ))
-            //            Spacer()
-            //            Ellipse().fill(
-            //                RadialGradient(
-            //                    gradient: Gradient(colors:[.blue,.yellow]),
-            //                    center: .center,
-            //                    startRadius: 10,
-            //                    endRadius: 65
-            //                ))
-            //            Spacer()
-            //            Ellipse().fill(
-            //                AngularGradient(
-            //                    gradient: Gradient(colors:[.blue,.yellow]),
-            //                    center: .center
-            //                ))
-            //            Spacer()
-            Image(systemName: "tortoise.fill")
-            Image(systemName: "tortoise.fill").font(.largeTitle)
-            Image(systemName: "tortoise.fill").font(.custom("", size: 50))
-            Spacer()
-            Image("clock-solid")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 150, height: 50)
+
         }
     }
 }
