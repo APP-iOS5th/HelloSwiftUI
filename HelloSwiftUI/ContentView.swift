a5c071eef1bcaf38f0fa2b5d5dc9b31a8d5105df
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 14:32:24 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,34 +11,7 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Spacer()
             
-            let myString = "Displays a string variable"
-//            Text("Hello World \(myString)")
-            Text("This is my age \(myString). Since I am retired, I am mow eligible for a pensoin and Social Security so I can spend the rest of my life relaxing and enhoying life without having to work for an income anymore.")
-                .multilineTextAlignment(.leading) // 문단정렬
-                .truncationMode(.tail) //라인생략 tail head middle
-                .lineLimit(4)
-            
-            Spacer()
-            
-            Text("Bold Text")
-                .font(.title)
-                .bold()
-            Text("Italic")
-                .font(.headline)
-                .italic()
-            Text("Underline")
-                .underline()
-            Text("Strikethrough")
-                .strikethrough()
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypewriter", size: 24))
-            Text("Line Spacing \n Second line")
-                .font(.body)
-                .lineSpacing(10) // 줄간격
-            
-            Spacer()
         }
     }
 }
