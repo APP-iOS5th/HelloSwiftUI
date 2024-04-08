4ea8ac7e8e97e190ece21b3cca9b87893833cff3
ksiomng0929 <nadana0929@gmail.com>
2024-04-08 14:22:11 +0900

    font & customfont & fontstyle ...

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,35 +10,35 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        VStack (spacing: 20){
-//            ZStack {
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.gray)
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.purple)
-//                    .position(x: 255, y: 125)
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.pink)
-//                    .offset(x: 74, y: 125)
-//            }
-            Text("1111")
-                .font(.system(size: 40))
-                .background(Color.red)
-            Text("2222")
-                .font(.system(size: 40))
-                .background(Color.orange)
-            Text("3333")
-                .font(.system(size: 40))
-                .background(Color.green)
-            Text("4444")
-                .font(.system(size: 40))
-                .background(Color.blue)
-            Text("5555")
-                .font(.system(size: 40))
-                .background(Color.purple)
+        VStack {
+            Spacer()
+            
+            let myString = "Displays a string variable"
+//            Text("Hello World \(myString)")
+            Text("This is my age \(myString). Since I am retired, I am mow eligible for a pensoin and Social Security so I can spend the rest of my life relaxing and enhoying life without having to work for an income anymore.")
+                .multilineTextAlignment(.leading) // 문단정렬
+                .truncationMode(.tail) //라인생략 tail head middle
+                .lineLimit(4)
+            
+            Spacer()
+            
+            Text("Bold Text")
+                .font(.title)
+                .bold()
+            Text("Italic")
+                .font(.headline)
+                .italic()
+            Text("Underline")
+                .underline()
+            Text("Strikethrough")
+                .strikethrough()
+            Text("Custom Font Text")
+                .font(.custom("AmericanTypewriter", size: 24))
+            Text("Line Spacing")
+                .font(.body)
+                .lineSpacing(10)
+            
+            Spacer()
         }
     }
 }
