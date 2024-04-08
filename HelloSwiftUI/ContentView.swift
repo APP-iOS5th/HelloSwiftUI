844652549d52c7f0db2abc5094ed37929b6222bf
Ruel <c0407h@gmail.com>
2024-04-08 21:11:49 +0900

    TextCont

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -16,8 +16,8 @@ struct ContentView: View {
         VStack() {
             let myString = "Displays a string variable"
 //            Text("Hello World, \(myString)")
-            Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-                .multilineTextAlignment(.trailing)
+            Text("This is my age \(myString). Since I am retired,\n I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+                .multilineTextAlignment(.leading)
                 .truncationMode(.tail)
                 .lineLimit(3)
 
