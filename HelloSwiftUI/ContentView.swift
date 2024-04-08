ec2fc505325e0fe862bab0fd5d5c1448e5c00ea8
SungWoon Lee <lsw.netline@gmail.com>
2024-04-08 14:59:58 +0900

    Label

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,32 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack  {
-            let myString = "Display a string variable"
-//            Text("Hello, World, \(myString)")
-            Text("a11111sdasdasdasdasdadadasdasdasdasdadasdadasdasdasdasdasdasfldkgnseklrhgnsefkjgsnergk.,fnv.kdgnae.rlgbjdfk.,bad.kserngailgmal/kgmadflgkmadfgk.amerlgkanergalngjakergnakergnadfg22222weweweweewefwefwefwefefwefwefwfwefwefwegfedgfdgsergersgdfgergdfgsergdfsdf22")
-                .truncationMode(.tail)//마지막 줄에 중략(...)을 어디에 넣을 것이냐. 앞 뒤 중간
-                .lineLimit(3)
-            
-            
-            Spacer()
-            Text("Bold Text")
-                .font(.title)
-                .bold()
-            
-            Text("Italic")
-                .font(.title2)
-                .italic()
-            Text("Underlined")
-                .font(.headline)
-                .underline()
-Text("Strikethrough")
-                .font(.subheadline)
-                .strikethrough()
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypeWriter", size: 24))
-            Text ("Line Spacing")
-                .font(.body)
-                .lineSpacing(10)
+            Label("Text", systemImage: "clock.circle").font(.title) //아이콘 넣기 sf~에서 이름 보고 넣기. "text", 이후 system~("") 까지. 모디파이어는 크기 때문에 붙인 것.
         }
     }
 }
