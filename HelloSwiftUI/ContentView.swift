e8a40cc21a1453728346db7d99d1d2b020ee0544
Jihye Park <deviloper.park@gmail.com>
2024-04-08 10:19:36 +0900

    Add Button

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("위")
+            HStack {
+                Text("왼쪽")
+                Text("오른쪽")
+            }
+            ZStack {
+                Text("배경")
+                Text("전경")
+            }
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                print("button1 click")
+            }
+            Button(action: {
+                print("button2 click")
+            }, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
         }
-        .padding()
     }
 }
 
