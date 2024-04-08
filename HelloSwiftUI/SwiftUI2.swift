92c0f10561a06a83f477ee9edf5d46af9b9dfacd
MadCow <lobasketve@naver.com>
2024-04-08 13:26:16 +0900

    Stacks alignment & spacing

--- HelloSwiftUI/SwiftUI2.swift
+++ HelloSwiftUI/SwiftUI2.swift
@@ -9,40 +9,45 @@ import SwiftUI
 
 struct SwiftUI2: View {
     var body: some View {
-        
-        // MARK: Padding Example
-        Text("Default padding of 16 points")
-            .padding()
-            .background(.yellow)
-        Text("Default padding of 45 points")
-            .padding(45)
-            .background(.yellow)
-        Text("Default padding of 3 points")
-            .padding(3)
-            .background(.yellow)
-        
-        Text("Top padding only")
-            .padding(.top)
-            .background(.yellow)
-        Text("Trailing padding only")
-            .padding(.trailing)
-            .background(.yellow)
-        Text("Leading padding only")
-            .padding(.leading)
-            .background(.yellow)
-        Text("Bottom padding only")
-            .padding(.bottom)
-            .background(.yellow)
-        Text("Bottom padding only 45 points")
-            .padding(.bottom, 45)
-            .background(.yellow)
-        
-        Text("Top and Leading padding of 45 points")
-            .padding([.top, .leading], 45)
-            .background(.yellow)
-        Text("Top and Trailing and Bottom padding of 45 points")
-            .padding([.top, .trailing, .bottom], 45)
-            .background(.yellow)
+        ScrollView {
+            VStack(alignment: .trailing, spacing: 30) {
+                // MARK: Padding Example
+                Text("Default padding of 16 points")
+                    .padding()
+                    .background(.yellow)
+                Text("Default padding of 45 points")
+                    .padding(45)
+                    .background(.yellow)
+                Text("Default padding of 3 points")
+                    .padding(3)
+                    .background(.yellow)
+                
+                HStack(alignment: .top) {                
+                    Text("Top padding only")
+                        .padding(.top)
+                        .background(.yellow)
+                    Text("Trailing padding only")
+                        .padding(.trailing)
+                        .background(.yellow)
+                    Text("Leading padding only")
+                        .padding(.leading)
+                        .background(.yellow)
+                    Text("Bottom padding only")
+                        .padding(.bottom)
+                        .background(.yellow)
+                    Text("Bottom padding only 45 points")
+                        .padding(.bottom, 45)
+                        .background(.yellow)
+                }
+                
+                Text("Top and Leading padding of 45 points")
+                    .padding([.top, .leading], 45)
+                    .background(.yellow)
+                Text("Top and Trailing and Bottom padding of 45 points")
+                    .padding([.top, .trailing, .bottom], 45)
+                    .background(.yellow)
+            }
+        }
     }
 }
 
