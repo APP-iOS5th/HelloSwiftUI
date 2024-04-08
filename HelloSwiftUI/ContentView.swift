dc3c4f9dd04bc70162b48c88980b9122d443a999
jmbae <jmbae@codegrove.co.kr>
2024-04-08 17:00:05 +0900

    Button @ViewBuilder

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,7 +14,27 @@ extension Color {
 struct ContentView: View {
     var body: some View {
         VStack {
-
+            Button("여기를 클릭하세요") {
+                print("클릭시 실행코드 1")
+            }
+            Button {
+                print("클릭시 실행코드 2")
+            } label: {
+                Text("Click here")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
+            Button {
+                print("클릭시 실행코드 3")
+            } label: {
+                Image("sample_dog_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
+            }
         }
     }
 }
