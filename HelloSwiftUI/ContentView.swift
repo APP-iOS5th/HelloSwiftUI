50d12140adacc32a2ec89852f2ad38bff665af66
seokyung402 <wni402@naver.com>
2024-04-08 13:26:59 +0900

    VStack & HStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,33 +13,35 @@ struct ContentView: View {
     @State private var sliderValue = 0.5
 
     var body: some View {
-        VStack {
+        VStack (alignment: .trailing, spacing: 24) {
             Text("Default padding of 16 points")
                 .padding()
                 .background(Color.yellow)
-            
             Text("Default padding of 45 points")
                 .padding(45)
                 .background(Color.yellow)
-            
             Text("Default padding of 3 points")
                 .padding(3)
                 .background(Color.yellow)
-            
-            Text("Top padding only")
-                .padding(.top)
-                .background(Color.yellow)
-            Text("Trailing padding only")
-                .padding(.trailing)
-                .background(Color.yellow)
-            Text("Bottom padding only")
-                .padding(.bottom)
-                .background(Color.yellow)
+            HStack (alignment: .bottom){
+                Text("Top padding only")
+                    .padding(.top)
+                    .background(Color.yellow)
+                Text("Trailing padding only")
+                    .padding(.trailing)
+                    .background(Color.yellow)
+                Text("Bottom padding only")
+                    .padding(.bottom)
+                    .background(Color.yellow)
+            }
             Text("Leading padding only")
                 .padding(.leading)
                 .background(Color.yellow)
             Text("Top, Leading padding only")
                 .padding([.top, .leading], 45)
+                .background(Color.yellow)
+            Text("Top, Leading, Bottom padding only")
+                .padding([.top, .leading, .bottom], 45)
                 .background(Color.yellow)
         }
     }
