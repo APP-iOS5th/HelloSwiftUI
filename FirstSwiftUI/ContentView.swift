3bffa36c26864a8cf5ed31381b6ab6127c59a8c5
avan31017 <avan31017@gmail.com>
2024-04-08 17:37:07 +0900

    onChange with Picker

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -9,20 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var selectedColor = Color.gray
+//    @State private var selectedColor = Color.gray
+    
+    @State private var message: String = "How are you?"
     
     var body: some View {
-        VStack (spacing: 28) {
-            Rectangle().fill(selectedColor)
-            Picker("Favorite Color", selection: $selectedColor, content: {
-                Text("Red")
-                    .tag(Color.red)
-                Text("Blue")
-                    .tag(Color.blue)
-                Text("Green")
-                    .tag(Color.green)
+//        VStack (spacing: 28) {
+//            Rectangle().fill(selectedColor)
+//            Picker("Favorite Color", selection: $selectedColor, content: {
+//                Text("Red")
+//                    .tag(Color.red)
+//                Text("Blue")
+//                    .tag(Color.blue)
+//                Text("Green")
+//                    .tag(Color.green)
+//            })
+//            .pickerStyle(SegmentedPickerStyle())
+//        }
+        VStack {
+            Text(message)
+            Picker("Feel", selection: $message, content: {
+                Text("Happy").tag("happy")
+                Text("Sad").tag("sad")
+                Text("Bored").tag("bored")
             })
             .pickerStyle(SegmentedPickerStyle())
+            .onChange(of: message) { value in
+                switch value {
+                case "happy":
+                    message = "Be happy and joyous"
+                case "sad":
+                    message = "Life can be a struggle at"
+                case "bored":
+                    message = "Look for your purpose"
+                default:
+                    break
+                }
+            }
         }
     }
 }
