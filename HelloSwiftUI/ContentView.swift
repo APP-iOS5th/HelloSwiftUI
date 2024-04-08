b3d95f21190274f3f97b728151cb617dcde35489
khyerim <ax1021@naver.com>
2024-04-08 11:48:48 +0900

    0408 am

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -2,59 +2,70 @@
 //  ContentView.swift
 //  HelloSwiftUI
 //
-//  Created by Jungman Bae on 4/8/24.
+//  Created by 김혜림 on 4/8/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    //상태 저장 변수
     @State private var isOn = true
     @State private var value = 0
     @State private var sliderValue = 0.5
     
     var body: some View {
-        VStack {
-            Slider(value: $sliderValue, in: 0...1)
-                .tint(.brown)
-                
-            
+        VStack /*버티컬 스택*/ {
             Stepper("값 \(value)", value: $value, in: 0...10)
+            Slider(value: $sliderValue, in: 0...1 )
+                .accentColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
+            /*
+            Toggle(isOn: $isOn, label: {
+                Text("Toggle Example")
+            })
+             */
+            Toggle("Toggle Message On/Off", isOn: $isOn)
             
-            Toggle("Toggle message on/off", isOn: $isOn)
             Text("위")
+            //수정자 추가 : .수정속성(수정내용)
+                .padding(.leading, 9.0)
                 .font(.largeTitle)
                 .fontWeight(.semibold)
             
-            HStack {
+            HStack /*호리젠탈 스택*/ {
                 Text("왼쪽")
                 Text("오른쪽")
             }
+            
             .background(Color.purple)
             ZStack {
                 Text("배경")
                 Text("전경")
             }
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                print("button1 click")
-            }
+            
+            .padding(/*@START_MENU_TOKEN@*/.horizontal, 10.0/*@END_MENU_TOKEN@*/)
             Button(action: {
                 print("button2 click")
             }, label: {
-                Text("Button")
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
             })
+            
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                print("button1 click")
+            }
         }
     }
 }
 
-#Preview {
-    ContentView()
-}
-
 /*
- Swift 3.0 이전 프리뷰
- struct ContentView_Previews: PreviewProvider {
+Swift 3.0 이전 프리뷰
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
- }
- */
+}
+*/
+
+//현재 프리뷰
+#Preview {
+    ContentView()
+}
