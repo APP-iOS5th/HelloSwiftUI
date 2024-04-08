5e6a9c8826e78f74fff879c317b980d55b141363
avan31017 <avan31017@gmail.com>
2024-04-08 13:08:14 +0900

    Ignore

---
+++ FirstSwiftUI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FirstSwiftUI
+//
+//  Created by 조성빈 on 4/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var toggleTF = true
+    @State private var stepValue = 0
+    @State private var slideValue: Float = 0
+    
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
---
+++ FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by 조성빈 on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
