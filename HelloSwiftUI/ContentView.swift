cb9016c5d1490c647457e92dee8c2ef5fa43c22b
seokyung402 <wni402@naver.com>
2024-04-08 13:15:47 +0900

    Padding

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,25 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isOn = true
+    @State private var value = 0
+    @State private var sliderValue = 0.5
+
     var body: some View {
         VStack {
-            Text("위")
-            HStack {
-                Text("왼쪽")
-                Text("오른쪽")
-            }
-            ZStack {
-                Text("배경")
-                Text("전경")
-            }
-            Button("Button") {
-                print("button1 click")
-            }
-            Button(action: {
-                print("button2 click")
-            }, label: {
-                    Text("Button")
-            })
+            Text("Default padding of 16 points")
+                .padding()
+                .background(Color.yellow)
+            
+            Text("Default padding of 45 points")
+                .padding(45)
+                .background(Color.yellow)
+            
+            Text("Default padding of 3 points")
+                .padding(3)
+                .background(Color.yellow)
+            
+            Text("Top padding only")
+                .padding(.top)
+                .background(Color.yellow)
+            Text("Trailing padding only")
+                .padding(.trailing)
+                .background(Color.yellow)
+            Text("Bottom padding only")
+                .padding(.bottom)
+                .background(Color.yellow)
+            Text("Leading padding only")
+                .padding(.leading)
+                .background(Color.yellow)
+            Text("Top, Leading padding only")
+                .padding([.top, .leading], 45)
+                .background(Color.yellow)
         }
     }
 }
