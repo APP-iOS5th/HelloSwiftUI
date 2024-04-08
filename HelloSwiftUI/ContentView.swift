dfe1fb148389fa9c2074d35467f618d6088917ec
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-08 15:26:41 +0900

    Image

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -3,20 +3,53 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Label("Text", systemImage: "clock.circle").font(.title)
+            //            Capsule().fill(Color.yellow)
+            //            Circle().fill(Color.blue)
+            //            Ellipse().fill(Color.brown)
+            //            Rectangle().fill(Color.red)
+            //            RoundedRectangle(cornerRadius: 30).fill(Color.green)
+//            Spacer()
+//            Ellipse().fill(
+//                RadialGradient(
+//                    gradient: Gradient(
+//                        colors: [.blue, .yellow]),
+//                    center: .top,
+//                    startRadius: 10,
+//                    endRadius: 65
+//                ))
+//            Spacer()
+//            Ellipse().fill(
+//                RadialGradient(
+//                    gradient: Gradient(
+//                        colors: [.blue, .yellow]),
+//                    center: .center,
+//                    startRadius: 10,
+//                    endRadius: 65
+//                ))
+//            Spacer()
+//            Ellipse().fill(
+//                AngularGradient(
+//                    gradient: Gradient(
+//                        colors: [.blue, .yellow]),
+//                    center: .center
+//                ))
+//            Spacer()
+            Image(systemName: "tortoise.fill")
+            Image(systemName: "tortoise.fill").font(.largeTitle)
+            Image(systemName: "tortoise.fill").font(.custom("", size: 50))
+            Spacer()
+            Image(systemName: "clock")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 50)
         }
     }
 }
 
-#Preview {
-    ContentView()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
 
-/*
- Swift 3.0 이전 프리뷰
- struct ContentView_Previews: PreviewProvider {
- static var previews: some View {
- ContentView()
- }
- }
- */
