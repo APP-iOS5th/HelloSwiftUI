c62b7221f2b3ae392773b24b74e5870265e09f9b
jinwoong <baseball0020@gmail.com>
2024-04-08 13:15:51 +0900

    add padding

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,40 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isOn = true
-    @State private var value = 0
-    @State private var sliderValue = 0.5
-    
     var body: some View {
         VStack {
-            Slider(value: $sliderValue, in: 0...1)
-                .tint(.brown)
-                
+            Text("Default paddding of 16 points")
+                .padding()
+                .background(Color.yellow)
+            Text("Default paddding of 45 points")
+                .padding(45)
+                .background(Color.yellow)
+            Text("Default paddding of 3 points")
+                .padding(3)
+                .background(Color.yellow)
             
-            Stepper("값 \(value)", value: $value, in: 0...10)
+            Text("Top padding only")
+                .padding(.top)
+                .background(Color.yellow)
+            Text("Trailing padding only")
+                .padding(.trailing)
+                .background(Color.yellow)
+            Text("Bottom padding only")
+                .padding(.bottom)
+                .background(Color.yellow)
+            Text("Leading padding only")
+                .padding(.leading, 45)
+                .background(Color.yellow)
+            Text("Top, Leading padding")
+                .padding([.top,.leading], 45)
+                .background(Color.yellow)
             
-            Toggle("Toggle message on/off", isOn: $isOn)
-            Text("위")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-            
-            HStack {
-                Text("왼쪽")
-                Text("오른쪽")
-            }
-            .background(Color.purple)
-            ZStack {
-                Text("배경")
-                Text("전경")
-            }
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                print("button1 click")
-            }
-            Button(action: {
-                print("button2 click")
-            }, label: {
-                Text("Button")
-            })
+            Text("Top, Leading and Bottom")
+                .padding([.top, .leading, .bottom], 45)
+                .background(Color.yellow)
         }
     }
 }
