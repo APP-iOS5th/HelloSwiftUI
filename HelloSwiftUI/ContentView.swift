38b59a713fbe58cbcbc8aab335902f2bf4200ee4
seonggeun an <eventscripts@nate.com>
2024-04-08 17:21:44 +0900

    SegmentPickerStyle

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,15 +7,19 @@
 
 import SwiftUI
 
-extension Color {
-    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
-}
-
 struct ContentView: View {
-    @State var colorMe = false
+    
+    @State private var selectedColor = Color.gray
+    
     var body: some View {
-        VStack {
-            
+        VStack (spacing: 28) {
+                    Rectangle().fill(selectedColor)
+                    Picker("Favorite Color", selection: $selectedColor, content: {
+                        Text("Red").tag(Color.red)
+                        Text("Green").tag(Color.green)
+                        Text("Blue").tag(Color.blue)
+                    })
+                    .pickerStyle(SegmentedPickerStyle())
         }
     }
 }
