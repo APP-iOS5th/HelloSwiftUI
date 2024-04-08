f5bcf1cffbeba703f82f9147bd540ed456e13da0
seonggeun an <eventscripts@nate.com>
2024-04-08 17:00:16 +0900

    Button View Builder

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,8 +14,30 @@ extension Color {
 struct ContentView: View {
     var body: some View {
         VStack {
+            Button("여기를 클릭하세요"){
+                print("클릭시 실행코드")
+            }
+            Button {
+                print("클릭시 실행코드")
+            }label: {
+                Text("Click Here")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
             
+            Button {
+                
+            } label: {
+                Image("sample_dog_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
+            }
         }
+        
     }
 }
 
