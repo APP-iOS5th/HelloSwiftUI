a680d147c0fe3957aa25ee8655227dd8b6514be1
wotjs020708 <wotjs020708@gmail.com>
2024-04-08 17:05:38 +0900

    Button @ViewBuilder

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,22 +14,27 @@ extension Color {
 struct ContentView: View {
     var body: some View {
         VStack{
-            // Asset color scheme 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color("custom_brown"))
-            // 지역상수 사용
-            let customColor = Color(red: 1, green: 0, blue: 0)
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(customColor)
-            // 전역상수 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color.myCustomColor)
+            Button("요기를 클릭하세요"){
+                print("클릭시 실행코드 1")
+            }
+            Button{
+                print("클릭시 실행코드 2")
+            } label: {
+                Text("Click here")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
+            Button{
+                print("클릭시 실행코드 3")
+            } label: {
+                Image("sample_dog_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
+            }
         }
     }
 }
