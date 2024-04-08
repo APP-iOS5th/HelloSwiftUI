96170bf740bcdaa8d4847e65d076a41e50717e09
rmfls0606 <rmfls0606@naver.com>
2024-04-08 13:22:09 +0900

    padding Example

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,36 +13,44 @@ struct ContentView: View {
     @State private var sliderValue = 0.5
     
     var body: some View {
-        VStack {
-            Slider(value: $sliderValue, in: 0...1)
-                .tint(.brown)
-                
+        VStack() {
+           Text("Default padding of 16 points")
+                .padding()
+                .background(.yellow)
+           
+            Text("Default padding of 45 points")
+                 .padding(45)
+                 .background(.yellow)
             
-            Stepper("값 \(value)", value: $value, in: 0...10)
+            Text("Default padding of 3 points")
+                 .padding(3)
+                 .background(.yellow)
             
-            Toggle("Toggle message on/off", isOn: $isOn)
-            Text("위")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
+            Text("Top padding only")
+                .padding(.top)
+                 .background(.yellow)
             
-            HStack {
-                Text("왼쪽")
-                Text("오른쪽")
-            }
-            .background(Color.purple)
-            ZStack {
-                Text("배경")
-                Text("전경")
-            }
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                print("button1 click")
-            }
-            Button(action: {
-                print("button2 click")
-            }, label: {
-                Text("Button")
-            })
-        }
+            Text("Trailing padding only")
+                .padding(.trailing)
+                 .background(.yellow)
+            
+            Text("Bottom padding only")
+                .padding(.bottom)
+                 .background(.yellow)
+            
+            Text("Leading padding only")
+                .padding(.leading, 45)
+                 .background(.yellow)
+            
+            Text("Top Leading padding only")
+                .padding([.top, .leading], 45)
+                 .background(.yellow)
+            
+            Text("Top Leading and Bottom padding only")
+                .padding([.top, .leading, .bottom], 45)
+                 .background(.yellow)
+            
+        }//: VSTACK
     }
 }
 
