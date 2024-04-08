7dee52a9cd0371996a038ccf8366690b4a67f162
Jinx <jinxwift@gmail.com>
2024-04-08 17:19:34 +0900

    segmented picker style

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -270,44 +270,44 @@
 //    }
 //}
 
-import SwiftUI
-
-extension Color {
-    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
-}
-
-struct ContentView: View {
-    @State var colorMe = false
-    var body: some View {
-        VStack {
-            Rectangle()
-                .fill(colorMe ? Color.green : Color.gray)
-                .frame(width: 250, height: 100)
-
-            Button("여기를 클릭하세요") {
-                colorMe.toggle()
-            }
-            Button {
-                colorMe.toggle()
-            } label: {
-                Text("Click here")
-                    .font(.largeTitle)
-                    .foregroundColor(.green)
-                    .padding()
-                    .border(Color.red, width: 6)
-            }
-            Button {
-                colorMe.toggle()
-            } label: {
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
-            }
-        }
-    }
-}
+//import SwiftUI
+//
+//extension Color {
+//    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
+//}
+//
+//struct ContentView: View {
+//    @State var colorMe = false
+//    var body: some View {
+//        VStack {
+//            Rectangle()
+//                .fill(colorMe ? Color.green : Color.gray)
+//                .frame(width: 250, height: 100)
+//
+//            Button("여기를 클릭하세요") {
+//                colorMe.toggle()
+//            }
+//            Button {
+//                colorMe.toggle()
+//            } label: {
+//                Text("Click here")
+//                    .font(.largeTitle)
+//                    .foregroundColor(.green)
+//                    .padding()
+//                    .border(Color.red, width: 6)
+//            }
+//            Button {
+//                colorMe.toggle()
+//            } label: {
+//                Image("sample_dog_square")
+//                    .resizable()
+//                    .frame(width: 150, height: 150)
+//                    .clipShape(Circle())
+//                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
+//            }
+//        }
+//    }
+//}
 //
 //struct ContentView: View {
 //    var body: some View {
@@ -336,6 +336,24 @@ struct ContentView: View {
 //    }
 //}
 
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedColor = Color.gray
+    
+    var body: some View {
+        VStack (spacing: 28) {
+            Rectangle().fill(selectedColor)
+            Picker("Favorite Color", selection: $selectedColor, content: {
+                Text("Red").tag(Color.red)
+                Text("Green").tag(Color.green)
+                Text("Blue").tag(Color.blue)
+            })
+            .pickerStyle(SegmentedPickerStyle())
+        }
+    }
+}
 
 #Preview {
     ContentView()
