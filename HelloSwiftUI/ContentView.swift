d07e658a9a344b836775c2d2a0749657ea0a6f4e
jinwoong <baseball0020@gmail.com>
2024-04-08 14:01:35 +0900

    add offset

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,39 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Spacer()
-            Text("Default paddding of 16 points")
-                .padding()
+            Text ("First")
+                .font(.system(size: 40))
                 .background(Color.yellow)
-            Text("Default paddding of 45 points")
-                .padding(45)
+            Text ("Second View")
+                .font(.system(size: 40))
                 .background(Color.yellow)
-            Text("Default paddding of 3 points")
-                .padding(3)
-                .background(Color.yellow)
-            
-            Text("Top padding only")
-                .padding(.top)
-                .background(Color.yellow)
-            Text("Trailing padding only")
-                .padding(.trailing)
-                .background(Color.yellow)
-            Text("Bottom padding only")
-                .padding(.bottom)
-                .background(Color.yellow)
-            Spacer()
-            Text("Leading padding only")
-                .padding(.leading, 45)
-                .background(Color.yellow)
-            Text("Top, Leading padding")
-                .padding([.top,.leading], 45)
-                .background(Color.yellow)
-            
-            Text("Top, Leading and Bottom")
-                .padding([.top, .leading, .bottom], 45)
-                .background(Color.yellow)
-            Spacer()
         }
+        .offset(x: 25, y: 155)
     }
 }
 
