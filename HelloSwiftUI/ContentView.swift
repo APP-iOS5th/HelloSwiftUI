fffb0a5622ffef8d79ecbb3ac01172442cc16bc3
Ruel <c0407h@gmail.com>
2024-04-08 21:30:33 +0900

    SystemImage with SF Symbols

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,41 +14,8 @@ struct ContentView: View {
     
     var body: some View {
         VStack() {
-            let myString = "Displays a string variable"
-//            Text("Hello World, \(myString)")
-            Text("This is my age \(myString). Since I am retired,\n I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-                .multilineTextAlignment(.leading)
-                .truncationMode(.tail)
-                .lineLimit(3)
-
-            Spacer()
-            Text("Bold Text")
-                .font(.title)
-                .bold()
-
-            Text("Italic")
-                .font(.title2)
-                .italic()
-
-            Text("Underlined")
-                .font(.headline)
-                .underline()
-
-            Text("Strikethrough")
-                .font(.subheadline)
-                .strikethrough()
-
-            Text("Custom Font Text")
-                .font(.custom("AmericanTypewriter", size: 24))
-
-            Text("Line Spacing\n Line Spacing")
-                .font(.body)
-                .lineSpacing(20)
-                .background(.red)
-        }
-
-    
-       
+            Label("Text", systemImage: "clock.circle").font(.title)
+        }       
     }
 }
 
