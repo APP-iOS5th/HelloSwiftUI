4315093a8d43add966469816a81111041b560bda
Jihye Park <deviloper.park@gmail.com>
2024-04-08 17:01:07 +0900

    Button @ViewBuilder

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,31 +7,33 @@
 
 import SwiftUI
 
-extension Color {
-    static let myCustomColor = Color(red: 0.8, green: 0.8, blue: 0.3)
-}
-
 struct ContentView: View {
     var body: some View {
         VStack {
-            // Asset Color scheme 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color("custom_brown"))
             
-            // 지역상수 사용
-            let customColor = Color(red: 1, green: 0, blue: 0)
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(customColor)
+            Button("여기를 클릭하세요") {
+                print("클릭 시 실행코드")
+            }
             
-            // 전역상수 사용
-            Image(systemName: "heart.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(Color.myCustomColor)
+            Button{
+                print("클릭 시 실행코드2")
+            } label: {
+                Text("Click here")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
+        
+            Button {
+                print("클릭 시 실행코드3")
+            } label: {
+                Image("sample_dog_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
+            }
         }
     }
 }
@@ -40,12 +42,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-/*
- Swift 3.0 이전 프리뷰
- struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
- }
- */
