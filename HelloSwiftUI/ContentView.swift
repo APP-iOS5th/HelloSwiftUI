0ba43aaf15a1fb3c49af0315dffffbcc79cbef62
DopeKS <kyusang.dev@gmail.com>
2024-04-08 14:06:14 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,28 +11,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-//            ZStack {
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.yellow)
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.yellow)
-//                    .position(x: 255, y: 127)
-//                Text("Top")
-//                    .font(.system(size: 40))
-//                    .background(Color.yellow)
-//                    .offset(x: 74, y: 125)
-//            }
-            Text ("First")
-                .font(.system(size: 40))
-                .background(Color.yellow)
-            Text ("Second View")
-                .font(.system(size: 40))
-                .background(Color.yellow)
+            
         }
-        // VStack offset 설정
-        .offset(x: 25, y: 125)
     }
 }
 
