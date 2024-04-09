dfda3d501e5b34489b4fd706cee73ad4425991c9
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 13:37:19 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,34 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var message = ""
     var body: some View {
         VStack {
-            Menu("Options") { //Menu라는 버튼안에 기능을 숨기는거
-                Button("Open", action: openFile)
-                Button("Find", action: findFile)
-                Button("Delete...", action: deleteFile)
-            }
-            .menuStyle(DefaultMenuStyle())
-            .menuOrder(.fixed)
-            
-            Text(message).padding()
-            
-            Link(destination: URL(string: "http://www.apple.com")!, label: { //링크 프리뷰에서는 동작 지원하지 않음
-                Text("Apple")
-            })
             
         }
-    }
-    func openFile() {
-        message = "Open chosen"
-    }
-    
-    func findFile() {
-        message = "Find chosen"
-    }
-    func deleteFile() {
-        message = "Delete chosen"
     }
 }
 
