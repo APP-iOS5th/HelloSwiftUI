69fb649e472ae62261af65f6e00e8bba0c670257
Jin Park <jinpark1504@gmail.com>
2024-04-09 17:36:45 +0900

    Group Box

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,20 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var messageOne = ""
-    @State var messageTwo = ""
+    @State var flag = false
+    @State var message = ""
     var body: some View {
         VStack (spacing: 20) {
             Form {
-                Text("This is the first form")
-                TextField("Type here", text: $messageOne)
+                Section {
+                    Text("This is a section has no header")
+                }
+                Section("Just a Header") {
+                    Text("This section uses a simple header")
+                }
+                Section {
+                    Text("This section uses a simple footer")
+                } footer: {
+                    Text("Just a Footer")
+                }
+                Section {
+                    Text("This section use both a header and a footer")
+                } header: {
+                    Text("The Header")
+                } footer: {
+                    Text("The Footer")
+                }
+                
             }
+            
             Form {
-                Text("This is the second form")
-                TextField("Type here", text: $messageTwo)
+                Text("This is a Form")
+                Toggle(isOn: $flag, label: {
+                    Text("Click me")
+                })
             }
-            Text("Form #1 = \(messageOne)")
-            Text("Form #2 = \(messageTwo)")
+            .frame(maxHeight: 150)
+            
+            GroupBox(label: Text("Group Box")) {
+                Text("This is a Group Box")
+                Toggle(isOn: $flag, label: {
+                    Text("Click me")
+                })
+                TextField("Type here", text: $message)
+                    .disabled(flag)
+            }
         }
     }}
 
