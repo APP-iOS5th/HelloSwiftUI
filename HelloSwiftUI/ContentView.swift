6121bd3cec50189519c72e9a9049da3e756bb0cb
ksiomng0929 <nadana0929@gmail.com>
2024-04-09 16:38:21 +0900

    ForEach List Animals

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,10 +1,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    struct Animal: Identifiable {
+        var id = UUID()
+        var name: String
+    }
+    
+    struct AnimalCategory: Identifiable {
+        var id = UUID()
+        var category: String
+        var animals: [Animal]
+    }
+    
+    let categories = [
+        AnimalCategory(category: "포유류", animals: [Animal(name: "고양이"), Animal(name: "강아지")]),
+        AnimalCategory(category: "파충류", animals: [Animal(name: "거북이"), Animal(name: "도마뱀")])
+    ]
     
     var body: some View {
         VStack {
-            
+            List {
+                ForEach(categories) { c in //id: \.id는 중복되지 않으면 생략가능 ?>
+                    Section(header: Text(c.category)) {
+                        ForEach(c.animals, id: \.id) { animal in
+                            Text(animal.name)
+                        }
+                    }
+                    
+                }
+            }
         }
     }
 }
