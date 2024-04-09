2b35cd733ea5674220bf0b0b7934d29c2559c74f
Jihye Park <deviloper.park@gmail.com>
2024-04-09 11:08:27 +0900

    DatePicker with Range

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,34 +7,26 @@
 
 import SwiftUI
 
-// 버전 컨트롤(버전 15 아래는 실행되지 않음)
-@available(iOS 15.0, *)
 struct ContentView: View {
-    @State private var choice = 0.0
-    @State private var myColor = Color.red
     @State var myDate = Date.now
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
+        let endCOmponents = DateComponents(year: 2024, month: 9, day: 13)
+        
+        return calendar.date(from: startComponents)!
+        ...
+        calendar.date(from: endCOmponents)!
+    }()
     
     var body: some View {
         VStack {
-            Picker(selection: $choice, label: Text("Picker")) {
-                Text("Bird").tag(1.7)
-                Text("Cat").tag(2.06)
-                Text("Lizard").tag(3.41)
-                Text("Dog").tag(4.13)
-                Text("Hamster").tag(5.28)
-            }.pickerStyle(SegmentedPickerStyle())
-            // 각 목록의 tag 값이 나옴
-            Text("You choose \(choice)")
-            
-            // 색상 선택
-            ColorPicker("Pick a color", selection: $myColor)
-            Rectangle()
-                .frame(width: 200, height: 150)
-                .foregroundColor(myColor)
-            
+            Text("Chosen date = \(myDate)")
+                .padding()
             // 날짜 선택
-            DatePicker(selection: $myDate, label: { Text("Date") })
+            DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date") })
                 .datePickerStyle(.graphical) // 캘린더
+                .padding()
         }
     }
 }
