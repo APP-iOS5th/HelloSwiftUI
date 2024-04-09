d7320e15562082c85601836cb400422322e5cd3d
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 15:39:47 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,27 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showAlert = false
     var body: some View {
         VStack {
-            Button("Show Alert") {
-                 showAlert.toggle()
-             }
-            .confirmationDialog("Warning", isPresented: $showAlert, titleVisibility: .visible) {
-                Button(role: .none) {
-                    print("option1 action")
-                } label: {
-                    Text("option1")
-                }
-                
-                Button(role: .destructive) {
-                    print("option2 action")
-                } label: {
-                    Text("option2")
-                }
-            } message: {
-                Text("Zombies on the loose")
-            }
 
          }
      }
