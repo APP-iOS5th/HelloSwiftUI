369cfcaf28e2d0a4d2bf0a1e1d5c521ef45f2293
jinwoong <baseball0020@gmail.com>
2024-04-09 11:02:18 +0900

    DatePicker range

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,6 +12,14 @@ struct ContentView: View {
     @State private var myColor = Color.red
     @State private var myDate = Date.now
     
+    private let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let start = DateComponents(year: 2024, month: 3)
+        let end = DateComponents(year: 2024, month: 5)
+        
+        return calendar.date(from: start)!...calendar.date(from: end)!
+    }()
+    
     var body: some View {
         VStack {
             Picker(selection: $choice) {
@@ -26,9 +34,16 @@ struct ContentView: View {
             Rectangle()
                 .frame(width: 200, height: 150)
                 .foregroundStyle(myColor)
-            DatePicker(selection: $myDate) {
-                Text("Date")
-            }
+//            DatePicker(selection: $myDate) {
+//                Text("Date")
+//            }
+//            .datePickerStyle(.graphical)
+            DatePicker(
+                "Date",
+                selection: $myDate,
+                in: dateRange,
+                displayedComponents: .date
+            )
             .datePickerStyle(.graphical)
         }
     }
