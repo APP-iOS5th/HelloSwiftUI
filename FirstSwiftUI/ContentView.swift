aba993c142b0cd8b116c09db0bb39e7476a35a98
avan31017 <avan31017@gmail.com>
2024-04-09 10:07:56 +0900

    dismissKeyboard

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -7,9 +7,13 @@
 
 import SwiftUI
 
+//특정 버전 지원 제한 -> 15.0 버전 이하는 실행불가.
+@available(iOS 15.0, *)
 struct ContentView: View {
     
     @State private var message = ""
+    @FocusState var dismissKeyBoard: Bool
+    
     
     var body: some View {
         VStack {
@@ -17,12 +21,19 @@ struct ContentView: View {
                 .autocorrectionDisabled()
                 .keyboardType(.emailAddress)
                 .submitLabel(.done)
+                .focused($dismissKeyBoard)
                 .textFieldStyle(.roundedBorder)
                 .frame(width: 200)
             
             SecureField("This is SecureTF.", text: $message)
                 .textFieldStyle(.roundedBorder)
                 .frame(width: 200)
+            
+            Button(action: {
+                dismissKeyBoard = false
+            }){
+                Text("Hide KeyBoard")
+            }
         }
     }
 }
