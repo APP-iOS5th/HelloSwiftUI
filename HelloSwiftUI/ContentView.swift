27a56d1b209ba5bb000ebd1c145bbd7c794157cf
ksiomng0929 <nadana0929@gmail.com>
2024-04-09 15:14:34 +0900

    Alert & dismissButton New

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,10 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showAlert = false
     
     var body: some View {
         VStack {
-
+            Button("Show") {
+                showAlert.toggle()
+            }
+            .alert("alert", isPresented: $showAlert) {
+                Button("OK") {}
+            } message: {
+                Text("message")
+            }
+            
         }
     }
 }
