3fee7f1ec6cad0b6094bd48e879aa6f30b112dd8
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-09 14:42:28 +0900

    Rotation Gesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,24 +11,21 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var tempValue: CGFloat = 0 //private 이 화면 밖에서 temp value를 수정할 일이 있을까요? 없으니 private 을 사용
-    @State private var finalValue: CGFloat = 1
+    @State private var degree = 0.0
+    
     var body: some View {
         VStack  {
+            Text("degree = \(degree)")
             Image(systemName: "star.fill")
                 .resizable()
                 .scaledToFit() //얘는 부모크기에 맞춰줌
                // .aspectRatio(contentMode: .fit) 얘는 비율에 fit, fill을 시키는 거고
                 .frame(width: 200, height: 200)
-                .scaleEffect(finalValue + tempValue)
+                .rotationEffect(Angle.degrees(degree))
                 .gesture(
-                    MagnificationGesture()
-                        .onChanged { amount in
-                            tempValue = amount - 1
-                        }
-                        .onEnded { amount in
-                            finalValue += tempValue
-                            tempValue = 0
+                    RotationGesture()
+                        .onChanged{ angle in
+                            degree = angle.degrees
                         }
                 )
         }
