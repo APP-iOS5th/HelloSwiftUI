86da5cf42039dad0497cb7189a64ae473f0cdf97
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-09 17:04:22 +0900

    form

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,34 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    struct Animal: Identifiable {
-        var id = UUID()
-        var name: String
-    }
-    
-    struct AnimalCategory: Identifiable {
-        var id = UUID()
-        var category: String
-        var animals: [Animal]
-    }
-    
-    let categories = [
-        AnimalCategory(category: "포유류", animals: [Animal(name: "고양이"),Animal(name: "강아지")]),
-        AnimalCategory(category: "파충류", animals: [Animal(name: "거북이"),Animal(name: "도마뱀")]),
-    ]
+    @State private var message1 = ""
+    @State private var message2 = ""
     
     var body: some View {
         VStack {
-            List {
-                ForEach(categories) { category in
-                    Section(header: Text(category.category)) {
-                        ForEach(category.animals) { animal in
-                            Text(animal.name)
-                        }
-                    }
-                }
+            Form {
+                Text("This is the first Form")
+                TextField("Type here", text: $message1)
             }
+            Form {
+                Text("This is the second Form")
+                TextField("Type here", text: $message2)
+                    .autocorrectionDisabled()
+            }
+            Text("Form #1 \(message1)")
+            Text("Form #2 \(message2)")
         }
     }
 }
