16284308448ad3c5ab2d3b7d21c07a218f22d14c
Jin Park <jinpark1504@gmail.com>
2024-04-09 11:20:52 +0900

    DatePicker with Range

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,31 +9,22 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-    @State private var choice = 0.0
-    @State private var myColor = Color.red
     @State private var myDate = Date.now
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
+        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
+        return calendar.date(from: startComponents)!...calendar.date(from: endComponents)!
+    }()
+    let formatter = DateFormatter()
+    
     var body: some View {
         VStack {
-//            Picker(selection: $choice, label: Text("Picker")) {
-            Picker("Picker", selection: $choice) {
-                Text("Bird").tag(1.7)
-                Text("Cat").tag(2.06)
-                Text("Lizard").tag(3.41)
-                Text("Dog").tag(4.13)
-                Text("Hamster").tag(5.28)
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            Text("You chose \(choice)")
-            
-//            ColorPicker("Pick a color", selection: $myColor)
-            ColorPicker(selection: $myColor, label: { Text("Pick a color")})
-            Rectangle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(myColor)
-            
-            DatePicker(selection: $myDate, label: { Text("Date") })
-//            DatePicker("Date", selection: $myDate)
-                .datePickerStyle(.compact)
+            Text("Chosne date = \(myDate)")
+                .padding()
+            DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date") })
+                .datePickerStyle(.graphical)
+                .padding()
         }
     }
 }
