cefba3af07b65d539c351ec39587a03d1831ba90
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-09 11:37:29 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,26 +9,8 @@ import SwiftUI
 
 @available(iOS 15.0, *) // 15 이하로는 실행 안되게하는?
 struct ContentView: View {
-    @State private var myDate = Date.now
-    let dateRange:  ClosedRange<Date> = {
-        let calendar = Calendar.current
-        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
-        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
-        return calendar.date(from: startComponents)!...calendar.date(from: endComponents)!
-    } () //() 이게 있어서 선언하자마자 바로 실행됨
-    
-    let formatter = DateFormatter() //얘는 함수 아니고 객체임 밑의 정보들을 가지고 있는,,
-    
     var body: some View {
         VStack  {
-            Text("Chosen data = \(formatter.string(from: myDate))")
-                .padding()
-            DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date")} )
-                .datePickerStyle(.graphical)
-                .padding()
-        } .onAppear() {
-            formatter.locale = Locale(identifier: "ko_KR")
-            formatter.dateStyle = .full
         }
     }
 }
