126defedba974423ab6cba4758d6d5bd9b970fe0
DopeKS <kyusang.dev@gmail.com>
2024-04-09 10:18:35 +0900

    dismissKeyboard

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,58 +8,32 @@
 
 import SwiftUI
 
-/// GPT가 알려준 건데.. 복습할 때, 뜯어보기~
-struct LimitInputModifier: ViewModifier {
-    let limit: Int
-    
-    func body(content: Content) -> some View {
-        content
-            .onReceive(NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification)) { notification in
-                if let textField = notification.object as? UITextField, let text = textField.text, text.count > limit {
-                    textField.text = String(text.prefix(limit))
-                }
-            }
-    }
-}
-
-extension View {
-    func limitInput(to limit: Int) -> some View {
-        self.modifier(LimitInputModifier(limit: limit))
-    }
-}
-
 struct ContentView: View {
     @State private var message = ""
     @State private var password = ""
-    @State private var email = ""
-    
-    // Simple email validation, 이렇게 하는건 아닐 듯.. (방법 생각해보기)
-    var isEmailValid: Bool {
-            return email.contains("@") && email.contains(".")
-        }
-        
-    
+    @FocusState var dismissKeyboard: Bool
+
     var body: some View {
         VStack {
             TextField("Placeholder text", text: $message)
-                .limitInput(to: 10) // 글자 수 제한
                 .textFieldStyle(.roundedBorder)
                 .autocorrectionDisabled(true)
                 .textContentType(.emailAddress)
                 .submitLabel(.done)
                 .padding()
+                .focused($dismissKeyboard)
             
             SecureField("Password", text: $password)
                 .textFieldStyle(.roundedBorder)
                 .padding()
-                
-            TextField("Placeholder text", text: $email)
-                .textFieldStyle(.roundedBorder)
-                .autocorrectionDisabled(true)
-                .textContentType(.emailAddress)
-                .submitLabel(.done)
-                .padding()
-                .background(isEmailValid ? Color.green.opacity(0.3) : Color.red.opacity(0.3))
+                .focused($dismissKeyboard)
+            
+            TextEditor(text: $message)
+                .focused($dismissKeyboard)
+            
+            Button("Hine Keyboard") {
+                dismissKeyboard = false
+            }
         }
     }
 }
