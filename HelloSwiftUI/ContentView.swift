c9acb99dff35c45f870b76ad4ea270ac8276b0d4
DopeKS <kyusang.dev@gmail.com>
2024-04-09 16:26:13 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,30 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    var myArray = ["Cat", "Dog", "Turtle", "Ferret", "Parrot", "Goldfish", "Lizard", "canary", "Tatantula", "Hamster"]
-    
     var body: some View {
         VStack {
-            List {
-                Text("Car")
-                Text("Dog")
-                Text("Bird")
-                Text("Reptile")
-                Text("Fish")
-            }
-            List {
-                ForEach(1...25, id: \.self) { index in
-                    Text("Animal #\(index)")
-                }
-            }
-            List {
-                ForEach(0...myArray.count-1, id: \.self) { index in
-                    Text(myArray[index])
-                }
-            }
+            
         }
     }
 }
+
 
 #Preview {
     ContentView()
