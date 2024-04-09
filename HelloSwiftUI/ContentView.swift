5f6f1cf586839d7d75b347fe222a384ca7c98136
Hyungjun Kim <hjhj919@naver.com>
2024-04-09 13:20:11 +0900

    slider

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -18,6 +18,8 @@ extension Color {
 struct ContentView: View {
     @State var myToggle = true
     @State var newValue = 0
+    @State var sliderValue = 0.0
+
     
     var body: some View {
         VStack {
@@ -34,6 +36,9 @@ struct ContentView: View {
                 Text("Stepper value = \(newValue)")
             }
             .padding()
+            Slider(value: $sliderValue, in: 1...50, step: 4)
+                            .padding()
+                        Text("Slider value = \(sliderValue)")
         }
     }
    
