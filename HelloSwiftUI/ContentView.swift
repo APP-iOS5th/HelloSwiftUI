23e3b38f3d8f4ffc31347d197b5f77dc19eba485
seonggeun an <eventscripts@nate.com>
2024-04-09 15:08:12 +0900

    Alert(Deprecated)

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showAlert = false
+    
     var body: some View {
         VStack {
-           
+            Button("Show Alert"){
+                showAlert.toggle()
+            }
+            .alert(isPresented: $showAlert, content: {
+                Alert(title: Text("Warning!"), message: Text("Zombies on the loose"),
+                      dismissButton: .default(Text("OK"))
+                )
+            })
         }
     }
 }
