65138ec15a904a28c36ff8be0ac17d8f1ee6e810
Min-Gyeong Hwang <freemin217@gmail.com>
2024-04-09 10:32:25 +0900

    Picker

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,20 +11,30 @@ extension Color{
     static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
 }
 
+@available(iOS 15.0, *)
 struct ContentView: View {
-    @State private var message = ""
+    @State private var choice = 0
+    @State private var myColor = Color.red
+    @State private var myDate = Date.now
+
     var body: some View {
         VStack {
-            TextField("Placeholder text", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .disableAutocorrection(true)//자동 완성 끄기
-                .textContentType(.emailAddress)//자판기 스타일 지정
-                .submitLabel(.done)
-                .padding()
+            Picker(selection: $choice, label: Text("Picker")){
+                Text("Bird").tag(1.7)
+                Text("Cat").tag(2.06)
+                Text("Lizard").tag(3.41)
+                Text("Dog").tag(4.13)
+                Text("Hamster").tag(5.28)
+            }.pickerStyle(SegmentedPickerStyle())
+            Text("You chose \(choice)")
             
-            SecureField("Password", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .padding()
+            ColorPicker("Pick a color", selection: $myColor)
+            Rectangle()
+                .frame(width: 200, height: 150)
+                .foregroundColor(myColor)
+            
+            DatePicker(selection: $myDate, label: {Text("Date")})
+                .datePickerStyle(.graphical)
         }
     }
 }
