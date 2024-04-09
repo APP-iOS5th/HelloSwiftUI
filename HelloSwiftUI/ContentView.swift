90d74b1c70e5edf4efed21e38ce3d07e848b30b4
DopeKS <kyusang.dev@gmail.com>
2024-04-09 10:19:24 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,31 +9,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = ""
-    @State private var password = ""
-    @FocusState var dismissKeyboard: Bool
-
     var body: some View {
         VStack {
-            TextField("Placeholder text", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .autocorrectionDisabled(true)
-                .textContentType(.emailAddress)
-                .submitLabel(.done)
-                .padding()
-                .focused($dismissKeyboard)
             
-            SecureField("Password", text: $password)
-                .textFieldStyle(.roundedBorder)
-                .padding()
-                .focused($dismissKeyboard)
-            
-            TextEditor(text: $message)
-                .focused($dismissKeyboard)
-            
-            Button("Hine Keyboard") {
-                dismissKeyboard = false
-            }
         }
     }
 }
