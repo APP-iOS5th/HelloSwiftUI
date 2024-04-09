3a5f5d23fdf06b516e1f0cfe93b43777bcc173d6
Hyungjun Kim <hjhj919@naver.com>
2024-04-09 10:46:40 +0900

    picker

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,24 +11,34 @@ import SwiftUI
 
 import SwiftUI
 
+
 struct ContentView:View {
-    @State private var message = ""
+    @State private var choice = 0.0
+    @State private var myColor = Color.red
+    @State var myDate = Date.now
     
     var body: some View {
-        VStack {
-            TextField("이메일을 입력하세요", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .disableAutocorrection(true)
-                .textContentType(.emailAddress)
-                .submitLabel(.done)
-                .padding()
-            
-            SecureField("Password", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .padding()
-            
-        }
+        Picker(selection: $choice, label: Text("Picker")) {
+            Text("Bird").tag(1.7)
+            Text("Cat").tag(2.06)
+            Text("Human").tag(3.4)
+            Text("Dog").tag(4.13)
+            Text("Leeseokyung").tag(5.28)
+            Text("Dodo").tag(6.2)
+            Text("Hamster").tag(7.2)
+        }.pickerStyle(SegmentedPickerStyle())
+        Text("You chose \(choice)")
+        
+        ColorPicker("Pick a color", selection: $myColor)
+        Rectangle()
+            .frame(width: 200, height: 150)
+            .foregroundStyle(myColor)
+        
+        DatePicker(selection: $myDate, label: { Text("Date")})
+            .datePickerStyle(.graphical)
     }
+    
+    
 }
 
 #Preview {
