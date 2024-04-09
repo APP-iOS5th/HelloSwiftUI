a14d673a0d6e710526b5cacba99c931b84a62d22
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-09 10:45:48 +0900

    various picker

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,33 +7,37 @@
 
 import SwiftUI
 
+enum Animal {
+    case bird
+    case cat
+    case lizard
+}
+
 extension Color {
     static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
 }
+
 struct ContentView: View {
-    @State private var message = ""
-    @FocusState var dismissKeyboard: Bool
+    @State private var choice: Animal = .bird
+    @State private var myColor = Color.red
+    @State var myDate = Date.now
     
     var body: some View {
         VStack {
-            TextField("Placeholder text", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .autocorrectionDisabled(true)
-                .textContentType(.emailAddress)
-                .submitLabel(.done)
-                .padding()
-                .focused($dismissKeyboard)
+            Picker(selection: $choice, label: Text("Picker")) {
+                Text("Bird").tag(Animal.bird)
+                Text("Cat").tag(Animal.cat)
+                Text("Lizard").tag(Animal.lizard)
+            }.pickerStyle(SegmentedPickerStyle())
+            Text("You choose \(choice)")
             
-            SecureField("Password", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .padding()
+            ColorPicker("Pick a color", selection: $myColor)
+            Rectangle()
+                .frame(width: 200, height: 150)
+                .foregroundColor(myColor)
             
-            TextEditor(text: $message)
-                .focused($dismissKeyboard)
-            
-            Button("Hide Keyboard") {
-                dismissKeyboard = false
-            }
+            DatePicker(selection: $myDate, label: {Text("Date")})
+                .datePickerStyle(.wheel)
         }
     }
 }
