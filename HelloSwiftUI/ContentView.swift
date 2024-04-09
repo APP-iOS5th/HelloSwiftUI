d86e51441eb5b269c08748bf1a5708edbe389e39
Hyungjun Kim <hjhj919@naver.com>
2024-04-09 09:19:30 +0900

    picker

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,40 +7,19 @@
 
 import SwiftUI
 
-extension Color {
-    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
-}
-
 
 
 struct ContentView: View {
-    @State var colorMe = false
+    @State private var selectedColor = Color.gray
     var body: some View{
-        VStack {
-            Rectangle()
-                .fill(colorMe ? Color.green : Color.red)
-                .frame(width: 80, height: 80)
-            Button("클릭") {
-                colorMe.toggle()
-            }
-            Button{
-                colorMe.toggle()
-            } label: {
-                Text("클릭해")
-                    .font(.largeTitle)
-                    .foregroundColor(.green)
-                    .padding()
-                    .border(Color.black)
-            }
-            Button{
-                colorMe.toggle()
-            } label: {
-                Image("IMG_1608")
-                    .resizable()
-                    .frame(width: 100, height: 150)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
-            }
+        VStack(spacing: 28) {
+            Rectangle().fill(selectedColor)
+            Picker("Favorite Color", selection: $selectedColor, content: {
+                Text("Red").tag(Color.red)
+                Text("Green").tag(Color.green)
+                Text("blue").tag(Color.blue)
+            })
+            .pickerStyle(SegmentedPickerStyle())
         }
     }
 }
