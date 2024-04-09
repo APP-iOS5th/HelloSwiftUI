1cd45985a80ad60f849e5d40c05d85a368ad25e9
seonggeun an <eventscripts@nate.com>
2024-04-09 16:45:09 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,36 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    struct Animal: Identifiable{
-        var id = UUID()
-        var name: String
-    }
-    
-    struct AnimalCategory: Identifiable {
-        var id = UUID()
-        var category: String
-        var animals: [Animal]
-    }
-    
-    let categories = [
-        AnimalCategory(category: "포유류", animals: [Animal(name:"고양이"),Animal(name:"강아지")]),
-        AnimalCategory(category: "파충류", animals: [Animal(name:"거북이"),Animal(name:"도마뱀")]),
-    ]
-    
-    
     var body: some View {
         VStack {
-            List {
-                //카테고리 에 따라 섹션을 나눈다 (id: \.id 는 UUID Hashable하고 유니크한 값이기때문에 생략가능)
-                ForEach(categories, id: \.id) { c in
-                    Section(header: Text(c.category)){
-                        //카테고리에 맞는 리스트값을 출력
-                        ForEach(c.animals, id: \.id ) { animal in
-                            Text(animal.name)
-                        }
-                    }
-                }
-            }
+           
         }
     }
 }
