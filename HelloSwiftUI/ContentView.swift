dc9c35a0a8f23c33ce2f6fb7354d5b7fa9eb2b76
SungWoon Lee <lsw.netline@gmail.com>
2024-04-09 11:30:41 +0900

    한글화, 년월일

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,28 +11,31 @@ import SwiftUI
 @available(iOS 15.0, *)  //ios 15 이상에서 가능.
 struct ContentView: View {
     
-    @State private var choice = 0
-    @State private var myColor = Color.red
+
     @State private var myDate = Date.now
+    let dateRange : ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let startComponents = DateComponents(year: 2024, month: 3, day:25)
+        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
+        return calendar.date(from: startComponents)! ... calendar.date(from: endComponents)!
+        
+    }()
+    let formmatter = DateFormatter()
     
     var body: some View {
         VStack {
-            Picker(selection: $choice, label: Text("Picker")) {
-                Text("Bird").tag(1)
-                Text("Cat").tag(2)
-                Text("Lizard").tag(3)
-                Text("Dog").tag(4)
-                Text("Hamster").tag(5)
-                
-                
-                
-            }.pickerStyle(SegmentedPickerStyle())
-            Text("You chose \(choice)")
-            ColorPicker("Pick a color", selection: $myColor)
-            Rectangle()
-                .frame(width: 200, height: 150)
-                .foregroundColor(myColor)
-            DatePicker(selection: $myDate, label: { Text("Date")}).datePickerStyle(.graphical)
+            
+            Text("Chosen date = \(formmatter.string(from:myDate))")
+                .padding()
+            DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: {Text("Date")})
+                .datePickerStyle(.graphical)
+                .padding()
+            
+        }.onAppear() {
+            formmatter.locale = Locale(identifier: "ko_KR")
+//            formmatter.dateStyle = .full
+            formmatter.dateStyle = .long
+            formmatter.timeStyle = .medium
         }
     }
 }
