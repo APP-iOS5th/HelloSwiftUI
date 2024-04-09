7084c1072b9232874705c946a6294884d0831a33
seonggeun an <eventscripts@nate.com>
2024-04-09 10:18:04 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,38 +7,13 @@
 
 import SwiftUI
 
-@available(iOS 15.0, *)
+
 struct ContentView: View {
-    @State private var message = ""
-    @FocusState var dismissKeyboard: Bool
+    
     
     var body: some View {
         VStack {
-            //일반텍스트상자처럼 공개된 텍스트박스
-            TextField("Placeholder text", text: $message)
-                .textFieldStyle(.roundedBorder)
-                //(철자,문자)자동수정 옵션
-                .disableAutocorrection(true)
-                //입력창의 방식 제한(아이디,비밀번호,이메일)
-                .textContentType(.emailAddress)
-                //.keyboardType(입력방식)
-                //.default, .emailAddress, .decimalPad, numbersAnadPunctuation
-                //입력을 완료했을때 Enter키 변경
-                .submitLabel(.done)
-                .padding()
-                .focused($dismissKeyboard)
             
-            //비밀번호창처럼 비공개 텍스트박스
-            SecureField("Password", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .padding()
-            
-            //텍스트 에디터
-            TextEditor(text: $message)
-            
-            Button("Hide Keyboard") {
-                dismissKeyboard = false
-            }
         }
     }
 }
