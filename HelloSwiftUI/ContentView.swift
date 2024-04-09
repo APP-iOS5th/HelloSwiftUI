1a6df1a60ee5a7d738e3609b47fc32e73ec8949b
YuleGlycerine <figurefig13@gmail.com>
2024-04-09 16:45:24 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,35 +13,11 @@ extension Color {
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-    struct Animal: Identifiable {
-        var id = UUID()
-        var name: String
-    }
-    struct AnimalCategory: Identifiable {
-        var id = UUID()
-        var category: String
-        var animals: [Animal]
-    }
-    
-    let categories = [AnimalCategory(category: "포유류", animals: [Animal(name: "고양이"),Animal(name: "강아지")]),
-                      AnimalCategory(category: "파충류", animals: [Animal(name: "거북이"),Animal(name: "도마뱀")]),
-    ]
-    
-    
-    
+   
     
     var body: some View {
         VStack {
-            List {
-                ForEach(categories, id: \.id) { category in
-                    Section(header: Text(category.category)) {
-                        
-                        ForEach(category.animals, id: \.id) { animal in
-                            Text(animal.name)
-                        }
-                    }
-                }
-            }
+          
         }
     }
 }
