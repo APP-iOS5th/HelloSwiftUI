396ed07b9c94a4d684206e0050ff4bef1dab45c5
wotjs020708 <wotjs020708@gmail.com>
2024-04-09 10:12:13 +0900

    dismissKeyboard

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -6,20 +6,26 @@
 //
 
 import SwiftUI
-
+@available(iOS 15.0, *)
 struct ContentView: View {
     @State private var message = ""
+    @FocusState var dismissKeyboard: Bool
     var body: some View {
         VStack{
             TextField("Placeholder text", text: $message)
                 .textFieldStyle(.roundedBorder)
-                .disableAutocorrection(true)
+                .autocorrectionDisabled(true)
                 .textContentType(.emailAddress)
                 .submitLabel(.done)
                 .padding()
+                .focused($dismissKeyboard)
+            
             SecureField("Password", text: $message)
                 .textFieldStyle(.roundedBorder)
                 .padding()
+            Button("Hide Keyboard"){
+                dismissKeyboard = false
+            }
             
         }
     }
