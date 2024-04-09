83630018c7485b739cdbbeecb90725a631405704
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-09 13:24:19 +0900

    Link Menu

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,10 +11,34 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State var message = ""
     
     var body: some View {
         VStack  {
+            Link(destination: /*@START_MENU_TOKEN@*/URL(string: "https://www.apple.com")!/*@END_MENU_TOKEN@*/, label: {
+                    Text("Apple")
+            })
+            
+            Text(message).padding()
+            
+            Menu("Options") {
+                Button("Open", action: openFile)
+                Button("Find", action: findFile)
+                Button("Delete...", action: deleteFile)
+            }
         }
+    }
+    
+    func openFile() {
+        message = "Open chosen"
+    }
+    
+    func findFile() {
+        message = "Find chosen"
+    }
+    
+    func deleteFile () {
+        message = "Delete chosen"
     }
 }
 
