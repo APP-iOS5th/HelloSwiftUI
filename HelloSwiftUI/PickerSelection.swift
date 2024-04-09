939fb65096cd10ae3aeeedac153b1bfb610300f1
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-04-09 11:00:57 +0900

    datePicker with Range

--- HelloSwiftUI/PickerSelection.swift
+++ HelloSwiftUI/PickerSelection.swift
@@ -13,24 +13,54 @@ struct PickerSelection: View {
     @State private var myColor = Color.red
     @State private var myDate = Date.now
     
+//    let dateRange = ClosedRange<Date> = {
+//        let calendar = Calendar.current
+//        let startComponents = DateComponents(year:2024,month: 3,day: 25)
+//        let endComponents = DateComponents(year: 2024,month: 9,day: 13)
+//        
+//        return calendar.date(from: startComponents)!
+//        ...
+//        calendar.date(from:endComponents)!
+//    }()
+
+    
+    
+    
+    
+    
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
+        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
+        return calendar.date(from: startComponents)!
+        ...
+        calendar.date(from:endComponents)!
+    }()
+    
+    
+    
+    
     var body: some View {
         VStack {
-            Picker(selection:$choice,label:Text("Picker")) {
-                Text("Bird").tag(1)
-                Text("Cat").tag(2)
-                Text("Lizard").tag(3)
-                Text("Dog").tag(4)
-                Text("Hamster").tag(5)
-            }.pickerStyle(SegmentedPickerStyle())
-            Text("You chose \(choice)")
+//            Picker(selection:$choice,label:Text("Picker")) {
+//                Text("Bird").tag(1)
+//                Text("Cat").tag(2)
+//                Text("Lizard").tag(3)
+//                Text("Dog").tag(4)
+//                Text("Hamster").tag(5)
+//            }.pickerStyle(SegmentedPickerStyle())
+//            Text("You chose \(choice)")
+//            
+//            ColorPicker("Pick a color", selection: $myColor)
+//            Rectangle()
+//                .frame(width: 200,height: 150)
+//                .foregroundColor(myColor)
             
-            ColorPicker("Pick a color", selection: $myColor)
-            Rectangle()
-                .frame(width: 200,height: 150)
-                .foregroundColor(myColor)
-            
-            DatePicker(selection: $myDate, label: {Text("Date")})
-                .datePickerStyle(.graphical)
+            Text("Chosen date = \(myDate)")
+                                        .padding()
+                        DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date") })
+                            .datePickerStyle(.graphical)
+                            .padding()
         }
     }
 }
