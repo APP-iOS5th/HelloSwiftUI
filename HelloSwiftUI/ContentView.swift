f9d00beb8aad3f3586e2b4e98fc4fcce68f34cad
Jin Park <jinpark1504@gmail.com>
2024-04-09 13:43:51 +0900

    Link and Menu

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,28 +7,35 @@
 
 import SwiftUI
 
-@available(iOS 15.0, *)
 struct ContentView: View {
-    @State private var myToggle = true
-    @State private var newValue = 1
-    @State private var sliderValue = 0.0
+    @State var message = ""
     var body: some View {
         VStack {
-            Toggle(isOn: $myToggle) {
-                Text(myToggle ? "Orange" : "Green")
-            }
-            .padding()
-            Rectangle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(myToggle ? .orange : .green)
-            Stepper(value: $newValue, in: 1...10) {
-                Text("Stepper value = \(newValue)")
-            }
-            .padding()
-            Slider(value: $sliderValue, in: 0...10, step: 1)
-                .padding()
-            Text("Slider value = \(sliderValue)")
+            Link(destination: URL(string: "https:/apple.com")!, label: {
+                Text("Link")
+            })
+            Text(message).padding()
+            Menu("Options") {
+                Button("Open", action: openFile)
+                Button("Find", action: findFile)
+                Button("Delete", action: deleteFile)
+                Menu("Submenus") {
+                    Button("Clear", action: clearFile)
+                }
+            }.menuOrder(.fixed)
         }
+    }
+    func openFile() {
+        message = "Open chosen"
+    }
+    func findFile() {
+        message = "Find chosen"
+    }
+    func deleteFile() {
+        message = "Delete chosen"
+    }
+    func clearFile() {
+        message = "Clear chosen"
     }
 }
 
