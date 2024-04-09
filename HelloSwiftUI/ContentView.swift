574684ca01ca08abf1d7e151bd39b8d3c6ea3c03
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 11:48:32 +0900

    Toggle Stepper

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State var myToggle = true
+    @State var newValue = 0
+    
     var body: some View {
         VStack {
-
+            Toggle(isOn: $myToggle){
+                Text(myToggle ? "Orange" : "Green")
+            }.padding()
+            
+            Rectangle()
+                .frame(width: 200, height: 150)
+                .foregroundColor(myToggle ? .orange : .green)
+            
+            Stepper(value: $newValue, in: 0...10) { //in여기서는 범위 한정자
+                Text("Stepper value = \(newValue)")
+            }
+            .padding()
         }
     }
 }
