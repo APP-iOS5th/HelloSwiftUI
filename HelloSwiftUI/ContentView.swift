297d6ae6f2e142f4ea51da209e6b6273adeab668
Jin Park <jinpark1504@gmail.com>
2024-04-09 10:44:15 +0900

    Picker, ColorPicker, and DatePicker

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,25 +9,31 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-    @State private var message = ""
-    @FocusState var dismissKeyboard: Bool
+    @State private var choice = 0.0
+    @State private var myColor = Color.red
+    @State private var myDate = Date.now
     var body: some View {
         VStack {
-            TextField("Placeholder", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .autocorrectionDisabled(false)
-                .textContentType(.URL)
-                .submitLabel(.go)
-                .padding()
-                .focused($dismissKeyboard)
-            SecureField("Password", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .padding()
-            TextEditor(text: $message)
-                .padding()
-            Button("Hide Keyboard") {
-                dismissKeyboard = false
+//            Picker(selection: $choice, label: Text("Picker")) {
+            Picker("Picker", selection: $choice) {
+                Text("Bird").tag(1.7)
+                Text("Cat").tag(2.06)
+                Text("Lizard").tag(3.41)
+                Text("Dog").tag(4.13)
+                Text("Hamster").tag(5.28)
             }
+            .pickerStyle(SegmentedPickerStyle())
+            Text("You chose \(choice)")
+            
+//            ColorPicker("Pick a color", selection: $myColor)
+            ColorPicker(selection: $myColor, label: { Text("Pick a color")})
+            Rectangle()
+                .frame(width: 200, height: 200)
+                .foregroundColor(myColor)
+            
+            DatePicker(selection: $myDate, label: { Text("Date") })
+//            DatePicker("Date", selection: $myDate)
+                .datePickerStyle(.compact)
         }
     }
 }
