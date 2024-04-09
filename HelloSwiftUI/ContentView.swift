a89cb3c8310eb759f757b52f6c85648a70e0740a
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 16:03:46 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,35 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myColor = Color.gray
     var body: some View {
         VStack {
-            Rectangle()
-                .foregroundColor(myColor)
-            
-            Text("Pick a color")
-                .padding()
-                .contextMenu { //길게눌러야함
-                    Button("Red", action:  {
-                        myColor = Color.red
-                    })
-                    Button("Puple", action: purple)
-                    Button("Green", action: green)
-                    Button("Orange", action: orange)
-                }
+
 
          }
      }
-    
-    func purple() {
-        myColor = Color.purple
-    }
-    func green() {
-        myColor = Color.green
-    }
-    func orange() {
-        myColor = Color.orange
-    }
  }
 
 #Preview {
