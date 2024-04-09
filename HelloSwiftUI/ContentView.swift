feb8ac833ff425a3224760e6bedef595bc933805
YuleGlycerine <figurefig13@gmail.com>
2024-04-09 10:43:55 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -19,22 +19,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack  {
-            Picker(selection: $choice, label: Text("Picker"))  {
-                Text("Bird").tag(1.7)
-                Text("Cat").tag(2.06)
-                Text("Lizard").tag(3.41)
-                Text("Dog").tag(4.13)
-                Text("Hamster").tag(5.2)
-            }.pickerStyle(SegmentedPickerStyle())
-            Text("You chose \(choice)")
             
-            ColorPicker("Pick a color", selection: $myColor)
-            Rectangle()
-                .frame(width: 200, height: 150)
-                .foregroundColor(myColor)
-            
-            DatePicker(selection: $myDate, label: { Text("Data") })
-                .datePickerStyle(.graphical)
         }
     }
 }
