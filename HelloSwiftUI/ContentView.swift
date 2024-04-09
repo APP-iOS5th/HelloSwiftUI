75cc558d921bac59a11480842158ab52d7980932
DopeKS <kyusang.dev@gmail.com>
2024-04-09 10:44:27 +0900

    Pickers

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,8 +9,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var intChoice1 = 0
+    @State private var intChoice2 = 0
+    @State private var floatChoice = 0.0
+    @State private var myColor = Color.green
+    @State var myDate = Date.now
+    
     var body: some View {
         VStack {
+            Spacer()
+            
+            HStack {
+                Picker(selection: $intChoice1, label: Text("Picker")) {
+                    Text("Bird").tag(1)
+                    Text("Cat").tag(2)
+                    Text("Lizard").tag(3)
+                    Text("Dog").tag(4)
+                    Text("Hamster").tag(5)
+                }
+                Text("You choose \(intChoice1)")
+                Spacer()
+                
+                Picker(selection: $floatChoice, label: Text("Picker")) {
+                    Text("Bird").tag(1.4)
+                    Text("Cat").tag(2.24)
+                    Text("Lizard").tag(3.76)
+                    Text("Dog").tag(4.73)
+                    Text("Hamster").tag(5.75)
+                }
+                Text("You choose \(String(format: "%.2f", floatChoice))")
+            }
+            Spacer()
+            
+            Picker(selection: $intChoice2, label: Text("Picker")) {
+                Text("Bird").tag(1)
+                Text("Cat").tag(2)
+                Text("Lizard").tag(3)
+                Text("Dog").tag(4)
+                Text("Hamster").tag(5)
+            }.pickerStyle(SegmentedPickerStyle())
+            Text("You choose \(intChoice2)")
+            Spacer()
+            
+            ColorPicker("Pick a color", selection: $myColor)
+            Rectangle()
+                .frame(width: 250, height: 80)
+                .foregroundColor(myColor)
+            Spacer()
+            
+            DatePicker(selection: $myDate, label: { Text("Date") })
+                .datePickerStyle(.graphical)
+            Spacer()
             
         }
     }
