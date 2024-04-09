e205e26243a0da56cf392169945a197396deb403
jihye <ganzi410@gmail.com>
2024-04-09 16:45:18 +0900

    List with Section

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,27 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var myArray = ["Cat", "Dog", "Turtle", "Ferret", "Parrot", "Goldfish", "Lizard", "Canary", "Tarantula", "Hamster"]
+    struct Animal: Identifiable {
+        var id = UUID()
+        var name: String
+    }
+    
+    struct AnimalCategory: Identifiable {
+        var id = UUID()
+        var category: String
+        var animals:[Animal]
+    }
+    
+    let categories = [
+        AnimalCategory(category: "포유류", animals: [Animal(name: "고양이"),Animal(name: "강아지")]),
+        AnimalCategory(category: "파충류", animals: [Animal(name: "거북이"),Animal(name: "도마뱀")]),
+    ]
     
     var body: some View {
         VStack {
             List {
-                Text("Dog")
-                Text("Bird")
-                Text("Lizard")
-                Text("Cat")
-                Text("Fish")
-            }
-            
-            List {
-                ForEach(1...25, id: \.self) { index in
-                    Text("Animal #\(index)")
-                }
-            }
-            
-            List {
-                ForEach(0...myArray.count - 1, id: \.self) { index in
-                    Text(myArray[index])
+                ForEach(categories) { c in
+                    Section(header: Text(c.category)) {
+                        ForEach(c.animals) { animal in
+                            Text(animal.name)}
+                    }
                 }
             }
         }
