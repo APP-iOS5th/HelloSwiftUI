e806c03d2c4582f507d49421d3ebd74774ca4267
yehjinjang <j95033176@gmail.com>
2024-04-09 10:16:32 +0900

    0409

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -34,6 +34,8 @@ struct ContentView: View{
                 .textFieldStyle(.roundedBorder)
                 .padding()
             
+            TextEditor(text: $email)
+            
             Button("Hide Keyboard"){
                 dismissKeyboard = false
             }
