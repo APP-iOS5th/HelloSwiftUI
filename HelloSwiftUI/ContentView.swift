8cd97c74c5aab0b73f54751c599ccc82398c598e
YuleGlycerine <figurefig13@gmail.com>
2024-04-09 16:18:24 +0900

    list

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,12 +13,27 @@ extension Color {
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-  
-    
+  var myArray = ["Cat", "Dog", "Bird", "Reptile", "Fish", "Cat", "Dog", "Bird", "Reptile", "Fish"]
     var body: some View {
         VStack {
-            
+            List {
+                Text("Cat")
+                Text("Dog")
+                Text("Bird")
+                Text("Reptile")
+                Text("Fish")
+            }
+            List {
+                ForEach(1...25, id: \.self) { index in
+                    Text("Animal #\(index)")
+                }
+            }
+            List {
+                ForEach(0...myArray.count - 1, id: \.self) { index in Text(myArray[index])
+                }
+            }
         }
+    
     }
 }
 
