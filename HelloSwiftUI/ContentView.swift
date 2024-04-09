3c76536e538b9061213afcdf810997efb8e059db
YuleGlycerine <figurefig13@gmail.com>
2024-04-09 14:07:01 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,40 +13,12 @@ extension Color {
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-    @State var changeMe1 = false
-    @State var changeMe2 = false
-    @State var changeMe3 = false
-    @State var message = ""
-    
+   
     var body: some View {
-        VStack {
-            
-            Rectangle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe1 ? .red : .yellow)
-                .onTapGesture {
-                    changeMe1.toggle()
-                }
-            
-            Rectangle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe2 ? .red : .yellow)
-                .onTapGesture(count: 2) {
-                    changeMe2.toggle()
-                }
-            
-            Text(message).padding()
-            
-            Rectangle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe3 ? .red : .yellow)
-                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: { stillPressed in
-                    message = "Long press in progress: \(stillPressed)"
-                }) {
-                    changeMe3.toggle()
-                }
-            
-            
+        
+        
+        
+        
         }
     }
 }
