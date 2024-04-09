ef5e1b134dc8908c5a6fa6ee5257bab88a8ee537
MadCow <lobasketve@naver.com>
2024-04-09 15:35:24 +0900

    comfirmationDialog & alert

--- HelloSwiftUI/AlertExample.swift
+++ HelloSwiftUI/AlertExample.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AlertExample: View {
     
     @State private var showAlert: Bool = false
-    @State private var secondAlert: Bool = false
+//    @State private var secondAlert: Bool = false
     
     var body: some View {
         VStack {
@@ -19,25 +19,34 @@ struct AlertExample: View {
             } label: {
                 Text("알림!")
             }
-            .alert("Zombie 어쩌구 저쩌구", isPresented: $showAlert) {
-                Button {
-                    self.secondAlert.toggle()
+            // MARK: - 여러개의 버튼들 알림처럼 표시
+            .confirmationDialog("Warning!", isPresented: $showAlert, titleVisibility: .visible) {
+                Button(role: .none) {
+                    print("option 1 Selected")
                 } label: {
-                    Text("First Alert")
+                    Text("option 1")
                 }
-//                MARK: alert 중첩은 안되네..
-//                .alert("무한 알람 ㄷㄷ", isPresented: $secondAlert) {
-//                    Button {
-//                        
-//                    } label: {
-//                        Text("Second Alert")
-//                    }
-//                } message: {
-//                    Text("Stop Alert")
-//                }
+                
+                Button(role: .destructive) {
+                    print("option 2 Selected")
+                } label: {
+                    Text("option 2")
+                }
             } message: {
-                Text("Zombie ....")
+                Text("Zombies on the loose")
             }
+            
+            // MARK: - Alert -> 중첩 alert는 안되더라
+//            .alert("Zombie 어쩌구 저쩌구", isPresented: $showAlert) {
+//                Button {
+//                    
+//                } label: {
+//                    Text("First Alert")
+//                }
+//            } message: {
+//                Text("Zombie ....")
+//            }
+            
             // MARK: Deprecated version
 //            .alert(isPresented: $showAlert, content: {
 //                Alert(title: Text("알림!"), message: Text("Zombies on the loose"), dismissButton: .default(Text("OK")))
