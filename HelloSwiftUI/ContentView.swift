45fc82ccb63cb79dfe87f6b14a42498b68e6d8ce
ksiomng0929 <nadana0929@gmail.com>
2024-04-09 15:46:04 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,24 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var showAlert = false
     
     var body: some View {
         VStack {
-            Text("Turtle Rock")
-                .padding()
-                .contextMenu {
-                    Button {
-                        
-                    } label: {
-                        Label("Add to Favorite", systemImage: "heart")
-                    }
-                    Button {
-                        
-                    } label: {
-                        Label("Show in Maps", systemImage: "star")
-                    }
-                }
+         
         }
     }
 }
