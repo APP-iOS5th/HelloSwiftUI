297b5c9360fee27a365967acd9732d8886fb7fd8
Jihye Park <deviloper.park@gmail.com>
2024-04-09 13:37:15 +0900

    Menu Ordering

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,17 +12,19 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Link(destination: URL(string: "https://www.apple.com")!, label: {
-                Text("Apple")
-            })
-            
-            Text(message).padding()
-            
-            Menu("Options") {
+            Menu("Options") { // 메뉴가 펼쳐지는 방향으로 쌓임
                 Button("Open", action: openFile)
                 Button("Find", action: findFile)
                 Button("Delete...", action: deleteFile)
             }
+            .menuStyle(DefaultMenuStyle())
+            .menuOrder(.fixed) // 글자 쌓이는 방향 고정
+            
+            Text(message).padding()
+            
+            Link(destination: URL(string: "https://www.apple.com")!, label: {
+                Text("Apple")
+            })
         }
     }
     
