7b95f61db112747719fcf35a6f1a4d0922b7efde
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 16:45:45 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,37 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-        struct Animal: Identifiable {
-            var id = UUID() //나혼자 쓰는 고유한 아이디를 만들어줌
-            var name: String
-        }
-        
-        struct AnimalCategory: Identifiable {
-            var id = UUID()
-            var category: String
-            var animals: [Animal]
-        }
-        
-        let categories = [
-            AnimalCategory(category: "포유류", animals:[Animal(name: "고양이"), Animal(name: "강아지")]),
-            AnimalCategory(category: "파충류", animals:[Animal(name: "거북이"), Animal(name: "도마뱀")]),
-        ]
-    
-    
-    
     var body: some View {
         VStack {
-            List {
-                ForEach(categories) { c in
-                    Section(header: Text(c.category)){
-                        ForEach(c.animals) { animal in
-                            Text(animal.name)
-                            
-                        }
-                    }
-                    
-                }
-            }
         }
     }
 }
