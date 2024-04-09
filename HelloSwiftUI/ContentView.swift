a8877c819848576c7a1f9177d5390354cf2995bd
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-09 10:32:19 +0900

    Picker

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -2,37 +2,33 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var message: String = ""
-    @State private var password: String = ""
-    @FocusState var dismissKeyboard: Bool
+    @State private var choice = 0.0
+    @State private var myColor = Color.red
+    @State private var myDate = Date.now
     var body: some View {
         VStack {
+            Picker(selection: $choice, label: Text("Picker")) {
+            Text("Bird").tag(1)
+            Text("Cat").tag(2)
+            Text("Liazrt").tag(3)
+            Text("Dog").tag(4)
+            Text("Hamster").tag(5)
+            }.pickerStyle(SegmentedPickerStyle())
+        Text("You chose \(choice)")
             
-            TextField("Placeholder text", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .disableAutocorrection(true)
-                .textContentType(.emailAddress)
-                .padding()
-                .submitLabel(.done)
-                .focused($dismissKeyboard)
-            
-            
-            SecureField("Password", text: $password)
-                .textFieldStyle(.roundedBorder)
-                .padding()
-            
-            TextEditor(text: $message)
-            
-            Button("Hide Keyboard") {
-                dismissKeyboard = false
-            }
+            ColorPicker("Pick a color", selection: $myColor)
+            Rectangle()
+                .frame(width: 200, height: 150)
+                .foregroundStyle(myColor)
+            DatePicker(selection: $myDate, label: {Text("Date")})
+                .datePickerStyle(.graphical)
         }
     }
 }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
-    
+}
+
