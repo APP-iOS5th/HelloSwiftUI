9673eeb00718e8dac1cf65d19f6f4f44e00ea0b2
jihye <ganzi410@gmail.com>
2024-04-09 17:08:44 +0900

    Basic Form

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,32 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    struct Animal: Identifiable {
-        var id = UUID()
-        var name: String
-    }
     
-    struct AnimalCategory: Identifiable {
-        var id = UUID()
-        var category: String
-        var animals:[Animal]
-    }
-    
-    let categories = [
-        AnimalCategory(category: "포유류", animals: [Animal(name: "고양이"),Animal(name: "강아지")]),
-        AnimalCategory(category: "파충류", animals: [Animal(name: "거북이"),Animal(name: "도마뱀")]),
-    ]
+    @State var messageOne = ""
+    @State var messageTwo = ""
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(categories) { c in
-                    Section(header: Text(c.category)) {
-                        ForEach(c.animals) { animal in
-                            Text(animal.name)}
-                    }
-                }
+        VStack (spacing: 20) {
+            Form {
+                Text("This is the first form")
+                TextField("Type Here", text: $messageOne)
             }
+            
+            Form {
+                Text("This is the second form")
+                TextField("Type Here", text: $messageTwo)
+            }
+            Text("Form #1 : \(messageOne)")
+            Text("Form #2 : \(messageTwo)")
         }
     }
 }
