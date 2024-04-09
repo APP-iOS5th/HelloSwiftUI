460057402b6983ca99c2037d6756b2d13e96ae41
YuleGlycerine <figurefig13@gmail.com>
2024-04-09 17:00:43 +0900

    spacing 0

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,11 +13,22 @@ extension Color {
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-   
+    @State var messageOne = ""
+    @State var messageTwo = ""
+    
     
     var body: some View {
-        VStack {
-          
+        VStack (spacing: 0) {
+            Form {
+                Text("This is the first Form")
+                TextField("Type here", text: $messageOne)
+            }
+            Form {
+                Text("This is the second Form")
+                TextField("Type here", text: $messageTwo)
+            }
+            Text("Form #1 = \(messageOne)")
+            Text("Form #2 = \(messageTwo)")
         }
     }
 }
