d4da264ee2743905df39531fa6d3b6a8e6c1a063
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-09 11:00:28 +0900

    DatePicker with Range

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -2,26 +2,22 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var choice = 0.0
-    @State private var myColor = Color.red
-    @State private var myDate = Date.now
+    @State var myDate = Date.now
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
+        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
+        return calendar.date(from: startComponents)!...calendar.date(from: endComponents)!
+    }()
+    
     var body: some View {
         VStack {
-            Picker(selection: $choice, label: Text("Picker")) {
-            Text("Bird").tag(1)
-            Text("Cat").tag(2)
-            Text("Liazrt").tag(3)
-            Text("Dog").tag(4)
-            Text("Hamster").tag(5)
-            }.pickerStyle(SegmentedPickerStyle())
-        Text("You chose \(choice)")
-            
-            ColorPicker("Pick a color", selection: $myColor)
-            Rectangle()
-                .frame(width: 200, height: 150)
-                .foregroundStyle(myColor)
-            DatePicker(selection: $myDate, label: {Text("Date")})
-                .datePickerStyle(.graphical)
+            Text("Chosen date = \(myDate)")
+                .padding()
+            DatePicker(selection: $myDate, displayedComponents: [.date], label: {
+                Text("Date")})
+            .datePickerStyle(.graphical)
+            .padding()
         }
     }
 }
