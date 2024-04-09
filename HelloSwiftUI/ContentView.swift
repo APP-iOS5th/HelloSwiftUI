9ed84be1127b4615a3e2ebffdb74631bc2199089
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-09 14:34:17 +0900

    MagnificationGesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,35 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var message = ""
-    @State var changeMe1 = false
-    @State var changeMe2 = false
-    @State var changeMe3 = false
+    @State var tempValue: CGFloat = 0
+    @State var finalValue: CGFloat = 1
+    
     var body: some View {
         VStack {
-            Rectangle()
-                .frame(width: 100, height: 100)
-                .foregroundStyle(changeMe1 ? .orange : .primary)
-                .onTapGesture (count: 2){
-                    changeMe1.toggle()
-                }
-            Rectangle()
-                .frame(width: 100, height: 100)
-                .foregroundStyle(changeMe2 ? .orange : .primary)
-                .onTapGesture (count: 2){
-                    changeMe2.toggle()
-                }
-            
-            Text(message).padding()
-            
-            Rectangle()
-                .frame(width: 100, height: 100)
-                .foregroundStyle(changeMe3 ? .orange : .primary)
-                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: { stillPressed in
-                    message = "Long press in progress \(stillPressed)"
-                }){
-                    changeMe3.toggle()
-                }
+            Text("tempValue = \(tempValue)")
+            Spacer()
+            Image(systemName: "star.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .scaleEffect(finalValue + tempValue)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { amount in
+                            tempValue = amount - 1
+                        }
+                        .onEnded { amount in
+                            finalValue += tempValue
+                            tempValue = 0
+                        }
+                )
+            Spacer()
         }
     }
 }
