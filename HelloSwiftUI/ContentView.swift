5a46ac0a30f60e60d4b55154f1fadc64dc68b001
Hyungjun Kim <hjhj919@naver.com>
2024-04-09 09:38:11 +0900

    TextField

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,30 +11,22 @@ import SwiftUI
 
 import SwiftUI
 
-extension Color {
-    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
-}
-struct ContentView: View {
+struct ContentView:View {
     @State private var message = ""
     
     var body: some View {
-        VStack (spacing: 28){
-            Text(message)
-            Picker("Favorite Color", selection: $message, content: {
-                Text("Happy").tag("happy")
-                Text("Sad").tag("sad")
-                Text("Bored").tag("bored")
-            })
-            .pickerStyle(SegmentedPickerStyle())
-            .onChange(of: message) { newValue in
-                switch newValue {
-                case "happy": message = "Be happy and joyous"
-                case "sad": message = "Life can be a struggle at times"
-                case "bored": message = "Look for your purpose"
-                default:
-                    break
-                }
-            }
+        VStack {
+            TextField("이메일을 입력하세요", text: $message)
+                .textFieldStyle(.roundedBorder)
+                .disableAutocorrection(true)
+                .textContentType(.emailAddress)
+                .submitLabel(.done)
+                .padding()
+            
+            SecureField("Password", text: $message)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            
         }
     }
 }
