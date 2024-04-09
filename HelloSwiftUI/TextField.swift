72453b42257b82837da8e346c1841cee50985f82
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-04-09 10:16:34 +0900

    textEditor

--- HelloSwiftUI/TextField.swift
+++ HelloSwiftUI/TextField.swift
@@ -7,21 +7,33 @@
 
 import SwiftUI
 
+
 struct TextFieldView: View {
     @State private var message = ""
+    @FocusState var dismissKeyboard: Bool
     var body: some View {
         VStack {
             TextField("Placeholder",text: $message)
                 .textFieldStyle(.roundedBorder)
+                .autocorrectionDisabled(true)
                 .textContentType(.emailAddress)
                 .submitLabel(.done)
                 .padding()
                 .disableAutocorrection(true)
+                .focused($dismissKeyboard)
             
             
             SecureField("Password", text: $message)
                 .textFieldStyle(.roundedBorder)
                 .padding()
+            
+            
+            TextEditor(text: $message)
+            
+            
+            Button("Hide Keyboard") {
+                dismissKeyboard = false
+            }
             
         }
     }
