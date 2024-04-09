b8890fb8c6a567dc0b8a4ba2bb7b9bceb29c928f
Jin Park <jinpark1504@gmail.com>
2024-04-09 14:10:17 +0900

    TapGesture and LongPressGesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,34 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var changeMe1 = false
+    @State var changeMe2 = false
+    @State var changeMe3 = false
     @State var message = ""
     var body: some View {
         VStack {
-            Link(destination: URL(string: "https:/apple.com")!, label: {
-                Text("Link")
-            })
-            Text(message).padding()
-            Menu("Options") {
-                Button("Open", action: openFile)
-                Button("Find", action: findFile)
-                Button("Delete", action: deleteFile)
-                Menu("Submenus") {
-                    Button("Clear", action: clearFile)
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe1 ? .red: .yellow)
+                .onTapGesture {
+                    changeMe1.toggle()
                 }
-            }.menuOrder(.fixed)
+
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe2 ? .red: .yellow)
+                .onTapGesture(count: 2) {
+                    changeMe2.toggle()
+                }
+
+            Text(message).padding()
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe3 ? .red: .yellow)
+                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: { stillPressed in
+                        message = "Long press in progress: \(stillPressed)"
+                }) {
+                    changeMe3.toggle()
+                }
         }
-    }
-    func openFile() {
-        message = "Open chosen"
-    }
-    func findFile() {
-        message = "Find chosen"
-    }
-    func deleteFile() {
-        message = "Delete chosen"
-    }
-    func clearFile() {
-        message = "Clear chosen"
     }
 }
 
