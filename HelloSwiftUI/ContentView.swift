d7fad796f861fb33ad6dd4bb46193f7794fd556a
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-09 15:19:44 +0900

    Alert

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,14 +8,19 @@ struct ContentView: View {
             Button("Show Alert") {
                 showAlert.toggle()
             }
-            .alert(isPresented: $showAlert, content: {
-                Alert(title: Text("Warning!"), message: Text("Zombies on the loose"),
-                      dismissButton: .default(Text("OK"))
-                )
-            })
+            .alert("Warning!", isPresented: $showAlert) {
+                Button(role: .cancel) {
+                    print("button action")
+                } label: {
+                    Text("OK!")
+                }
+            } message: {
+                Text("Zombie")
+            }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
