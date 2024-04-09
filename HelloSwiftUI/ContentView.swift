6e175225855c2c30bfeec6dfb1b26221e612f148
YuleGlycerine <figurefig13@gmail.com>
2024-04-09 14:34:55 +0900

    magnificationG

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,12 +13,26 @@ extension Color {
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-   
+    @State private var tempValue: CGFloat = 0
+    @State private var finalValue: CGFloat = 1
+    
     var body: some View {
-        
-        
-        
-        
+        VStack {
+            Image(systemName: "star.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .scaleEffect(finalValue + tempValue)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { amount in
+                            tempValue = amount - 1
+                        }
+                        .onEnded { amount in
+                            finalValue += tempValue
+                            tempValue = 0
+                        }
+                )
         }
     }
 }
