59ab65b96b0a3d2cb044845c4f8be40f65373c04
jinwoong <baseball0020@gmail.com>
2024-04-09 11:49:21 +0900

    Toggle and stepper

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,43 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var choice = 0.0
-    @State private var myColor = Color.red
-    @State private var myDate = Date.now
-    
-    private let dateRange: ClosedRange<Date> = {
-        let calendar = Calendar.current
-        let start = DateComponents(year: 2024, month: 3)
-        let end = DateComponents(year: 2024, month: 5)
-        
-        return calendar.date(from: start)!...calendar.date(from: end)!
-    }()
+    @State private var myToggle = true
+    @State private var newValue = 1
     
     var body: some View {
         VStack {
-            Picker(selection: $choice) {
-                Text("Bird").tag(1.7)
-                Text("Cat").tag(2.7)
-            } label: {
-                Text("Picker")
+            Toggle(isOn: $myToggle) {
+                Text(myToggle ? "Orange" : "Green")
             }
-            .pickerStyle(.segmented)
-            Text("You choose \(choice)")
-            ColorPicker("Pick a color", selection: $myColor)
+            .padding()
             Rectangle()
                 .frame(width: 200, height: 150)
-                .foregroundStyle(myColor)
-//            DatePicker(selection: $myDate) {
-//                Text("Date")
-//            }
-//            .datePickerStyle(.graphical)
-            DatePicker(
-                "Date",
-                selection: $myDate,
-                in: dateRange,
-                displayedComponents: .date
-            )
-            .datePickerStyle(.graphical)
+                .foregroundStyle(myToggle ? .orange : .green)
+            Stepper(value: $newValue, in: 1...10) {
+                Text("Stepper Value = \(newValue)")
+            }
+            .padding()
         }
     }
 }
