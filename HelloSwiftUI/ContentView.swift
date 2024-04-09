8f296770e432fcfb8126f23ef31a844211413a7f
ksiomng0929 <nadana0929@gmail.com>
2024-04-09 17:04:29 +0900

    Form

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,10 +1,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var messageOne = ""
+    @State var messageTwo = ""
     
     var body: some View {
         VStack {
-            
+            Form {
+                Text("This is the first Form")
+                TextField("Type here", text: $messageOne)
+            }
+            .frame(height: 150)
+            Form {
+                Text("This is the first Form")
+                TextField("Type here", text: $messageTwo)
+            }
+            .frame(height: 150)
+            List {
+                Text("Form #1 \(messageOne)")
+                Text("Form #2 \(messageTwo)")
+            }
         }
     }
 }
