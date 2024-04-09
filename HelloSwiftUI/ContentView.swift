fbf5f6b5fab2d08cdbc10e19774760c35e1ec42e
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 11:07:19 +0900

    DatePicker With Range

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,9 +10,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State var myDate = Date.now
+    let dateRange: ClosedRange<Date> = {
+        let calendar =  Calendar.current
+        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
+        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
+        return calendar.date(from: startComponents)!...calendar.date(from: endComponents)!
+    }()
     var body: some View {
         VStack {
-
+            Text("Chosen date = \(myDate)")
+                .padding()
+            DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date") } )
+                .datePickerStyle(.graphical)
+                .padding()
         }
     }
 }
