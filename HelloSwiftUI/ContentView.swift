68adf13ea040038542674eca7116f2023a363e18
Hyunseo Yook <hhh3332@naver.com>
2024-04-09 14:05:22 +0900

    TapGesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,38 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State var changeme1 = false
+    @State var changeme2 = false
+    @State var changeme3 = false
     @State var message = ""
     
     var body: some View {
         VStack {
-            Link(destination: /*@START_MENU_TOKEN@*/URL(string: "https://www.apple.com")!/*@END_MENU_TOKEN@*/, label: {
-                Text("apple")
-            })
-            
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeme1 ? .red : .yellow)
+                .onTapGesture {
+                    changeme1.toggle()
+                 }
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeme2 ? .red : .yellow)
+                .onTapGesture(count: 2) {
+                    changeme2.toggle()
+                 }
             Text(message).padding()
-                        
-                        Menu("Options") {
-                            Button("Open", action: openFile)
-                            Button("Find", action: findFile)
-                            Button("Delete...", action: deleteFile)
-                        }
-                    }
-                }
-                
-                func openFile() {
-                    message = "Open chosen"
-                }
-                
-                func findFile() {
-                    message = "Find chosen"
-                }
-                
-                func deleteFile() {
-                    message = "Delete chosen"
-                }
-                
-            }
+            
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeme3 ? .red : .yellow)
+                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: {stillPressed in
+                                    message = "Long Press in progress: \(stillPressed)"}){
+                    changeme3.toggle()
+                 }
+        }
+    }
+}
 
 
 #Preview {
