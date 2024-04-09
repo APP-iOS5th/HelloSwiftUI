3f9c8ad478c71e33c3c5ccf5c3e5c4bba52aab79
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 14:07:05 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,35 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var message = ""
-    @State var changeMe1 = false
-    @State var changeMe2 = false
-    @State var changeMe3 = false
     var body: some View {
         VStack {
-            Rectangle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe1 ? .red : .yellow)
-                .onTapGesture {
-                    changeMe1.toggle()
-                }
-            
-            Rectangle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe2 ? .red : .yellow)
-                .onTapGesture(count: 2) { //클릭 2번하면 변함
-                    changeMe2.toggle()
-                }
-            
-            Rectangle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe3 ? .red : .yellow)
-                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: { //길게 누르면 동작
-                    stillPressed in
-                    message = "Long press in progress \(stillPressed)"
-                }) {
-                    changeMe3.toggle()
-                }
+
             
         }
     }
