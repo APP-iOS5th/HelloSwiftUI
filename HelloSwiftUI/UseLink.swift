42ce2f73b4aaf97ff14a5f7fe492a63f43b447ab
JongWon Jeong <jjwon214@gmail.com>
2024-04-09 13:22:23 +0900

    Link Menu

---
+++ HelloSwiftUI/UseLink.swift
@@ -0,0 +1,50 @@
+//
+//  UseLink.swift
+//  HelloSwiftUI
+//
+//  Created by 정종원 on 4/9/24.
+//
+
+import SwiftUI
+
+struct UseLink: View {
+    
+    @State var message = ""
+    
+    var body: some View {
+        
+        VStack {
+            
+            //Link
+            Link(destination: URL(string: "https://www.apple.com")!) {
+                Text("Apple")
+            }
+            
+            Text(message).padding()
+            
+            //Menu
+            Menu("Options") {
+                Button("Open", action: openFile)
+                Button("Find", action: findFile)
+                Button("Delete", action: deleteFile)
+            }
+            
+        }
+        
+    }
+    func openFile() {
+        message = "Open chosen"
+    }
+    
+    func findFile() {
+        message = "Find chosen"
+    }
+    
+    func deleteFile() {
+        message = "Delete chosen"
+    }
+}
+
+#Preview {
+    UseLink()
+}
