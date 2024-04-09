52a0209f06160603a4104eaca9836a9b9595eb41
seonggeun an <eventscripts@nate.com>
2024-04-09 17:18:44 +0900

    Section GroupBox

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,12 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var flag1 = false
+    @State var flag2 = false
+    @State var message = ""
+    
     var body: some View {
-        VStack {
-            
+        VStack(spacing: 5) {
+            Form{
+                Section {
+                    Text("This is Section has no header")
+                }
+                Section("Just a Header") {
+                    Text("This Section uses a simple header")
+                }
+                Section {
+                    Text("This Section uses a simple footer")
+                } footer: {
+                    Text("Just a Footer")
+                }
+                Section {
+                    Text("This Section uses both a header and footer")
+                } header: {
+                    Text("The Header")
+                } footer: {
+                    Text("The Footer")
+                }
+                
+                Form {
+                    Text("This is a Form")
+                    Toggle(isOn: $flag1, label: {
+                        Text("Click me")
+                    })
+                }
+                
+                GroupBox(label: Text("Group Box")) {
+                    Text("This is a Group box")
+                    Toggle(isOn: $flag2, label : {
+                        Text("Click me")
+                    })
+                    TextField("Type here", text: $message)
+                }
+            }
         }
     }
 }
+
 
 #Preview {
     ContentView()
