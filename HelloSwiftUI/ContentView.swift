f32b8dabce68f598b0a875649399b52acd12e606
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 15:43:43 +0900

    ContextMenu

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,6 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("Trutle Rock")
+                .padding()
+                .contextMenu { //길게눌러야함
+                    Button {
+                        
+                    } label: {
+                        Label("Add to Favorite", systemImage: "heart")
+                    }
+                    Button {
+                        
+                    } label: {
+                        Label("Show in Maps", systemImage: "mappin")
+                    }
+                }
 
          }
      }
