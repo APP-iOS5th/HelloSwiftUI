f5c8114e7e2806b9f99eae2336d9e017c9e7bd13
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 15:16:04 +0900

    Alert

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -6,29 +6,19 @@
 //
 
 import SwiftUI
-//확대 축소 제스처
+
 struct ContentView: View {
-    @State private var tempValue: CGFloat = 0
-    @State private var finalValue: CGFloat = 1
+    @State private var showAlert = false
     var body: some View {
         VStack {
-            Image(systemName: "star.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .scaledToFit() //부모 크기에 맞춰서
-                .frame(width: 200, height: 200)
-                .scaleEffect(finalValue + tempValue)
-                .gesture(
-                    MagnificationGesture()
-                        .onChanged { amount in
-                            tempValue = amount - 1
-                        }
-                        .onEnded { amount in
-                            finalValue += tempValue
-                            tempValue = 0
-                            
-                        }
+            Button("Show Alert") {
+                showAlert.toggle()
+            }
+            .alert(isPresented: $showAlert, content: {
+                Alert(title: Text("Warning!"), message: Text("Zombies on the loose"),
+                      dismissButton: .default(Text("OK"))
                 )
+            })          
         }
     }
 }
