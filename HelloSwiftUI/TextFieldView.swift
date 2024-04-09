5e99dc6b0dc1fada9f577986a3a676a2a075598d
JongWon Jeong <jjwon214@gmail.com>
2024-04-09 10:09:27 +0900

    dismissKeyboard

--- HelloSwiftUI/TextFieldView.swift
+++ HelloSwiftUI/TextFieldView.swift
@@ -7,21 +7,29 @@
 
 import SwiftUI
 
+
+@available(iOS 15.0, *)
 struct TextFieldView: View {
     
     @State private var message = ""
+    @FocusState var dissmissKeyboard: Bool
     
     var body: some View {
         TextField("Placeholder Text", text: $message)
             .textFieldStyle(.roundedBorder)
-            .disableAutocorrection(true)
+            .autocorrectionDisabled(true)
             .textContentType(.emailAddress)
             .submitLabel(.done)
             .padding()
+            .focused($dissmissKeyboard)
         
         SecureField("password Text", text: $message)
             .textFieldStyle(.roundedBorder)
             .padding()
+        
+        Button("Hide Keyboard") {
+            dissmissKeyboard = false
+        }
     }
 }
 
