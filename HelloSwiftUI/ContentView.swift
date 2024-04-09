acfa9520d217e5be858c78d1d0e94120420ace59
DopeKS <kyusang.dev@gmail.com>
2024-04-09 13:20:59 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,27 +9,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myToggle = false
-    @State var newValue = 0
-    @State var sliderValue = 0.0
-    
     var body: some View {
         VStack {
-            Toggle(isOn: $myToggle) {
-                Text(myToggle ? "Orange" : "Green")
-            }.padding()
             
-            Rectangle()
-                .frame(width: 200, height: 150)
-                .foregroundColor(myToggle ? .orange : .green)
-            
-            Stepper(value: $newValue, in: 1...60, step: 3) {
-                Text("Stepper value = \(newValue)")
-            }.padding()
-            
-            Slider(value: $sliderValue, in: 0...50, step: 5)
-                .padding()
-            Text("Slider value = \(String(format: "%.2f", sliderValue))")
         }
     }
 }
