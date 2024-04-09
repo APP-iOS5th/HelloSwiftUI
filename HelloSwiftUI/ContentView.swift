29098ac949d0ddb3c54b5b839f288d31c85624f7
yehjinjang <j95033176@gmail.com>
2024-04-09 09:43:19 +0900

    TextField

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -4,74 +4,41 @@
 //
 //  Created by 장예진 on 4/8/24.
 
+
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-//            이미지 크기 조정, 크롭(자르기, 원형)
-//            Image("sample_dog_square")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//
-//            Image("sample_cat_square")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .clipShape(Circle())
-         
-//          이미지 그림자 효과
-//            Image("sample_dog_square")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .shadow(color: .red, radius: 46, x: 0, y: 0)
-//
-//            Image("sample_cat_square")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .clipShape(Circle())
-//                .shadow(color: .green, radius: 46, x: 90, y: 50)
 
-//          이미지 테두리 오버레이
-//            Image("sample_dog_square")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .shadow(color: .red, radius: 46, x: 0, y: 0)
-//                .overlay(Rectangle().stroke(Color.blue, lineWidth: 10))
-//
-//            Image("sample_cat_square")
-//                .resizable()
-//                .frame(width: 250, height: 250)
-//                .aspectRatio(contentMode: .fill)
-//                .clipShape(Circle())
-//                .shadow(color: .green, radius: 46, x: 90, y: 50)
-//                .overlay(Circle().stroke(Color.purple, lineWidth: 20))
+extension Color{
+    static let myCustomColor = Color(red:0.5, green:0.8, blue:0.3)
+}
 
-//          이미지 투명도 조절
-            Image("sample_dog_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .opacity(0.5)
-            
-            Image("sample_cat_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .opacity(0.25)
+struct ContentView: View{
+    @State private var email = ""
+    @State private var password = ""
 
-            
+    
+    
+    var body: some View{
+        VStack{
+            TextField("Placeholder Text",text: $email)
+                .textFieldStyle(.roundedBorder)
+                .disableAutocorrection(true)
+                .textContentType(.emailAddress)
+                .submitLabel(.done)
+                .padding()
+                
+            SecureField("Password", text: $password)
+                .textFieldStyle(.roundedBorder)
+                .padding()
         }
     }
 }
 
+
 #Preview {
     ContentView()
 }
+
 
 /*
  Swift 3.0 이전 프리뷰
@@ -80,4 +47,4 @@ struct ContentView: View {
  ContentView()
  }
  }
- */ 
+ */
