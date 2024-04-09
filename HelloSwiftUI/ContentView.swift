725826414ff0daff0664350be589e202465b213f
Jihye Park <deviloper.park@gmail.com>
2024-04-09 14:35:10 +0900

    Gesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,36 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var message = ""
+    @State private var tempValue: CGFloat = 0
+    @State private var finalValue: CGFloat = 1
     
     var body: some View {
         VStack {
-            Menu("Options") { // 메뉴가 펼쳐지는 방향으로 쌓임
-                Button("Open", action: openFile)
-                Button("Find", action: findFile)
-                Button("Delete...", action: deleteFile)
-            }
-            .menuStyle(DefaultMenuStyle())
-            .menuOrder(.fixed) // 글자 쌓이는 방향 고정
-            
-            Text(message).padding()
-            
-            Link(destination: URL(string: "https://www.apple.com")!, label: {
-                Text("Apple")
-            })
+            Image(systemName: "star.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .scaleEffect(finalValue + tempValue) // 1
+                .gesture(
+                    MagnificationGesture() // option + 드래그
+                        .onChanged { amount in // 변하고 있는 중
+                            tempValue = amount - 1} // 영점 조정
+                        .onEnded { amount in // 손가락 딱 뗐을 때
+                            finalValue += tempValue
+                            tempValue = 0
+                        }
+                )
         }
-    }
-    
-    func openFile() {
-        message = "Open chosen"
-    }
-    
-    func findFile() {
-        message = "Find chosen"
-    }
-    
-    func deleteFile() {
-        message = "Delete chosen"
     }
 }
 
