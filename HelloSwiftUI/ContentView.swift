b07d596e06830fc41db829ad28d68d7f63c41077
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 17:02:50 +0900

    Basic Form

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,9 +7,23 @@
 
 import SwiftUI
 
+//form
 struct ContentView: View {
+    @State var messageOne = ""
+    @State var messageTwo = ""
+    
     var body: some View {
         VStack {
+            Form {
+                Text("This is the first Form")
+                TextField("Type here", text: $messageOne)
+            }
+            Form {
+                Text("This is the second Form")
+                TextField("Type here", text: $messageTwo)
+            }
+            Text("Form #1 =\(messageOne)")
+            Text("Form #2 =\(messageTwo)")
         }
     }
 }
