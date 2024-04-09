80bff29d9078596ee9695489a0c869e29bdabed8
YACHAE <104hsh76@gmail.com>
2024-04-09 13:38:57 +0900

    URL, Menu

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,34 +10,41 @@ import SwiftUI
 extension Color {
     static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
 }
+
+
+@available(iOS 15.0, *)
 struct ContentView: View {
-    @State private var message = ""
-    @FocusState var dismissKeyboard: Bool
+    @State var message = ""
     
     var body: some View {
         VStack {
-            TextField("Placeholder", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .autocorrectionDisabled(true)
-                .textContentType(.emailAddress)
-                .submitLabel(.done)
-                .padding()
-                .focused($dismissKeyboard)
+            Link(destination: URL(string: "https://www.apple.com")!, label: {
+                Text("Apple")
+            })
             
-            SecureField("Password", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .padding()
+            Text(message).padding()
             
-            TextEditor(text: $message)
-            
-            Button("버튼 숨기기") {
-                dismissKeyboard = false
+            Menu("Options") {
+                Button("Open", action: openFile)
+                Button("Find", action: findFile)
+                Button("Delete...", action: deleteFile)
             }
-            
-           
-             
+            .menuOrder(.fixed)
         }
     }
+    
+    func openFile() {
+        message = "open chosen"
+    }
+    
+    func findFile() {
+        message = "Find chosen"
+    }
+    
+    func deleteFile() {
+        message = "Delte chosen"
+    }
+    
 }
 #Preview {
     ContentView()
