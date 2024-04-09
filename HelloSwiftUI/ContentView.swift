fe935ac1f9ca3d216ddc02c15e713d85c903ecf5
YuleGlycerine <figurefig13@gmail.com>
2024-04-09 11:27:29 +0900

    formatter

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,12 +13,34 @@ extension Color {
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-    @State private var choice = 0.0
-    @State private var myColor = Color.red
+    
     @State var myDate = Date.now
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
+        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
+        return calendar.date(from: startComponents)!
+        ...
+        calendar.date(from:endComponents)!
+        
+    }()
+    let formatter = DateFormatter()
     
     var body: some View {
         VStack  {
+            
+            
+            Text("Chosen date = \(formatter.string(from: myDate))")
+                .padding()
+            DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date") })
+                .datePickerStyle(.graphical)
+                .padding()
+            
+        }.onAppear() {
+            formatter.locale = Locale(identifier: "ko_KR")
+            formatter.dateStyle = .short
+            formatter.timeStyle = .short
+            
             
         }
     }
