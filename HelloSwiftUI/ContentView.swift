6dd1be75c9f1adf7a376ca910c42111b82b4dae8
Jihye Park <deviloper.park@gmail.com>
2024-04-09 11:28:01 +0900

    DateFormatter

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -15,18 +15,24 @@ struct ContentView: View {
         let endCOmponents = DateComponents(year: 2024, month: 9, day: 13)
         
         return calendar.date(from: startComponents)!
-        ...
+        ... // ClosedRange에 존재하는 operator
         calendar.date(from: endCOmponents)!
-    }()
+    }() // 선언부에 정의된 함수를 실행한다는 의미
+    
+    let formatter = DateFormatter()
     
     var body: some View {
         VStack {
-            Text("Chosen date = \(myDate)")
+            Text("Chosen date = \(formatter.string(from: myDate))")
                 .padding()
             // 날짜 선택
             DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date") })
-                .datePickerStyle(.graphical) // 캘린더
+                .datePickerStyle(.graphical)
                 .padding()
+        }.onAppear() {
+            formatter.locale = Locale(identifier: "ko_KR")
+            formatter.dateStyle = .full // 요일까지 출력
+            formatter.timeStyle = .medium // 시간 출력
         }
     }
 }
