4f4cd3c587a7d8dd6cace7535867f458a9de5f8b
wotjs020708 <wotjs020708@gmail.com>
2024-04-09 13:08:42 +0900

    Slider

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 struct ContentView: View {
     @State var mytoggle = true
     @State var newValue = 0
+    @State var sliderValue = 0.0
     var body: some View {
         VStack {
             Toggle(isOn: $mytoggle) {
@@ -24,6 +25,10 @@ struct ContentView: View {
                 Text("Stepper value = \(newValue)")
             }
             .padding()
+            
+            Slider(value: $sliderValue, in: 1...50, step: 4)
+                .padding()
+            Text("Slider value = \(sliderValue)")
         }
     }
 }
