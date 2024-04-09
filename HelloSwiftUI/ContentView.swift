aea6484f850377b7200a70bf9a5fe56ffc00872c
jmbae <jmbae@codegrove.co.kr>
2024-04-09 11:46:31 +0900

    Toggle Stepper

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,10 +11,26 @@ extension Color {
     static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
 }
 
-struct ContentView: View {    
+struct ContentView: View {
+    @State var myToggle = true
+    @State var newValue = 0
+
     var body: some View {
         VStack {
+            Toggle(isOn: $myToggle) {
+                Text(myToggle ? "Orange" : "Green")
+            }
+            .padding()
             
+            Rectangle()
+                .frame(width: 200, height: 150)
+                .foregroundColor(myToggle ? .orange : .green)
+            
+            Stepper(value: $newValue, in: 1...10) {
+                Text("Stepper value = \(newValue)")
+            }
+            .padding()
+
         }
     }
 }
