4063b61e6ac5331922cc8f6a834e162752aa8f54
seonggeun an <eventscripts@nate.com>
2024-04-09 09:36:00 +0900

    TextField

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var message = ""
     
     var body: some View {
         VStack {
-                    
+            
+            //일반텍스트상자처럼 공개된 텍스트박스
+            TextField("Placeholder text", text: $message)
+                .textFieldStyle(.roundedBorder)
+                //(철자,문자)자동수정 옵션
+                .disableAutocorrection(true)
+                //입력창의 방식 제한(아이디,비밀번호,이메일)
+                .textContentType(.emailAddress)
+                //.keyboardType(입력방식)
+                //.default, .emailAddress, .decimalPad, numbersAnadPunctuation
+                //입력을 완료했을때 Enter키 변경
+                .submitLabel(.done)
+                .padding()
+            
+            //비밀번호창처럼 비공개 텍스트박스
+            SecureField("Password", text: $message)
+                .textFieldStyle(.roundedBorder)
+                .padding()
         }
     }
 }
