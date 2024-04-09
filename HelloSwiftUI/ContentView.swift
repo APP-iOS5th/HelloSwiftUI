49841514688b0b1d27ec4fd69cd99462d4789956
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 14:18:30 +0900

    MagnificationGesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
-
+//확대 축소 제스처
 struct ContentView: View {
+    @State private var tempValue: CGFloat = 0
+    @State private var finalValue: CGFloat = 1
     var body: some View {
         VStack {
-
-            
+            Image(systemName: "star.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .scaleEffect(finalValue + tempValue)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { amount in
+                            tempValue = amount - 1
+                        }
+                        .onEnded { amount in
+                            finalValue += tempValue
+                            tempValue = 0
+                            
+                        }
+                )
         }
     }
 }
