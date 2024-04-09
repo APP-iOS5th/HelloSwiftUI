657bda110ad98d0ddf3985504685bc5c9e608c74
yehjinjang <j95033176@gmail.com>
2024-04-09 15:23:49 +0900

    alert

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,37 +7,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tempScaleValue: CGFloat = 1 // 현재 제스처로 인한 임시 스케일 값
-    @State private var finalScaleValue: CGFloat = 1 // 최종 스케일 값을 저장
-    @State private var degree: Double = 0.0 // 회전 각도
-
-    var body: some View {
+    @State var showAlert = false
+    
+    var body: some View{
         VStack {
-            Text("tempValue = \(tempScaleValue)")
-            Spacer()
-            Image(systemName: "heart.fill")
-                .resizable()
-                .scaledToFit()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 200, height: 200)
-                .scaleEffect(finalScaleValue * tempScaleValue) // 스케일 적용
-                .rotationEffect(.degrees(degree)) // 회전 적용
-                .gesture(
-                    MagnificationGesture()
-                        .onChanged { amount in
-                            tempScaleValue = amount // 임시 스케일 값 업데이트
-                        }
-                        .onEnded { amount in
-                            finalScaleValue *= tempScaleValue // 최종 스케일 값 업데이트
-                            tempScaleValue = 1 // 임시 스케일 값 초기화
-                        }
-                        .simultaneously(with: RotationGesture() // 회전 제스처와 동시에 적용
-                            .onChanged { angle in
-                                degree = angle.degrees // 회전 각도 업데이트
-                            }
-                        )
-                )
-            Spacer()
+            Button ("Show Alert"){
+                showAlert.toggle()
+            }
+            .alert("Warning", isPresented: $showAlert) {
+                Button(role: .cancel) {
+                    print("button action")
+                } lable: {
+                    Text("OK!")
+                }
+            } message: {
+                Text("Zombie on the loose")
+                
+            }
         }
     }
 }
