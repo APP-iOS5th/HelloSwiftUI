e41389d9ae684ec638c666827682a9d595f044d5
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-09 13:14:30 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,28 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myToggle = true
-    @State var newValue = 0
-    @State var sliderValue = 0.0
-    
     var body: some View {
         VStack {
-            Toggle(isOn: $myToggle){ //$쓰면 뷰에 연결시켜줌, SwiftUI는 readonly기반
-                Text(myToggle ? "Orange" : "Green")
-            }.padding()
-            
-            Rectangle()
-                .frame(width: 200, height: 150)
-                .foregroundColor(myToggle ? .orange : .green)
-            
-            Stepper(value: $newValue, in: 0...10) { //in여기서는 범위 한정자
-                Text("Stepper value = \(newValue)")
-            }
-            .padding()
-            
-            Slider(value: $sliderValue, in: 1...50, step: 4) //step: 범위만큼 끊어서
-                .padding()
-            Text("Slider value = \(sliderValue)")
+
         }
     }
 }
