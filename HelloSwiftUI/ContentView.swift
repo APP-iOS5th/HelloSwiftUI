bf9370437ce759a2bb955ed62f8cac2d27b60111
Hyunseo Yook <hhh3332@naver.com>
2024-04-09 11:51:00 +0900

    Toggle Stepper

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,30 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myDate = Date.now
-    let dateRange: ClosedRange<Date> = {
-        let calendar = Calendar.current
-        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
-        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
-        return calendar.date(from: startComponents)!
-        ...
-        calendar.date(from:endComponents)!
-    }()
-    let formatter = DateFormatter()
-
+    
+    @State var myToggle = true
+    @State var newValue = 0
+    
     var body: some View {
         VStack {
-            Text("Chosen date = \(formatter.string(from: myDate))")
-                            .padding()
-            DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date") })
-                .datePickerStyle(.graphical)
-                .padding()
-
-        }.onAppear() {
-            formatter.locale = Locale(identifier: "ko_KR")
-            formatter.dateStyle = .medium
-//            formatter.dateStyle = .full
-//            formatter.dateStyle = .long
+            Toggle(isOn: $myToggle) {
+                Text(myToggle ? "Orange" : "Green")
+            }
+            .padding()
+            
+            Rectangle()
+                .frame(width: 200, height: 150)
+                .foregroundColor(myToggle ? .orange : .green)
+            
+            Stepper(value: $newValue, in: 1...10) {
+                Text("Stepper value = \(newValue)")
+            }
+            .padding()
         }
     }
 }
