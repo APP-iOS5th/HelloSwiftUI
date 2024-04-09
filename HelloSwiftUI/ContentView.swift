5fa73cc4e0721d99ac1720f72857cdf80a82f36d
Min-Gyeong Hwang <freemin217@gmail.com>
2024-04-09 15:42:29 +0900

    contextMenu

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,23 +12,20 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Button("Show Alert") {
-                showAlert.toggle()
-            }
-            .confirmationDialog("Warning!", isPresented: $showAlert, titleVisibility: .visible){
-                Button(role: .none){
-                    print("option1 action")
-                } label: {
-                    Text("option1")
+            Text("Turtle Rock")
+                .padding()
+                .contextMenu {
+                    Button {
+                        
+                    } label: {
+                        Label("Add to Favorite", systemImage: "heart")
+                    }
+                    Button {
+                        
+                    } label: {
+                        Label("Show in Maps", systemImage: "mappin")
+                    }
                 }
-                Button(role: .destructive){
-                    print("option2 action")
-                } label: {
-                    Text("option2")
-                }
-            } message: {
-                Text("Zombies on the loose")
-            }
         }
     }
 }
