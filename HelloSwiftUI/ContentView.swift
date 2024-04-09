199bfef729b8a614e1afcb551401bf1f659a55ca
Jongchan Yun <jcyun1996@gmail.com>
2024-04-09 15:48:41 +0900

    confirmation Dialog

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,14 +13,19 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Button("Show Alert") {
+            Button("Show ActionSheet") {
                 showAlert.toggle()
             }
-            .alert("Warning!", isPresented: $showAlert) {
-                Button(role: .cancel) {
-                    print("button action")
+            .confirmationDialog("Warning!", isPresented: $showAlert) {
+                Button(role: .none) {
+                    print("option1 action")
                 } label: {
-                    Text("OK!")
+                    Text("option1")
+                }
+                Button(role: .destructive) {
+                    print("option2 action")
+                } label: {
+                    Text("option2")
                 }
             } message: {
                 Text("Zombies on the loose")
