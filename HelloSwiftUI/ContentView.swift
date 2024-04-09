b1fb4a998a8a2b3a44e422f8884d43f82ba3f3bb
Jihye Park <deviloper.park@gmail.com>
2024-04-09 10:09:19 +0900

    dismissKeyboard

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,22 +7,32 @@
 
 import SwiftUI
 
+// 버전 컨트롤(버전 15 아래는 실행되지 않음)
+@available(iOS 15.0, *)
 struct ContentView: View {
     @State private var message = ""
+    @FocusState var dismissKeyboard: Bool
     
     var body: some View {
         VStack {
             TextField("Placeholder text", text:$message)
                 .textFieldStyle(.roundedBorder)
-                .disableAutocorrection(true) // 자동완성 기능 끄기
+//                .disableAutocorrection(true) // 자동완성 기능 끄기
+                .autocorrectionDisabled(true) // 자동완성 기능 끄기
                 .textContentType(.emailAddress) // 키보드 타입
                 .submitLabel(.done) // 제출 버튼
                 .padding()
+                .focused($dismissKeyboard)
             
             // 보안 필드
             SecureField("Password", text: $message)
                 .textFieldStyle(.roundedBorder)
                 .padding()
+            
+            // 키보드 숨기기 버튼
+            Button("Hide Keyboard") {
+                dismissKeyboard = false
+            }
             
             
         }
