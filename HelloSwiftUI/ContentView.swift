d4683f4567dc984707da3a444ee5863cb91b349e
seonggeun an <eventscripts@nate.com>
2024-04-09 11:46:44 +0900

    Stepper

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-    
+    @State var myToggle = true
+    @State var newValue = 0
     
     var body: some View {
         VStack {
+            Toggle(isOn: $myToggle){
+                Text("Toggle Text")
+            }
+            .padding()
+            
+            Rectangle()
+                .frame(width:200 ,height: 150)
+                .foregroundColor(myToggle ? .orange : .green)
+            
+            Stepper(value: $newValue, in:1...10) {
+                Text("Stepper value = \(newValue)")
+            }
+            .padding()
             
         }
     }
