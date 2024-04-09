4c4d34a2ef901b09e2bcb0c60bf46c5e495ddf92
DopeKS <kyusang.dev@gmail.com>
2024-04-09 09:17:55 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,26 +9,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = ""
-    
     var body: some View {
-        VStack (spacing: 28) {
-            Text(message)
-            Picker("Favorite Color", selection: $message, content: {
-                Text("Happy").tag("happy")
-                Text("Sad").tag("sad")
-                Text("Bored").tag("board")
-            })
-            .pickerStyle(SegmentedPickerStyle())
-            .onChange(of: message) { oldValue, newValue in
-                switch newValue {
-                case "happy": message = "Be happy and joyous"
-                case "sad": message = "Life can be a struggle at"
-                case "board": message = "Look for your purpose"
-                default:
-                    break
-                }
-            }
+        VStack {
+            
         }
     }
 }
