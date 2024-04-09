f3ee86a4198331acbf5a0beefcd483217c3ae43c
DopeKS <kyusang.dev@gmail.com>
2024-04-09 14:09:33 +0900

    TapGesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,9 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var changeMe1 = false
+    @State var changeMe2 = false
+    @State var changeMe3 = false
+    @State var message = ""
+    
     var body: some View {
         VStack {
+            Text("Change tap")
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe1 ? .red : .yellow)
+                .onTapGesture {
+                    changeMe1.toggle()
+                }
             
+            Text("Change double tap")
+                .padding(.top, 30)
+            
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe2 ? .red : .yellow)
+                .onTapGesture(count: 2) {
+                    changeMe2.toggle()
+                }
+            
+            Text("Change Long press tap")
+                .padding(.top, 30)
+            
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe3 ? .red : .yellow)
+                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: { stillPressed in
+                    message = "Long press in progress : \(stillPressed)"
+                }) {
+                    changeMe3.toggle()
+                }
+            
+            Text(message).padding()
         }
     }
 }
