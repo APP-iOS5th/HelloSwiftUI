57b6b69d7609d44c83337a46b216c139cc4799f1
yehjinjang <j95033176@gmail.com>
2024-04-09 14:16:01 +0900

    Magnification Gesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,39 +8,27 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var changeMe1 = false
-    @State var changeMe2 = false
-    @State var changeMe3 = false
-    @State var message = ""
-
+    //CGFloat => 그래픽처리할때 쓰는 Float & CGPoint => 그래픽처리할때 쓰는 좌표(x,y)값
+    @State private var tempValue: CGFloat = 0
+    @State private var finalValue: CGFloat = 1
 
     
     var body: some View{
         VStack{
-            Circle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe1 ? .red : .yellow)
-            //count:2 (2 click to change)
-                .onTapGesture(count: 2) {
-                    changeMe1.toggle()
-                }
-            
-            Circle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe2 ? .red : .yellow)
-                .onTapGesture(count: 2) {
-                    changeMe2.toggle()
-                }
-            
-            Text(message).padding()
-            
-            Circle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe3 ? .red : .yellow)
-                .onLongPressGesture(minimumDuration: 2, maximumDistance:2 , pressing: { stillPressed in message = "Long press in progress: \(stillPressed)"
-                }){
-                    changeMe3.toggle()
-                }
+            Image(systemName: "heart.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width:100, height:200)
+                .scaleEffect(finalValue + tempValue)
+                .gesture(MagnificationGesture()
+                    .onChanged { amount in
+                        tempValue = amount - 1
+                    }
+                    .onEnded{ amount in
+                        finalValue += tempValue
+                        tempValue = 0
+                    })
+
             
         }
     }
