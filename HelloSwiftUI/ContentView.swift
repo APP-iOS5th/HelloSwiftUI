159adf617002ca696819a850b443c2a3dd0ae00a
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-09 14:09:00 +0900

    gesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,36 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var changeMe1 = false
+    @State private var changeMe2 = false
+    @State private var changeMe3 = false
     @State private var message = ""
     
     var body: some View {
         VStack {
-            Menu("options") {
-                Button("Open", action: openFile)
-                Button("Find", action: findFile)
-                Button("Delete", action: deleteFile)
-            }
-            .menuStyle(DefaultMenuStyle())
-            .menuOrder(.fixed)
+            
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe1 ? .red : .yellow)
+                .onTapGesture {
+                    changeMe1.toggle()
+                }
+            
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe2 ? .red : .yellow)
+                .onTapGesture(count: 2) {
+                    changeMe2.toggle()
+                }
             
             Text(message).padding()
             
-            Link(destination: URL(string: "https://www.apple.com")!) {
-                Text("Apple")
-            }
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe3 ? .red : .yellow)
+                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: {
+                    stillPressed in
+                    message = "Long press in progress : \(stillPressed)"
+                }) {
+                    changeMe3.toggle()
+                }
         }
-    }
-    
-    func openFile() {
-        message = "Open"
-    }
-    
-    func findFile() {
-        message = "Find"
-    }
-    
-    func deleteFile() {
-        message = "Delete"
     }
 }
 
