ff7e102181e2e3892054b47206f8f369033e39c9
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-09 14:42:38 +0900

    magnify gesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,40 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var changeMe1 = false
-    @State private var changeMe2 = false
-    @State private var changeMe3 = false
-    @State private var message = ""
+    @State private var tempValue: CGFloat = 0
+    @State private var finalValue: CGFloat = 1
     
     var body: some View {
         VStack {
-            
-            Rectangle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe1 ? .red : .yellow)
-                .onTapGesture {
-                    changeMe1.toggle()
-                }
-            
-            Rectangle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe2 ? .red : .yellow)
-                .onTapGesture(count: 2) {
-                    changeMe2.toggle()
-                }
-            
-            Text(message).padding()
-            
-            Rectangle()
-                .frame(width: 175, height: 125)
-                .foregroundStyle(changeMe3 ? .red : .yellow)
-                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: {
-                    stillPressed in
-                    message = "Long press in progress : \(stillPressed)"
-                }) {
-                    changeMe3.toggle()
-                }
+            Image(systemName: "star.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .scaleEffect(tempValue + finalValue)
+                .gesture(
+                MagnifyGesture()
+                    .onChanged { value in
+                        tempValue = value.magnification - 1
+                    }
+                    .onEnded { _ in
+                        finalValue += tempValue
+                        tempValue = 0
+                    }
+                )
+                
         }
     }
 }
@@ -49,12 +36,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-/*
- Swift 3.0 이전 프리뷰
- struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
- }
- */
