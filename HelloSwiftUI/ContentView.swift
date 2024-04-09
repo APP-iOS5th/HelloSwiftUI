d00e679fcb9adf429d3e9a40374091b1aaf52ed1
ksiomng0929 <nadana0929@gmail.com>
2024-04-09 15:13:23 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,16 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var showAlert = false
     
     var body: some View {
         VStack {
-            Button("Show Alert") {
-                showAlert.toggle()
-            }
-            .alert(isPresented: $showAlert, content: {
-                Alert(title: Text("Warining!"), message: Text("Zombies on the loose"), dismissButton: .default(Text("Ok")))
-            }) // dismissButton Alert창을 사라지게 하는 버튼
+
         }
     }
 }
