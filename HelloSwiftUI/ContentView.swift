984a2ac2cd63889d50838b395fc8990aa72b0812
ksiomng0929 <nadana0929@gmail.com>
2024-04-09 13:22:42 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,42 +1,10 @@
-//
-//  ContentView.swift
-//  HelloSwiftUI
-//
-//  Created by Mac on 4/8/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = ""
-    @State private var color = Color.gray
-
+    
     var body: some View {
-        VStack (spacing: 23){
-            Text(message)
-                .font(.title)
-                .background(color)
-            Picker("Favorite Color", selection: $message, content: {
-                Text("Happy").tag("happy")
-                Text("Sad").tag("sad")
-                Text("Bored").tag("bored")
-            })
-            .pickerStyle(SegmentedPickerStyle()) //종류가 여러개 잇음
-            .onChange(of: message) { newValue in
-                switch newValue {
-                case "happy":
-                    message = "Be happy and joyous"
-                    color = Color.red
-                case "sad":
-                    message = "Life can be a struggle at times"
-                    color = Color.gray
-                case "bored":
-                    message = "Look for your purpose"
-                    color = Color.blue
-                default:
-                    break
-                }
-            }
+        VStack {
+
         }
     }
 }
@@ -44,11 +12,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-/*
- swift 3.0이전
- struct ContentView_Previews: PreviewProvider {
- static var previews: some View {
- contentView()
- }
- }
- */
+
