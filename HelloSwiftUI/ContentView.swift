67579d5d159ffc0b6a76c3c193e460960c7696f3
seonggeun an <eventscripts@nate.com>
2024-04-09 13:37:11 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,38 +9,10 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-    @State var message = ""
-    
     var body: some View {
         VStack {
 
-            
-            Menu("Options") {
-                Button("Open", action: openFile)
-                Button("Find", action: findFile)
-                Button("Delete...", action: deleteFile)
-            }
-            .menuStyle(DefaultMenuStyle())
-            .menuOrder(.fixed)
-            
-            Text(message).padding()
-            
-            Link(destination: /*@START_MENU_TOKEN@*/URL(string: "https://www.apple.com")!/*@END_MENU_TOKEN@*/, label: {
-                Text("Apple")
-            })
         }
-    }
-    
-    func openFile(){
-        message = "Open chosen"
-    }
-    
-    func findFile(){
-        message = "Find chosen"
-    }
-    
-    func deleteFile(){
-        message = "Delete chosen"
     }
 }
 
