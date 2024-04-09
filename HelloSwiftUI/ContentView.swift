732900e082c4d7a71a34908b43e55ab69e5a40ee
seokyung402 <wni402@naver.com>
2024-04-09 15:09:08 +0900

    Alert

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,23 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var degree = 0.0
+    @State var showAlert = false
+    
     var body: some View {
         VStack {
-            Text("degree = \(degree)")
-            Spacer()
-            Image(systemName: "star.fill")
-                .resizable() // 이미지 사이즈 조정 가능
-                .aspectRatio(contentMode: .fit) // 비율 유지
-                .frame(width: 200, height: 200) // 프레임 크기
-                .rotationEffect(Angle.degrees(degree))
-                .gesture( // 이미지에 제스처 추가
-                    RotationGesture()
-                        .onChanged({ angle in
-                            degree = angle.degrees
-                        })
+            Button("Show Alert") {
+                showAlert.toggle()
+            }
+            .alert(isPresented: $showAlert, content: {
+                Alert(title: Text("Warning"), message: Text("Zombies on the loose"), dismissButton: .default(Text("OK"))
                 )
-            Spacer()
+            })
         }
     }
 }
