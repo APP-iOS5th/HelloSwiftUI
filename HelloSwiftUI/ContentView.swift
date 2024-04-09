5287470eceea7a9a4da63abd93647b2ac4825422
YuleGlycerine <figurefig13@gmail.com>
2024-04-09 13:38:44 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,25 +13,15 @@ extension Color {
 
 @available(iOS 15.0, *)
 struct ContentView: View {
-    @State var myToggle = true
-    @State var newValue = 0
+    
     
     var body: some View {
-        VStack  {
-            Toggle(isOn: $myToggle) {
-                Text(myToggle ? "Orange" : "Green")
-            }
-            Rectangle()
-                .frame(width: 200, height: 150)
-                .foregroundColor(myToggle ? .orange : .green)
+        VStack {
             
-            Stepper(value: $newValue, in: 1...10) {
-                Text("Stepper value = \(newValue)")
-            }
-            .padding()
-            }
+            
         }
     }
+}
 
 #Preview {
     ContentView()
