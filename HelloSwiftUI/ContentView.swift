be7f0cae127fd33f9effa7ec8fd43196b7856b9b
ksiomng0929 <nadana0929@gmail.com>
2024-04-09 15:04:09 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,24 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var degree = 0.0
     
     var body: some View {
         VStack {
-            Text("degree = \(degree)")
-            Spacer()
-            Image(systemName: "star.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 200, height: 200)
-                            .rotationEffect(Angle.degrees(degree))
-                            .gesture(
-                                RotationGesture()
-                                    .onChanged { angle in
-                                        degree = angle.degrees
-                                    }
-                            )
-            Spacer()
+            
         }
     }
 }
