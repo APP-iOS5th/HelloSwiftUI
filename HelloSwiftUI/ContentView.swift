c3fb10a240d79aea119965246fe36c66f9cb87e0
Jongchan Yun <jcyun1996@gmail.com>
2024-04-09 14:17:51 +0900

    TapGesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,36 +12,40 @@ extension Color {
 }
 
 struct ContentView: View {
+    @State var changeMe1 = false
+    @State var changeMe2 = false
+    @State var changeMe3 = false
     @State var message = ""
     
     var body: some View {
         VStack {
-            Link(destination: /*@START_MENU_TOKEN@*/URL(string: "https://www.apple.com")!/*@END_MENU_TOKEN@*/, label: {
-                Text("Apple")
-            })
+            
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe1 ? .red : .cyan)
+                .onTapGesture {
+                    changeMe1.toggle()
+                }
+            
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe2 ? .red : .cyan)
+                .onTapGesture(count: 2) {
+                    changeMe2.toggle()
+                }
             
             Text(message).padding()
             
-            Menu("Options") {
-                Button("Open", action: openFile)
-                Button("Find", action: findFile)
-                Button("Delete...", action: deleteFile)
-            }
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe3 ? .red : .cyan)
+                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: { stillPressed in
+                    message = "Long press in progress: \(stillPressed)"
+                }) {
+                    changeMe3.toggle()
+                }
         }
     }
-    
-    func openFile() {
-        message = "Open chosen"
-    }
-    
-    func findFile() {
-        message = "Find chosen"
-    }
-    
-    func deleteFile() {
-        message = "Delete chosen"
-    }
-    
 }
 
 #Preview {
