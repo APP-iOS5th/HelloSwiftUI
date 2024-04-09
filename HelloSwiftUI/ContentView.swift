c0cef54db1cddc0fad7b6e2f37af0275d6dcebde
Jihye Park <deviloper.park@gmail.com>
2024-04-09 13:27:55 +0900

    Link Menu

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,30 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myToggle = true
-    @State var newValue = 0
-    @State var sliderValue = 0.0
+    @State var message = ""
     
     var body: some View {
         VStack {
-            Toggle(isOn: $myToggle) { // 상태값 변경에 대해서 필요
-                Text(myToggle ? "Orange" : "Green")
+            Link(destination: URL(string: "https://www.apple.com")!, label: {
+                Text("Apple")
+            })
+            
+            Text(message).padding()
+            
+            Menu("Options") {
+                Button("Open", action: openFile)
+                Button("Find", action: findFile)
+                Button("Delete...", action: deleteFile)
             }
-            .padding()
-            
-            Rectangle()
-                .frame(width: 200, height: 150)
-                .foregroundColor(myToggle ? .orange : .green)
-            
-            Stepper(value: $newValue, in: 1 ... 10) {
-                Text("Stepper value = \(newValue)")
-            }
-            .padding()
-            
-            Slider(value: $sliderValue, in: 0...50, step: 4) // 기본은 0부터 1
-                .padding()
-            Text("Slider value = \(sliderValue)")
         }
+    }
+    
+    func openFile() {
+        message = "Open chosen"
+    }
+    
+    func findFile() {
+        message = "Find chosen"
+    }
+    
+    func deleteFile() {
+        message = "Delete chosen"
     }
 }
 
