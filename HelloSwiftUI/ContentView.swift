67db124d1c4b42ca36a49560796b896b42d17b03
seokyung402 <wni402@naver.com>
2024-04-09 11:47:13 +0900

    Toggle Stepper

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,33 +11,25 @@ extension Color {
     static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
 }
 
-
 struct ContentView: View {
-    @State var myDate = Date.now
-    let dateRange: ClosedRange<Date> = {
-        let calendar = Calendar.current
-        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
-        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
-        return calendar.date(from: startComponents)! ... calendar.date(from: endComponents)!
-    }()
-    let formatter = DateFormatter()
+    @State var myToggle = true
+    @State var newValue = 0
     
     var body: some View {
         VStack {
-            Text("Chosen date = \(formatter.string(from: myDate))")
-                .padding()
-            DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date") })
-                .datePickerStyle(.graphical)
-                .padding()
-        }.onAppear() {
-            //formatter.dateStyle = .short
-            formatter.locale = Locale(identifier: "ko_KR")
-            formatter.dateStyle = .long
-            formatter.timeStyle = .short
+            Toggle(isOn: $myToggle) {
+                Text(myToggle ? "Orange" : "Green")
+            }
+            Rectangle()
+                .frame(width: 200, height: 150)
+                .foregroundColor(myToggle ? .orange : .green)
+            Stepper(value: $newValue, in: 1...10) {
+                Text("Stepper value = \(newValue)")
+            }
+            .padding()
         }
     }
 }
-
 
 
 struct ContentView_Previews: PreviewProvider {
