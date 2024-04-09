8e7c907177f8f26fd97556976b172ab616331835
DopeKS <kyusang.dev@gmail.com>
2024-04-09 16:46:31 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,37 +9,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    struct Animal: Identifiable {
-        var id = UUID()
-        var name: String
-    }
-    
-    struct AnimalCategory: Identifiable {
-        var id = UUID()
-        var category: String
-        var animals: [Animal]
-    }
-    
-    let categories = [
-        AnimalCategory(category: "포유류", animals: [Animal(name: "고양이"),Animal(name: "강아지")]),
-        AnimalCategory(category: "파충류", animals: [Animal(name: "거북이"),Animal(name: "도마뱀")]),
-    ]
-    
     var body: some View {
         VStack {
-            List {
-                ForEach(categories) { categor2 in
-                    Section(header: Text(categor2.category)) {
-                        ForEach(categor2.animals) { animal in
-                            Text(animal.name)
-                        }
-                    }
-                }
-            }
+            
         }
     }
 }
-
 
 #Preview {
     ContentView()
