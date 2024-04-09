57c186aa0c493f12bd75315ed9ecc7745862ff6f
JongWon Jeong <jjwon214@gmail.com>
2024-04-09 11:29:07 +0900

    DateFormatter

--- HelloSwiftUI/DatePickerWithRange.swift
+++ HelloSwiftUI/DatePickerWithRange.swift
@@ -14,18 +14,22 @@ struct DatePickerWithRange: View {
         let calendar = Calendar.current
         let startComponents = DateComponents(year: 2024, month: 3, day: 25)
         let endComponents = DateComponents(year: 2024, month: 9, day: 13)
-        return calendar.date(from: startComponents)!
-        ...
-        calendar.date(from: endComponents)!
+        return calendar.date(from: startComponents)! ... calendar.date(from: endComponents)!
     }()
+    
+    let formatter = DateFormatter()
     
     var body: some View {
         VStack {
-            Text("Chosen date = \(myDate)")
+            Text("Chosen date = \(formatter.string(from: myDate))")
                 .padding()
             DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: {Text("Date")})
                 .datePickerStyle(.graphical)
                 .padding()
+        }.onAppear() {
+            formatter.locale = Locale(identifier: "ko_KR")
+            formatter.dateStyle = .long
+            formatter.timeStyle = .medium
         }
     }
 }
