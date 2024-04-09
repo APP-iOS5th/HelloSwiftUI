e64c73a706914b67fe21fbe23d0c509a4602d20c
yehjinjang <j95033176@gmail.com>
2024-04-09 11:46:43 +0900

    Toggle Stepper

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -4,47 +4,33 @@
 //
 //  Created by 장예진 on 4/8/24.
 
-
 import SwiftUI
 
-extension Color {
-    static let myCustomColor = Color(red: 0.5, green: 0.8, blue: 0.3)
-}
 
-@available(iOS 15.0, *)
 struct ContentView: View {
-    @State var myDate = Date.now
-    let dateRange: ClosedRange<Date> = {
-        let calendar = Calendar.current
-        let startComponents = DateComponents(year: 2024, month: 3, day:25)
-        let endComponents = DateComponents(year:2024, month: 9, day:13)
-        return calendar.date(from: startComponents)!
-        ...
-        calendar.date(from:endComponents)!
-    }()
-        
+    @State var myToggle = true //boolean
+    @State var newValue = 0 //int
     
     var body: some View{
         VStack{
-            Text("Chosen data = \(myDate)")
-                .padding()
-            DatePicker(selection: $myDate, displayedComponents: [.date], label: {
-                Text("Date") })
-            .datePickerStyle(.graphical)
+            Toggle(isOn: $myToggle){
+                Text("Toggle Text")
+            }
+            Rectangle()
+                .frame(width: 200, height:150)
+                .foregroundColor(myToggle ? .orange: .green)
+            // int 값의 범위한정자를 지정해줘야함. (1~10으로)
+            Stepper(value: $newValue, in: 1...10) {
+                Text("Stepper value = \(newValue)")
+            }
             .padding()
         }
     }
 }
 
-    
-    
-    
-
-
 #Preview {
     ContentView()
 }
-
 
 /*
  Swift 3.0 이전 프리뷰
