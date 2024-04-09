51c44ca1d8ddd1d2b4b562d456a2080032b4093f
Min-Gyeong Hwang <freemin217@gmail.com>
2024-04-09 15:03:25 +0900

    rotationEffect degree

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,21 +7,24 @@
 
 import SwiftUI
 
-@available(iOS 15.0, *)
 struct ContentView: View {
-    @State private var tempValue: CGFloat = 0
-    @State private var finalValue: CGFloat = 1
+    @State private var degree = 0.0
     var body: some View {
         VStack {
-           Image(systemName: "star.fill")
+            Text("tempValue = \(degree)")
+            Spacer()
+            Image(systemName: "star.fill")
                 .resizable()
-                .scaledToFit()
+                .aspectRatio(contentMode: .fit)
                 .frame(width: 200, height: 200)
-                .scaleEffect(finalValue + tempValue)
-                .gesture(MagnificationGesture()
-                    .onChanged { amount in tempValue = amount - 1}
-                    .onEnded { amount in finalValue += tempValue 
-                        tempValue = 0})
+                .rotationEffect(Angle.degrees(degree))
+                .gesture(
+                    RotationGesture()
+                        .onChanged { angle in
+                            degree = angle.degrees
+                            }
+                    )
+            Spacer()
         }
     }
 }
