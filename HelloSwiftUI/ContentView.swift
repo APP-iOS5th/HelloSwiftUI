2c045485664de545e4fe1a8f127d423fa33405f4
SeongKookKIM <ksk8646@naver.com>
2024-04-09 09:18:04 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -46,15 +46,15 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    ContentView()
+//}
 
-/*
- Swift 3.0 이전 프리뷰
+
+ 
  struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
  }
- */
+
