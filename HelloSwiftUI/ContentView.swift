5a2b1c7ed8ab6a8eb8077ce05ae10a94147dcfeb
jihye <ganzi410@gmail.com>
2024-04-09 10:36:17 +0900

    Picker(normal, color, date)

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,28 +10,28 @@ import SwiftUI
 @available(iOS 15.0, *)
 struct ContentView: View {
     
-    @State private var message = ""
-    @FocusState var dismissKeyboard: Bool
+    @State private var choice = 0.0
+    @State private var myColor = Color.red
+    @State var myDate = Date.now
     
     var body: some View {
         VStack {
-            TextField("Placeholder Text", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .disableAutocorrection(true) // 자동완성 off
-                .textContentType(.emailAddress)
-                .submitLabel(.done) // 키보드 Enter 버튼 모양
-                .padding()
-                .focused($dismissKeyboard)
+            Picker(selection: $choice, label: Text("Picker")) {
+                Text("Bird").tag(1.0)
+                Text("Cat").tag(2.0)
+                Text("Dog").tag(3.0)
+                Text("Lizard").tag(4.0)
+                Text("Hamster").tag(5.0)
+            }.pickerStyle(SegmentedPickerStyle())
+            Text("You Choose \(choice)")
             
-            SecureField("Password", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .padding()
+            ColorPicker("Choice Color", selection: $myColor)
+            Rectangle()
+                .frame(width: 150, height: 100)
+                .foregroundStyle(myColor)
             
-            TextEditor(text: $message)
-            
-            Button("Hide keyboard") {
-                dismissKeyboard = false
-            }
+            DatePicker(selection: $myDate, label: { Text("Date") })
+                .datePickerStyle(.graphical)
         }
     }
 }
