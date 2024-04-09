c70b408da273042ab7663637d17435a01be0b9d1
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-09 14:02:07 +0900

    Tap Gesture

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,38 +11,43 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State var changeMe1 = false
+    @State var changeMe2 = false
+    @State var changeMe3 = false
     @State var message = ""
     
     var body: some View {
         VStack  {
-            Link(destination: URL(string: "https://www.apple.com")!, label: {
-                Text("Apple")
-            })
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe1 ? .red : .yellow)
+                .onTapGesture {
+                    changeMe1.toggle()
+                }
+            
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe2 ? .red : .yellow)
+                .onTapGesture(count: 2) {
+                    changeMe2.toggle()
+                }
             
             Text(message).padding()
             
-            Menu("Options") {
-                Button("Open", action: openFile)
-                Button("Find", action: findFile)
-                Button("Delete...", action: deleteFile)
-            }
-            .menuStyle(DefaultMenuStyle())
-            .menuOrder(.fixed)
+            Rectangle()
+                .frame(width: 175, height: 125)
+                .foregroundStyle(changeMe3 ? .red : .yellow)
+                .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: { stillPressed in
+                    message = "Long press in progress: \(stillPressed)"
+                }) {
+                    changeMe3.toggle()
+                }
+            
+            
         }
     }
-    
-    func openFile() {
-        message = "Open chosen"
-    }
-    
-    func findFile() {
-        message = "Find chosen"
-    }
-    
-    func deleteFile () {
-        message = "Delete chosen"
-    }
 }
+
 
 #Preview {
     ContentView()
