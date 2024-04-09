c95a5db992596174349253a73047f34ffabbd182
MadCow <lobasketve@naver.com>
2024-04-09 16:26:05 +0900

    List Example

---
+++ HelloSwiftUI/24.04.09/ListExample.swift
@@ -0,0 +1,37 @@
+//
+//  ListExample.swift
+//  HelloSwiftUI
+//
+//  Created by MadCow on 2024/4/9.
+//
+
+import SwiftUI
+
+struct ListExample: View {
+    var myArray = ["Cat", "Dog", "Turtle", "Ferret", "Parrot", "Goldfish", "Lizard", "Canary", "Tarantula", "Hamster"]
+    var body: some View {
+        VStack {
+            List {
+                Text("Cat")
+                Text("Dog")
+                Text("Bird")
+                Text("Reptile")
+                Text("Fish")
+            }
+            List {
+                ForEach(1...25, id: \.self) { index in
+                    Text("Animal #\(index)")
+                }
+            }
+            List {
+                ForEach(0...myArray.count - 1, id: \.self) { index in
+                    Text(myArray[index])
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ListExample()
+}
