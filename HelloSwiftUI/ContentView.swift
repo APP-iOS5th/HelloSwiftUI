0f91b521c18b21daee112314003dd2baba40c38d
ksiomng0929 <nadana0929@gmail.com>
2024-04-09 15:51:06 +0900

    Turtle Rock2

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,11 +1,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var myColor = Color.gray
     
     var body: some View {
         VStack {
-         
+         Rectangle()
+                .foregroundColor(myColor)
         }
+        
+        Text("Pick a color")
+            .padding()
+            .contextMenu {
+                Button("Red", action: {
+                    myColor = Color.red
+                })
+                Button("Purple", action: purple)
+                Button("Green", action: green)
+                Button("Orange", action: orange)
+            }
+    }
+    func purple() {
+        myColor = Color.purple
+    }
+    func green() {
+        myColor = Color.green
+    }
+    func orange() {
+        myColor = Color.orange
     }
 }
 
