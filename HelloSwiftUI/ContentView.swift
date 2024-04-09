6f0aa00ce7c838527c69ca820ceea7e8a8d24bd7
jmbae <jmbae@codegrove.co.kr>
2024-04-09 17:08:39 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,21 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var messageOne = ""
-    @State var messageTwo = ""
-    
     var body: some View {
         VStack (spacing: 20) {
-            Form {
-                Text("This is the first Form")
-                TextField("Type here", text: $messageOne)
-            }
-            Form {
-                Text("This is the second Form")
-                TextField("Type here", text: $messageTwo)
-            }
-            Text("Form #1 = \(messageOne)")
-            Text("Form #2 = \(messageTwo)")
+
         }
     }
 }
