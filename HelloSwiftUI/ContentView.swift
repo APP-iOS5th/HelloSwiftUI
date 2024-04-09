610be579cb21ca827f6d65740d4ff387c454ea0d
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-09 10:08:44 +0900

    Keyboard Dismiss

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -6,9 +6,10 @@
 //
 
 import SwiftUI
-
+@available(iOS 15.0, *) // 15 이하로는 실행 안되게하는?
 struct ContentView: View {
     @State private var message = ""
+    @FocusState var dismissKeyboard: Bool
     var body: some View {
         VStack  {
             TextField("Placehloder text", text: $message) //달러사인(4)은 뷰에서 나온 텍스트가 메세지에 연결되게끔 바인딩시켜주는것
@@ -17,12 +18,15 @@ struct ContentView: View {
                 .textContentType(.emailAddress)
                 .submitLabel(.send)
                 .padding()
+                .focused($dismissKeyboard)
             
             SecureField("Password", text: $message) //같은 메세지 바인딩이 되어있어서 연동됨
                 .textFieldStyle(.roundedBorder)
                 .padding()
             
-            
+            Button("Hide Keyboard") {
+                dismissKeyboard = false
+            }
             
             
             
