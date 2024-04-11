8188c4b8801c735e40db245d5f4662ae8cda7057
DopeKS <kyusang.dev@gmail.com>
2024-04-11 11:07:57 +0900

    NavigationLink

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,10 +8,22 @@
 
 import SwiftUI
 
+struct FlieView: View {
+    var choice: String
+    
+    var body: some View {
+        VStack {
+            Text("선택 = \(choice)")
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            
+            NavigationLink(destination: FlieView(choice: "헤드")) {
+                Text("헤드 선택")
+            }
         }
     }
 }
