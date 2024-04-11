338689f82b274da8e0adaf862b694386f90115f3
ksiomng0929 <nadana0929@gmail.com>
2024-04-11 09:40:20 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -3,11 +3,7 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        ScrollView(Axis.Set.vertical, showsIndicators: true) {
-            ForEach(0..<50) {
-                Text("Item #\($0)")
-            }
-        }
+        
     }
 }
 
