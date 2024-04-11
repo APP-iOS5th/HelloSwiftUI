d9bd8ab554f3bdf8700ce6d250a3b0f8edf65ef5
ksiomng0929 <nadana0929@gmail.com>
2024-04-11 17:18:05 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,18 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var changeMe = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "tortoise.fill")
-                .font(.system(size: 100))
-                .foregroundStyle(.purple)
-                .scaleEffect(changeMe ? 1.75 : 1)
-                .animation(.default, value: changeMe)
-                .frame(width: 200, height: 200)
-            Toggle(isOn: $changeMe, label: {
-                Text("Toggle me")
-            })
+            
         }
     }
 }
