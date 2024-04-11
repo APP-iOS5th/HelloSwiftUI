cae7e7d445fec332c8a5c83f5a97bc0f26f1fb09
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-11 16:15:04 +0900

    tabview style

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,30 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State var selectedView = 1
+
     var body: some View {
-        TabView {
-            Text("One")
-                .tabItem {
-                    Image(systemName: "heart.fill")
-                    Text("One")
+        VStack {
+            HStack {
+                ForEach(1..<5) { index in
+                    Button("\(index)") {
+                        selectedView = index
+                    }
                 }
-            Text("Two")
-                .tabItem {
-                    Image(systemName: "hare.fill")
-                    Text("Two")
-                }
-            Text("Three")
-                .tabItem {
-                    Image(systemName: "tortoise.fill")
-                    Text("Three")
-                }
-            Text("Four")
-                .tabItem {
-                    Image(systemName: "folder.fill")
-                    Text("Four")
-                }
+            }
+            TabView (selection: $selectedView) {
+                Text("One")
+                    .tabItem {
+                        Image(systemName: "heart.fill")
+                        Text("One")
+                    }.tag(1)
+                
+                Text("Two")
+                    .tabItem {
+                        Image(systemName: "hare.fill")
+                        Text("Two")
+                    }.tag(2)
+                
+                Text("Three")
+                    .tabItem {
+                        Image(systemName: "tortoise.fill")
+                        Text("Three")
+                    }.tag(3)
+                
+                
+                Text("Four")
+                    .tabItem {
+                        Image(systemName: "folder.fill")
+                        Text("Four")
+                    }.tag(4)
+            }
+            .tabViewStyle(.page)
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         }
+        .tint(.teal)
     }
 }
 
