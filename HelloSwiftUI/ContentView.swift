f792dc79ab3040f4056f31ea0021bbda0b5b1dc7
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-11 17:20:12 +0900

    Rotation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,45 +1,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var myDegress: Double = 0.0
+    @State var flag = false
     var body: some View {
         VStack{
-            let gridItems = [GridItem(.fixed(25)),                 GridItem(.fixed(25)),                 GridItem(.fixed(25))]
-            ScrollView (Axis.Set.horizontal, showsIndicators: true){
-                LazyHGrid(rows: gridItems) {
-                    Image(systemName: "1.circle")
-                    Image(systemName: "2.circle")
-                    Image(systemName: "3.circle")
-                    Image(systemName: "4.circle")
-                    Image(systemName: "5.circle")
-                    Image(systemName: "6.circle")
-                    Image(systemName: "7.circle")
-                    Image(systemName: "8.circle")
-                    Image(systemName: "9.circle")
-                    Image(systemName: "10.circle")
-                }
-                .font(.largeTitle)
+            Text("Hello, world!")
+                .padding()
+                .rotationEffect(Angle(degrees: flag ? myDegress : 0))
+                .animation(.default, value: flag)
+            Button("Animate now") {
+                flag.toggle()
             }
+            Slider(value: $myDegress, in: -180...180, step: 1)
+                .padding()
             
-            ScrollView (Axis.Set.vertical, showsIndicators: true){
-                LazyVGrid(columns: gridItems) {
-                    Image(systemName: "1.square")
-                    Image(systemName: "2.square")
-                    Image(systemName: "3.square")
-                    Image(systemName: "4.square")
-                    Image(systemName: "5.square")
-                    Image(systemName: "6.square")
-                    Image(systemName: "7.square")
-                    Image(systemName: "8.square")
-                    Image(systemName: "9.square")
-                    Image(systemName: "10.square")
-                }
-                .font(.largeTitle)
-            }
         }
     }
 }
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
