dad12337de42d7d4c7510f403f1a996282554fed
Jongchan Yun <jcyun1996@gmail.com>
2024-04-11 15:22:17 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,38 +7,3 @@
 
 import SwiftUI
 
-
-struct FileView: View {
-    @Binding var choice: String
-    var body: some View {
-        HStack {
-            Spacer()
-            VStack {
-                Spacer()
-                TextField("Type here:", text: $choice)
-                Spacer()
-            }
-            Spacer()
-        }.background(Color.cyan)
-    }
-}
-
-struct ContentView: View {
-    @State var message = ""
-    var body: some View {
-        NavigationStack {
-            TextField("Type here:", text: $message)
-            NavigationLink(destination: FileView(choice: $message)) {
-                Text("Heads")
-            }
-            NavigationLink(destination: SeparateFile(passedData: "Tails")) {
-                Text("Tails")
-            }
-            .navigationTitle("Flip a Coin")
-        }
-    }
-}
-
-#Preview {
-    ContentView()
-}
