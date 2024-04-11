7f12966f25e4fad6c5d7bfd6a8023936a2be748b
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-11 09:26:46 +0900

    DisclosureGroup

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,23 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message1 = ""
-    @State private var message2 = ""
+    @State private var sliderValue = 0.0
+    @State private var message = ""
+    @State var flag = true
     
     var body: some View {
-        VStack {
-            Form {
-                Text("This is the first Form")
-                TextField("Type here", text: $message1)
-            }
-            Form {
-                Text("This is the second Form")
-                TextField("Type here", text: $message2)
-                    .autocorrectionDisabled()
-            }
-            Text("Form #1 \(message1)")
-            Text("Form #2 \(message2)")
+        DisclosureGroup("펼치기") {
+            Text("입력한 내용 \(message)")
+            TextField("Type here", text: $message)
+                .padding()
+            Text(flag ? "True" : "False")
+            Toggle(isOn: $flag, label: {
+                Text("Toggle")
+            })
+            Text("The slider value = \(sliderValue)")
+            Slider(value: $sliderValue, in: 0...15)
+                .padding()
         }
+        .padding()
     }
 }
 
