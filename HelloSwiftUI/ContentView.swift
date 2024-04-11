763a35002a237eeba7ed9d01d2c3b83c340b80a7
yehjinjang <j95033176@gmail.com>
2024-04-11 16:28:21 +0900

    Grid

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,50 +12,43 @@ struct ContentView: View{
     @State var selectedView = 1
     
     var body: some View{
-        VStack {
-            HStack{
-                Button("1") {
-                    selectedView = 1
+        VStack{
+            //가로로 출력하는 그리드아이템은 3개의 기준으로 나뉘어짐 (3개로 설정했기때문)
+            let gridItems = [GridItem(),GridItem(),GridItem()]
+            ScrollView(Axis.Set.horizontal, showsIndicators: true ){
+                LazyHGrid(rows: gridItems){
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                    Image(systemName: "4.circle")
+                    Image(systemName: "5.circle")
+                    Image(systemName: "6.circle")
+                    Image(systemName: "7.circle")
+                    Image(systemName: "8.circle")
+                    Image(systemName: "9.circle")
+                    Image(systemName: "10.circle")
                 }
-                Button("2") {
-                    selectedView = 2
-                }
-                Button("3") {
-                    selectedView = 3
-                }
-                Button("4") {
-                    selectedView = 4
-                }
-                
+                .font(.largeTitle)
             }
-            TabView (selection: $selectedView){
-                Text("One")
-                    .tabItem {
-                        Image(systemName: "heart.fill")
-                        Text("One")
-                    }.tag(1)
-                Text("Two")
-                    .tabItem {
-                        Image(systemName: "hare.fill")
-                        Text("Two")
-                    }.tag(2)
-                Text("Three")
-                    .tabItem {
-                        Image(systemName: "tortoise.fill")
-                        Text("Three")
-                    }.tag(3)
-                Text("Four")
-                    .tabItem {
-                        Image(systemName: "tortoise.fill")
-                        Text("Four")
-                    }.tag(4)
+            ScrollView(Axis.Set.vertical, showsIndicators: true ){
+                LazyVGrid(columns: gridItems){
+                    Image(systemName: "1.square")
+                    Image(systemName: "2.square")
+                    Image(systemName: "3.square")
+                    Image(systemName: "4.square")
+                    Image(systemName: "5.square")
+                    Image(systemName: "6.square")
+                    Image(systemName: "7.square")
+                    Image(systemName: "8.square")
+                    Image(systemName: "9.square")
+                    Image(systemName: "10.square")
+                }
+                .font(.title)
             }
-            .tabViewStyle(.page)
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         }
-        .tint(.pink)
     }
 }
+
 #Preview {
     ContentView()
 }
