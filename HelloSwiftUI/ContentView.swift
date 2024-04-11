45a277c5f06aa7fe94b9f1963f1d68b33bfc4f0a
DopeKS <kyusang.dev@gmail.com>
2024-04-11 17:16:39 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,17 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var changeMe = true
-    
     var body: some View {
-        Image(systemName: "tortoise.fill")
-            .font(.system(size: 100))
-            .foregroundStyle(.red)
-            .scaleEffect(changeMe ? 1.75 : 1)
-            .animation(.default, value: changeMe)
-            .onTapGesture {
-                changeMe.toggle()
-            }
+        VStack {
+            
+        }
     }
 }
 
