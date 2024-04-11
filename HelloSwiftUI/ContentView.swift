642db1ffb0c6859c425aeaea13bfccb094e38c4e
yehjinjang <j95033176@gmail.com>
2024-04-11 11:20:18 +0900

    MovieNaviatorStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -4,27 +4,56 @@
 //
 //  Created by 장예진 on 4/8/24.
 
+
+
 import SwiftUI
 
-struct FileView : View {
-    var choice : String
-    var body : some View {
+struct Movie: Identifiable, Hashable {
+    let id = UUID()
+    let title : String
+    let description : String
+}
+class MovieListViewModel: ObservableObject{
+    @Published var movies : [Movie] = [
+        Movie(title: "movie1", description: "movie1 explanation"),
+        Movie(title: "movie2", description: "movie2 explanation"),
+        Movie(title: "movie3", description: "movie3 explanation"),
+        
+    ]
+}
+
+struct MovieDetailView: View {
+    
+    let movie : Movie
+    
+    var body: some View {
         VStack {
-            Text("select = \(choice)")
+            Text(movie.title)
+                .font(.title)
+            Text(movie.description)
+                .padding()
         }
     }
 }
 
 struct ContentView: View {
-    
+    //view model은 movie 라는 객체를 가진단 얘기
+    @StateObject private var viewModel = MovieListViewModel()
     var body: some View {
         NavigationStack {
-            NavigationLink(destination:  FileView(choice: "head")){
-            Text("select Head")
+            List(viewModel.movies){ movie in
+                NavigationLink(movie.title, value: movie)
+            }
+            .navigationTitle("movie list")
+            //modifier
+            .navigationDestination(for: Movie.self) { movie in
+                MovieDetailView(movie: movie)
             }
         }
     }
 }
+
+
 #Preview {
     ContentView()
 }
