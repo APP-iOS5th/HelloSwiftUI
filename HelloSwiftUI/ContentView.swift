04b7423e0ee66962e651d7a0c554f1c76b84ef8a
yehjinjang <j95033176@gmail.com>
2024-04-11 09:15:09 +0900

    0411 DisclosureGroup

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -5,38 +5,27 @@
 //  Created by 장예진 on 4/8/24.
 
 import SwiftUI
-//UUID 범용고유 식별자
-struct ContentView: View {
-    struct Animal : Identifiable{
-        var id = UUID()
-        var name: String
-    }
-    
-    struct AnimalCategory: Identifiable{
-        var id = UUID()
-        var category: String
-        var animals: [Animal]
-    }
-    //연관관계가있는 데이터를 이중배열 처럼 사용
-    let categories = [
-        AnimalCategory(category: "mammals", animals: [Animal(name: "cat"),Animal(name: "dog")]),
-        AnimalCategory(category: "mammals", animals: [Animal(name: "turtle"),Animal(name: "lizard")])
-        
-    ]
-    var body: some View{
-        VStack{
-            List{
-                ForEach(categories, id: \.id) { c in
-                    Section(header: Text(c.category)){
-                        ForEach(c.animals, id: \.id){ animal in
-                            Text(animal.name)
-                        }
-                        
-                    }
-                }
-            }
 
-         }
+struct ContentView: View {
+    @State var sliderValue = 0.0
+    @State var message = ""
+    @State var flag = true
+    
+    var body: some View {
+        DisclosureGroup("Expand it"){
+            Text("insert = \(message)")
+            TextField("Type here",text :$message)
+                .padding()
+            Text(flag ? "Toggle = true" : "Toggle = false")
+            Toggle(isOn: $flag) {
+                Text("Toggle")
+            }
+            // 삼항연산자 
+            Text("The slider value = \(sliderValue)")
+            Slider(value:   $sliderValue, in : 0...15)
+                .padding()
+        }
+        .padding()
     }
 }
 #Preview {
