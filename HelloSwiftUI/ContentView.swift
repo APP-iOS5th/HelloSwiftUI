cb2218529ea499b5a5babc0347ac46497e3ec147
Jin Park <jinpark1504@gmail.com>
2024-04-11 17:39:47 +0900

    Animation transition and withAnimation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,16 +1,30 @@
 
 import SwiftUI
 struct ContentView: View {
-    @State var myDegrees: Double = 0.0
+    @State var myDegrees: Double = -180.0
     @State var flag = false
     var body: some View {
         VStack {
             Text("Hello, world!")
                 .padding()
                 .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
-                .animation(.easeInOut(duration: 2), value: flag)
+//                .animation(.easeIn(duration: 2), value: flag)
+            Text("Hello, world!")
+                .padding()
+                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
+//                .animation(.easeOut(duration: 2), value: flag)
+            Text("Hello, world!")
+                .padding()
+                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
+//                .animation(.easeInOut(duration: 2), value: flag)
+            Text("Hello, world!")
+                .padding()
+                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
+//                .animation(.default, value: flag)
             Button("Animate now"){
-                flag.toggle()
+                withAnimation(.easeInOut(duration: 2)) {
+                    flag.toggle()
+                }
             }
             Slider(value: $myDegrees, in: -180...180, step: 1)
                 .padding()
