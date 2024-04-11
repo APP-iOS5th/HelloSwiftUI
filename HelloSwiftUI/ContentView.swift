8d489f3881e78d025f3ef656de59ab3b527cc4ac
jmbae <jmbae@codegrove.co.kr>
2024-04-11 10:41:15 +0900

    NavigationStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var flag = true
+    @State var message = ""
+    
     var body: some View {
-        VStack {
-            
+        NavigationStack {
+            Text(message)
+            Toggle(isOn: $flag, label: {
+                Text("토글 디스플레이 모드")
+            })
+            .navigationTitle("네비게이션 타이틀")
+//            .navigationBarHidden(false)
+            .navigationBarTitleDisplayMode(flag ? .large : .inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        message = "iCloud 아이콘 탭됨"
+                    } label: {
+                        Image(systemName: "icloud")
+                    }
+                }
+            }
         }
+        .accentColor(.purple)
     }
 }
 
