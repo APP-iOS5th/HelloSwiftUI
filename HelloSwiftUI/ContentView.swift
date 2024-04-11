86f867346e3cff5bf802e795a48f9340c808cd0e
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-11 11:09:32 +0900

    Navigation Link

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,47 +9,23 @@ import SwiftUI
 
 @available(iOS 15.0, *) // 15 이하로는 실행 안되게하는?
 
-class Species: Identifiable {
-    let id = UUID()
-    var name: String
-    var classification: [Species]?
-    init(name: String, classification: [Species]? = nil) { //nil이 없으면 무조건 있어야함
-        self.name = name
-        self.classification = classification
+struct FileView: View {
+    var choice: String
+    
+    var body: some View {
+        VStack {
+            Text("선택 = \(choice)")
+        }
     }
 }
 
-
 struct ContentView: View {
-    @State var flag = true
-    @State var message = ""
     var body: some View {
         NavigationStack {
-            Text(message)
-            Toggle(isOn: $flag, label: {
-                Text("Toggle display mode")
-            }) //달러를 왜 써야한다고? 왜 바로 핸들링 못한다고?
-            .navigationTitle("Navigation Title")
-            //.navigationBarTitleDisplayMode(flag ? .large : .inline)
-            .navigationBarHidden(flag)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button {
-                        message = "iCloud 아이콘 탭 됨"
-                    } label: {
-                        Image(systemName: "icloud")
-                    }
-                }
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        message = "완료 버튼 탭 됨"
-                    } label: {
-                        Text("완료")
-                    }
-                }
+            NavigationLink(destination: FileView(choice: "헤드")) {
+                Text("헤드 선택")
             }
         }
-        .accentColor(.purple)
     }
 }
 
