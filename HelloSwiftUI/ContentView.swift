aee247a93f59d4dc79d3dff4ad7494bbe715241e
seonggeun an <eventscripts@nate.com>
2024-04-11 11:44:26 +0900

    FileView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,13 +7,39 @@
 
 import SwiftUI
 
+//ObservalObject 는 추적,관찰 가능한 Rx디자인
+class ShareString: ObservableObject {
+    @Published var message = ""
+}
+
+struct FileView: View {
+    var body: some View {
+           HStack {
+               Spacer()
+               VStack {
+                   Spacer()
+                   Text("This is a separate structure")
+                   Text("that's stored in the same file")
+                   Spacer()
+               }
+               Spacer()
+           }.background(Color.yellow)
+       }
+   }
+    
+
 
 struct ContentView: View {
     
+    @StateObject var showMe = ShareString()
+    
     var body: some View {
         NavigationStack{
-            
+            NavigationLink(destination: FileView()) {
+                Text("Send Message")
+            }
         }
+        .environmentObject(showMe)
     }
 }
 
