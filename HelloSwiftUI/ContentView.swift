629f154198a5ca3953e1224c46de7d74f05a4c2c
ksiomng0929 <nadana0929@gmail.com>
2024-04-11 11:10:12 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,23 +1,9 @@
 import SwiftUI
 
-struct FileView: View {
-    var choice: String
-    
-    var body: some View {
-        VStack {
-            Text("선택 = \(choice)")
-        }
-    }
-}
-
 struct ContentView: View {
     
     var body: some View {
-        NavigationStack {
-            NavigationLink(destination: FileView(choice: "헤드")) {
-                Text("헤드 선택")
-            }
-        }
+        
     }
 }
 
