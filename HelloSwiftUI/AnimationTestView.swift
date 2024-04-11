904267bc6c9d1c91c17eedfb86db0084c9304393
Ruel <c0407h@gmail.com>
2024-04-11 17:36:48 +0900

    Animation option and withAnimation Block

--- HelloSwiftUI/AnimationTestView.swift
+++ HelloSwiftUI/AnimationTestView.swift
@@ -14,13 +14,66 @@ struct AnimationTestView: View {
     
     var body: some View {
         VStack {
-            Text("Hello, World")
-                .padding()
-                .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
-                .animation(.default, value: flag)
+            
+            LazyVGrid(columns: [GridItem(.fixed(200)),GridItem(.fixed(200))], content: {
+                Text("Hello, World")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.default, value: flag)
+                
+                Text("bouncy")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.bouncy, value: flag)
+                
+                Text("easeIn")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.easeIn, value: flag)
+                
+                Text("easeInOut")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.easeInOut, value: flag)
+                
+                Text("easeOut")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.easeOut, value: flag)
+                
+                Text("interactiveSpring")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.interactiveSpring, value: flag)
+                
+                Text("interpolatingSpring")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.interpolatingSpring, value: flag)
+                
+                Text("linear")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.linear, value: flag)
+                
+                Text("smooth")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.smooth, value: flag)
+                
+                Text("snappy")
+                    .padding()
+                    .rotationEffect(Angle.degrees(flag ? myDgrees : 0))
+//                    .animation(.snappy, value: flag)
+            })
+            
+            
             
             Button("Animate now") {
-                flag.toggle()
+                withAnimation(.easeInOut(duration: 2.1)) {
+                    flag.toggle()
+                }
+                
             }
             
             Slider(value: $myDgrees, in: -180...180, step: 1)
