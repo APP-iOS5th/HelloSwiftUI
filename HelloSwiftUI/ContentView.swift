f19ab9f8e6a0cea7ba610305eb7fb8a6e087201d
YuleGlycerine <figurefig13@gmail.com>
2024-04-11 17:40:51 +0900

    ease in&out

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,24 +1,44 @@
+
+
 import SwiftUI
 
- struct ContentView: View {
-     @State var myDegree: Double = 0.0
-     @State var flag = false
+struct ContentView: View {
+    @State var myDegrees: Double = -180.0
+    @State var flag = false
     
-     var body: some View {
-         VStack {
-             Text("Hello, world!")
-                 .padding()
-                 .rotationEffect(Angle(degrees: flag ? myDegree : 0))
-                 .animation(.default, value: flag)
-             Button("Animate now"){
-                 flag.toggle()
-             }
-             Slider(value: $myDegree, in: -180...180, step: 1)
-                 .padding()
-             }
-         
-     }
- }
+    var body: some View {
+        VStack (spacing: 50) {
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+                                    .animation(.default, value: flag)
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+                                    .animation(.easeIn, value: flag)
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+                                    .animation(.easeOut, value: flag)
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+                                    .animation(.easeInOut, value: flag)
+            
+            Button("Animate now") {
+              //  withAnimation(.easeInOut(duration: 2.1)) {
+                    flag.toggle()
+                }
+            }
+            Slider(value: $myDegrees, in: -180...180, step: 1)
+                .padding()
+        }
+    }
+
 #Preview {
     ContentView()
 }
