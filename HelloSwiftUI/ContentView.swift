846746b10fd6095023fde45ba3eff075630d8dd5
Hyunseo Yook <hhh3332@naver.com>
2024-04-11 17:21:01 +0900

    rotation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,17 +10,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var changeMe = true
+    @State var myDegree: Double = 0.0
+    @State var flag = false
+    
     var body: some View {
         VStack{
-            Image(systemName: "tortoise.fill")
-                .font(.system(size: 100))
-                .foregroundColor(.green)
-                .scaleEffect(changeMe ? 1.75 : 1)
-                .animation(.default, value: changeMe)
-                .onTapGesture {
-                    changeMe.toggle()
-                }
+            Text("Hello, world!")
+                .padding()
+                .rotationEffect(Angle(degrees: flag ? myDegree : 0))
+                .animation(.default, value: flag)
+            
+            Button("Animate now") {
+                flag.toggle()
+            }
+            Slider(value: $myDegree, in: -180...180, step:1)
+                .padding()
                         
         }
     }
