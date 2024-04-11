17b29fb9fd2f18e749a3380f7796859bed39ba79
jihye <ganzi410@gmail.com>
2024-04-11 14:01:25 +0900

    State, Binding

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,47 +7,34 @@
 
 import SwiftUI
 
-struct Movie: Identifiable, Hashable {
-    let id = UUID()
-    let title: String
-    let description: String
-}
-
-class MovieListViewModel {
-    var movies: [Movie] = [
-        Movie(title: "영화 1", description: "영화 1 설명"),
-        Movie(title: "영화 2", description: "영화 2 설명"),
-        Movie(title: "영화 3", description: "영화 3 설명")
-    ]
-}
-
-struct MovieDetailView: View {
-    let movie: Movie
+struct FileView: View {
+    @Binding var choice: String
     
     var body: some View {
-        VStack {
-            Text(movie.title)
-                .font(.title)
-            Text(movie.description)
-                .padding()
-        }
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                TextField("You Choose : ", text: $choice)
+                Spacer()
+            }
+            Spacer()
+        }.background(Color.yellow)
     }
 }
 
 struct ContentView: View {
-    private var viewModel = MovieListViewModel()
-    
-    
+    @State var message = ""
     var body: some View {
         NavigationStack {
-            List(viewModel.movies) { movie in
-                NavigationLink(movie.title, value: movie)
+            TextField("Heads or Tails", text: $message)
+            NavigationLink(destination: FileView(choice: $message)) {
+                Text("Heads")
             }
-            .navigationTitle("영화 목록")
-            .navigationDestination(for: Movie.self) { movie in
-                //                MovieDetail
-                MovieDetailView(movie: movie)
+            NavigationLink(destination: SeperateFile(passedData: "Tails")) {
+                Text("Tails")
             }
+            .navigationTitle("Flip a Coin")
         }
     }
 }
