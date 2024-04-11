0672ae6c7fd64dab796c56212ead64a63abc6248
yehjinjang <j95033176@gmail.com>
2024-04-11 16:05:24 +0900

    Tab selection with params

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -6,33 +6,53 @@
 
 import SwiftUI
 
+
 struct ContentView: View{
+    
+    @State var selectedView = 1
+    
     var body: some View{
-        TabView {
-            Text("One")
-                .tabItem {
-                    Image(systemName: "heart.fill")
-                    Text("One")
+        VStack {
+            HStack{
+                Button("1") {
+                    selectedView = 1
                 }
-            Text("Two")
-                .tabItem {
-                    Image(systemName: "hare.fill")
-                    Text("Two")
+                Button("2") {
+                    selectedView = 2
                 }
-            Text("Three")
-                .tabItem {
-                    Image(systemName: "tortoise.fill")
-                    Text("Three")
+                Button("3") {
+                    selectedView = 3
                 }
-            Text("Four")
-                .tabItem {
-                    Image(systemName: "tortoise.fill")
-                    Text("Four")
+                Button("4") {
+                    selectedView = 4
                 }
+                
+            }
+            TabView (selection: $selectedView){
+                Text("One")
+                    .tabItem {
+                        Image(systemName: "heart.fill")
+                        Text("One")
+                    }.tag(1)
+                Text("Two")
+                    .tabItem {
+                        Image(systemName: "hare.fill")
+                        Text("Two")
+                    }.tag(2)
+                Text("Three")
+                    .tabItem {
+                        Image(systemName: "tortoise.fill")
+                        Text("Three")
+                    }.tag(3)
+                Text("Four")
+                    .tabItem {
+                        Image(systemName: "tortoise.fill")
+                        Text("Four")
+                    }.tag(4)
+            }
         }
     }
 }
-
 #Preview {
     ContentView()
 }
