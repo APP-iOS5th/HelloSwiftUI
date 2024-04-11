2810f33abaa2c22b4a7f516ad95792d4baef40d1
Hyunseo Yook <hhh3332@naver.com>
2024-04-11 16:09:02 +0900

    Tab Selection with params

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,34 +10,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedView = 1
     var body: some View {
-        TabView {
-            Text("One")
-                .tabItem {
-                    Image(systemName: "heart.fill")
+        VStack{
+            HStack{
+                Button("1") {
+                    selectedView = 1
+                }
+                Button("2") {
+                    selectedView = 2
+                }
+                Button("3") {
+                    selectedView = 3
+                }
+                Button("4") {
+                    selectedView = 4
+                }
+            }
+            TabView(selection: $selectedView) {
                     Text("One")
-                }
-            
-            Text("Two")
-                .tabItem {
-                    Image(systemName: "heart.fill")
+                        .tabItem {
+                            Image(systemName: "heart.fill")
+                            Text("One")
+                        }.tag(1)
+                    
                     Text("Two")
-                }
-  
-            Text("Three")
-                .tabItem {
-                    Image(systemName: "heart.fill")
+                        .tabItem {
+                            Image(systemName: "hare.fill")
+                            Text("Two")
+                        }.tag(2)
+                    
                     Text("Three")
-                }
-  
-            Text("Four")
-                .tabItem {
-                    Image(systemName: "heart.fill")
+                        .tabItem {
+                            Image(systemName: "tortoise.fill")
+                            Text("Three")
+                        }.tag(3)
+                    
                     Text("Four")
-                }
-  
-  
+                        .tabItem {
+                            Image(systemName: "folder.fill")
+                            Text("Four")
+                        }.tag(4)
+                    
+                    Text("Five")
+                        .tabItem {
+                            Image(systemName: "internaldrive.fill")
+                            Text("Five")
+                        }.tag(5)
+                    
+                    Text("Six")
+                        .tabItem {
+                            Image(systemName: "cloud.drizzle.fill")
+                            Text("Six")
+                        }.tag(6)
+            }
         }
+        .tint(.black)
     }
 }
 
