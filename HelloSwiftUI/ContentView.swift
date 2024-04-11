236905c5c0de153bbefb9b0b9e3e74d48b1e3a61
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-11 09:38:14 +0900

    scrollView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,24 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sliderValue = 0.0
-    @State private var message = ""
-    @State var flag = true
     
     var body: some View {
-        DisclosureGroup("펼치기") {
-            Text("입력한 내용 \(message)")
-            TextField("Type here", text: $message)
-                .padding()
-            Text(flag ? "True" : "False")
-            Toggle(isOn: $flag, label: {
-                Text("Toggle")
-            })
-            Text("The slider value = \(sliderValue)")
-            Slider(value: $sliderValue, in: 0...15)
-                .padding()
+        ScrollView(Axis.Set.vertical, showsIndicators: true) {
+            ForEach(0..<50) {
+                Text("Item #\($0)")
+            }
         }
-        .padding()
     }
 }
 
