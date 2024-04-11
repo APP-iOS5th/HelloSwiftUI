debc21b7196267e24f36403d80689871d882b592
avan31017 <avan31017@gmail.com>
2024-04-11 17:20:17 +0900

    animation rotation

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -11,17 +11,23 @@ import SwiftUI
 @available(iOS 15.0, *)
 struct ContentView: View {
     
-    @State var changeMe = true
+    @State var myDegrees: Double = 0.0
+    @State var flag = false
     
     var body: some View {
-        Image(systemName: "person.fill")
-            .font(.system(size: 100))
-            .foregroundColor(changeMe ? .red : .green)
-            .scaleEffect(changeMe ? 1.75 : 1)
-            .animation(.default, value: changeMe)
-            .onTapGesture {
-                changeMe.toggle()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
+                .animation(.default, value: flag)
+            Button(action: {
+                flag.toggle()
+            }) {
+                Text("Animate now")
             }
+            Slider(value: $myDegrees, in: -180...180, step: 1)
+                .padding()
+        }
     }
 }
 //#Preview {
