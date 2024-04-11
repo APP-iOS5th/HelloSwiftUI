466fec94089544c9ae377c4c8566ee753e8f5829
ksiomng0929 <nadana0929@gmail.com>
2024-04-11 11:29:51 +0900

    NavigationStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,9 +1,45 @@
 import SwiftUI
 
-struct ContentView: View {
+struct Movie: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let description: String
+}
+
+class MovieListViewModel: ObservableObject {
+    var movies: [Movie] = [ //
+        Movie(title: "영화 1", description: "영화 1 설정"),
+        Movie(title: "영화 2", description: "영화 2 설정"),
+        Movie(title: "영화 3", description: "영화 3 설정")
+    ]
+}
+
+struct MovieDetailView: View {
+    let movie: Movie
     
     var body: some View {
-        
+        VStack {
+            Text(movie.title)
+                .font(.title)
+            Text(movie.description)
+                .padding()
+        }
+    }
+}
+
+struct ContentView: View {
+    private var viewModel = MovieListViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            List(viewModel.movies) { movie in
+                NavigationLink(movie.title, value: movie)
+            }
+            .navigationTitle("영화 목록")
+            .navigationDestination(for: Movie.self) { movie in
+                MovieDetailView(movie: movie)
+            }
+        }
     }
 }
 
