3e9b9b8fb78b4f71c0eb9bcb8bfa735f4e253994
SungWoon Lee <lsw.netline@gmail.com>
2024-04-11 11:28:00 +0900

    목록을 누르면 상세 페이지로 이동

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,28 +1,47 @@
 import SwiftUI
 
 
-struct FileView: View {
-    var choice: String
-    
-    var body: some View {
-        
-        VStack{
-            Text("선택 = \(choice)")
-            
-        }
-    }
+struct Movie: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let description: String
 }
+
+class MovieListModel: ObservableObject {
+    @Published var movies: [Movie] = [
+        Movie(title: "영화 1", description: "영화 1 설명"),
+        Movie(title: "영화 2", description: "영화 2 설명"),
+        Movie(title: "영화 3", description: "영화 3 설명")
+    ]
+}
+
+struct MovieDetailView: View {
+     let movie: Movie
+
+     var body: some View {
+         VStack {
+             Text(movie.title)
+                 .font(.title)
+             Text(movie.description)
+                 .padding()
+         }
+     }
+ }
 
 struct ContentView: View {
+    @StateObject private var viewModel = MovieListModel()
+    
     var body: some View {
         NavigationStack {
-            NavigationLink(destination: FileView(choice: "헤드")) {
-                Text("헤드 선택")
+            List(viewModel.movies) { movie in
+                NavigationLink(movie.title, value: movie)
             }
+            .navigationTitle("영화 목록")
+            .navigationDestination(for: Movie.self) {movie in MovieDetailView(movie: movie)
         }
     }
 }
-
+}
 #Preview {
     ContentView()
 }
