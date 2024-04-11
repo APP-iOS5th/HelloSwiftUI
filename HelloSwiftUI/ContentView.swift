a705639cf8bd47fb2c178d2ef368f2ba4eeada0c
wotjs020708 <wotjs020708@gmail.com>
2024-04-11 17:06:13 +0900

    animation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,40 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var move = true
     var body: some View {
         VStack {
-            let gridItems = [GridItem(.fixed(25)), GridItem(.fixed(25)), GridItem(.fixed(25))]
-            ScrollView(Axis.Set.horizontal, showsIndicators: true){
-                LazyHGrid(rows: gridItems){
-                    Image(systemName: "1.circle")
-                    Image(systemName: "2.circle")
-                    Image(systemName: "3.circle")
-                    Image(systemName: "4.circle")
-                    Image(systemName: "5.circle")
-                    Image(systemName: "6.circle")
-                    Image(systemName: "7.circle")
-                    Image(systemName: "8.circle")
-                    Image(systemName: "9.circle")
-                    Image(systemName: "10.circle")
-                }
-                .font(.largeTitle)
-            }
-            ScrollView(Axis.Set.vertical, showsIndicators:  true){
-                LazyVGrid(columns: gridItems){
-                    Image(systemName: "1.square")
-                    Image(systemName: "2.square")
-                    Image(systemName: "3.square")
-                    Image(systemName: "4.square")
-                    Image(systemName: "5.square")
-                    Image(systemName: "6.square")
-                    Image(systemName: "7.square")
-                    Image(systemName: "8.square")
-                    Image(systemName: "9.square")
-                    Image(systemName: "10.square")
-                }
-                .font(.largeTitle)
-            }
+           
+            Text("A Text view")
+                .offset(x: move ? 100 : 0, y: move ? 100 : 0)
+                .animation(.default, value: move)
+                .frame(width: 300, height: 300)
+            Toggle(isOn: $move, label: {
+                Text("Toggle me")
+            })
         }
+        
     }
 }
 
