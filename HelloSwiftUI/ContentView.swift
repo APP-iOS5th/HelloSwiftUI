12d3f177ad4d67fb3ff3597ca7ce2de95468b5e5
shin-jae-uk <shinju1412@gmail.com>
2024-04-11 15:18:58 +0900

    TabView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,45 +7,30 @@
 
 import SwiftUI
 
-struct Movie: Identifiable, Hashable {
-    let id = UUID()
-    let title: String
-    let description: String
-}
-
-class MovieListViewModel: ObservableObject {
-    @Published var movies: [Movie] = [
-        Movie(title: "영화 1", description: "영화 1 설명"),
-        Movie(title: "영화 2", description: "영화 2 설명"),
-        Movie(title: "영화 3", description: "영화 3 설명")
-    ]
-}
-
-struct MovieDetailView: View {
-    let movie: Movie
-    
-    var body: some View {
-        VStack {
-            Text(movie.title)
-                .font(.title)
-            Text(movie.description)
-                .padding()
-        }
-    }
-}
-
 struct ContentView: View {
-    @StateObject private var viewModel = MovieListViewModel()
     
     var body: some View {
-        NavigationStack {
-            List(viewModel.movies) { movie in
-                NavigationLink(movie.title, value: movie)
-            }
-            .navigationTitle("영화 목록")
-            .navigationDestination(for: Movie.self) { movie in
-                MovieDetailView(movie: movie)
-            }
+        TabView {
+            Text("One")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("One")
+                }
+            Text("Two")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("Two")
+                }
+            Text("Three")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("Three")
+                }
+            Text("Four")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("Four")
+                }
         }
     }
 }
