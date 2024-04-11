0a67352ed0f46287598683e1f39bc88165277624
Jongchan Yun <jcyun1996@gmail.com>
2024-04-11 15:24:22 +0900

    TabView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,3 +7,33 @@
 
 import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("One")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("One")
+                }
+            Text("Two")
+                .tabItem {
+                    Image(systemName: "hare.fill")
+                    Text("Two")
+                }
+            Text("Three")
+                .tabItem {
+                    Image(systemName: "tortoise.fill")
+                    Text("Three")
+                }
+            Text("Four")
+                .tabItem {
+                    Image(systemName: "folder.fill")
+                    Text("Four")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
