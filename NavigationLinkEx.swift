de1a94a583ab6f7555272c003ca0f73d4c19b72d
JongWon Jeong <jjwon214@gmail.com>
2024-04-11 11:06:45 +0900

    NavigationLink

---
+++ NavigationLinkEx.swift
@@ -0,0 +1,34 @@
+//
+//  NavigationLinkEx.swift
+//  HelloSwiftUI
+//
+//  Created by 정종원 on 4/11/24.
+//
+
+import SwiftUI
+
+struct FileView: View {
+    var choice: String
+    
+    var body: some View {
+        VStack {
+            Text("선택")
+        }
+    }
+}
+
+struct NavigationLinkEx: View {
+    var body: some View {
+        VStack {
+            
+            NavigationLink(destination: FileView(choice: "헤드")) {
+                Text("헤드 선택")
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    NavigationLinkEx()
+}
