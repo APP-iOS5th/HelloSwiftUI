502207d025d947d2b2668901fc52c56c28236e7b
Ruel <c0407h@gmail.com>
2024-04-11 09:46:19 +0900

    OutlineGroup

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,20 +7,43 @@
 
 import SwiftUI
 
+class Species: Identifiable {
+    let id = UUID()
+    var name: String
+    var classification: [Species]?
+    
+    init(name: String, classification: [Species]? = nil) {
+        self.name = name
+        self.classification = classification
+    }
+}
 
 struct ContentView: View {
-    @State var sliderValue = 0.0
-    @State var message = ""
-    @State var flag = true
+    var animals: [Species] = [
+    Species(name: "포유류", classification: [
+        Species(name: "개", classification: [
+            Species(name: "푸들"),
+            Species(name: "진도"),
+            Species(name: "비숑")
+        ]),
+        Species(name: "고양이"),
+        Species(name: "코끼리"),
+        Species(name: "고래")
+    ]),
+    Species(name: "조류", classification: [
+        Species(name: "독수리"),
+        Species(name: "참새"),
+        Species(name: "비둘기")
+    ])
+    ]
     
     var body: some View {
-        
-        ScrollView(Axis.Set.vertical, showsIndicators: true) {
-            ForEach(0..<50) {
-                Text("Item #\($0)")
+        List {
+            OutlineGroup(animals, id: \.id, children: \.classification) { creature in
+                Text(creature.name)
             }
+    
         }
-        
 
     }
 
