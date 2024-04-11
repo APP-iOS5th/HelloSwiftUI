32676858605de6b2f3c7faaecbf28062868701e2
jmbae <jmbae@codegrove.co.kr>
2024-04-11 11:32:31 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,45 +7,10 @@
 
 import SwiftUI
 
-struct Movie: Identifiable, Hashable {
-    let id = UUID()
-    let title: String
-    let description: String
-}
 
-class MovieListViewModel {
-    var movies: [Movie] = [
-        Movie(title: "영화 1", description: "영화 1 설명"),
-        Movie(title: "영화 2", description: "영화 2 설명"),
-        Movie(title: "영화 3", description: "영화 3 설명")
-    ]
-}
-
-struct MovieDetailView: View {
-    let movie: Movie
-    
-    var body: some View {
-        VStack {
-            Text(movie.title)
-                .font(.title)
-            Text(movie.description)
-                .padding()
-        }
-    }
-}
-
-struct ContentView: View {
-    private var viewModel = MovieListViewModel()
-    
+struct ContentView: View {    
     var body: some View {
         NavigationStack {
-            List(viewModel.movies) { movie in
-                NavigationLink(movie.title, value: movie)
-            }
-            .navigationTitle("영화 목록")
-            .navigationDestination(for: Movie.self) { movie in
-                MovieDetailView(movie: movie)
-            }
         }
     }
 }
