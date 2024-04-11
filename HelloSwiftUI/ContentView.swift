35a8e68e5040eaf42160efb395e14eea5d42253d
seonggeun an <eventscripts@nate.com>
2024-04-11 15:16:40 +0900

    TabView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,8 +12,27 @@ import SwiftUI
 
  struct ContentView: View {
      var body: some View {
-         VStack{
-             
+         TabView{
+             Text("One")
+                 .tabItem{
+                     Image(systemName: "heart.fill")
+                     Text("One")
+                 }
+             Text("Two")
+                 .tabItem{
+                     Image(systemName: "hare.fill")
+                     Text("Two")
+                 }
+             Text("Three")
+                 .tabItem{
+                     Image(systemName: "tortoise.fill")
+                     Text("Three")
+                 }
+             Text("Four")
+                 .tabItem{
+                     Image(systemName: "folder.fill")
+                     Text("Four")
+                 }
          }
      }
  }
