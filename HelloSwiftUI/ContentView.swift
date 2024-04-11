f1edc85f1eab47defbab84a7c0bbdff440568f12
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-11 11:28:10 +0900

    Movie NavigationStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,11 +7,44 @@
 
 import SwiftUI
 
+struct Movie: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let description: String
+}
+
+class MovieLsitViewModel:    ObservableObject {
+    @Published var movies: [Movie] = [
+        Movie(title: "영화 1 ", description: "영화 1 설명"),
+        Movie(title: "영화 2 ", description: "영화 2 설명"),
+        Movie(title: "영화 3 ", description: "영화 3 설명")
+    ]
+}
+
+struct MovieDetailView: View {
+    let movie: Movie
+    
+    var body: some View {
+        VStack {
+            Text(movie.title)
+                .font(.title)
+            Text(movie.description)
+                .padding()
+        }
+    }
+}
 
 struct ContentView: View {
+    @StateObject private var viewModel = MovieLsitViewModel()
     var body: some View {
         NavigationStack {
-
+            List(viewModel.movies) { movie in
+                NavigationLink(movie.title, value: movie)
+            }
+            .navigationTitle("영화 목록")
+            .navigationDestination(for: Movie.self) { movie in
+                MovieDetailView(movie: movie)
+            }
         }
     }
 }
