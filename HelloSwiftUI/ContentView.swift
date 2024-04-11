68b7fec07faa22a742da05806e6d20a4f1b3ec3e
jmbae <jmbae@codegrove.co.kr>
2024-04-11 11:09:20 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,24 +7,11 @@
 
 import SwiftUI
 
-struct FileView: View {
-    var choice: String
-
-    var body: some View {
-        VStack {
-            Text("선택 = \(choice)")
-        }
-    }
-}
-
-
 struct ContentView: View {
    
     var body: some View {
         NavigationStack {
-            NavigationLink(destination: FileView(choice: "헤드")) {
-                Text("헤드 선택")
-            }
+
         }
     }
 }
