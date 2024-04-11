5a89c01aa502bc7c96e09c6065ede20dc8539b81
DopeKS <kyusang.dev@gmail.com>
2024-04-11 16:42:32 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,72 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            let hGridItems1 = [GridItem(.fixed(25))]
-            let hGridItems2 = [GridItem(.flexible(minimum: 25, maximum: 450)), GridItem(.fixed(35)), GridItem(.flexible(minimum: 25, maximum: 450))]
-            let vGridItems1 = [GridItem(.fixed(25))]
-            let vGridItems2 = [GridItem(.flexible(minimum: 25, maximum: 450)), GridItem(.flexible(minimum: 25, maximum: 450)), GridItem(.flexible(minimum: 25, maximum: 450))]
-           
             
-            ScrollView(Axis.Set.horizontal, showsIndicators: true) {
-                LazyHGrid(rows: hGridItems1) {
-                    Image(systemName: "1.circle")
-                    Image(systemName: "2.circle")
-                    Image(systemName: "3.circle")
-                    Image(systemName: "4.circle")
-                    Image(systemName: "5.circle")
-                    Image(systemName: "6.circle")
-                    Image(systemName: "7.circle")
-                    Image(systemName: "8.circle")
-                    Image(systemName: "9.circle")
-                    Image(systemName: "10.circle")
-                }
-                .font(.largeTitle)
-            }
-            ScrollView(Axis.Set.horizontal, showsIndicators: true) {
-                LazyHGrid(rows: hGridItems2) {
-                    Image(systemName: "1.circle")
-                    Image(systemName: "2.circle")
-                    Image(systemName: "3.circle")
-                    Image(systemName: "4.circle")
-                    Image(systemName: "5.circle")
-                    Image(systemName: "6.circle")
-                    Image(systemName: "7.circle")
-                    Image(systemName: "8.circle")
-                    Image(systemName: "9.circle")
-                    Image(systemName: "10.circle")
-                }
-                .font(.largeTitle)
-            }
-            ScrollView(Axis.Set.vertical, showsIndicators: true) {
-                LazyVGrid(columns: vGridItems1) {
-                    Image(systemName: "1.square")
-                    Image(systemName: "2.square")
-                    Image(systemName: "3.square")
-                    Image(systemName: "4.square")
-                    Image(systemName: "5.square")
-                    Image(systemName: "6.square")
-                    Image(systemName: "7.square")
-                    Image(systemName: "8.square")
-                    Image(systemName: "9.square")
-                    Image(systemName: "10.square")
-                }
-                .font(.largeTitle)
-            }
-            ScrollView(Axis.Set.vertical, showsIndicators: true) {
-                LazyVGrid(columns: vGridItems2) {
-                    Image(systemName: "1.square")
-                    Image(systemName: "2.square")
-                    Image(systemName: "3.square")
-                    Image(systemName: "4.square")
-                    Image(systemName: "5.square")
-                    Image(systemName: "6.square")
-                    Image(systemName: "7.square")
-                    Image(systemName: "8.square")
-                    Image(systemName: "9.square")
-                    Image(systemName: "10.square")
-                }
-                .font(.largeTitle)
-            }
         }
     }
 }
