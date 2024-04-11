ebfa2cdedf2bbd30acbd5f8ce39560897cc3680f
avan31017 <avan31017@gmail.com>
2024-04-11 09:15:33 +0900

    DisclosureGroup

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -10,46 +10,23 @@ import SwiftUI
 //특정 버전 지원 제한 -> 15.0 버전 이하는 실행불가.
 @available(iOS 15.0, *)
 struct ContentView: View {
-    @State var flag = false
+    
+    @State var sliderValue = 0.0
     @State var message =  ""
+    @State var flag = true
+    
     var body: some View {
-        VStack (spacing: 20) {
-            Form {
-                Section {
-                    Text("This is Section has no header")
-                }
-                Section("Just a Header") {
-                    Text("This Section uses a simple header")
-                }
-                Section {
-                    Text("This Section  uses a simple footer")
-                } footer: {
-                    Text("Just a Footer")
-                }
-                Section {
-                    Text("This Section uses both a header and footer")
-                } header: {
-                    Text("The header")
-                } footer: {
-                    Text("The footer")
-                }
+        DisclosureGroup("펼치기") {
+            Text("입력한 내용 = \(message)")
+            TextField("입력하세요.", text: $message)
+            Text("Toggle : \(flag)")
+            Toggle(isOn: $flag) {
+                Text("Toggle.")
             }
-            
-            Form {
-                Text("This is a Form")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-            }
-            
-            GroupBox(label: Text("Group Box")) {
-                Text("This is a Group box")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-                TextField("Type here", text: $message)
-            }
+            Text("The slider value = \(sliderValue)")
+            Slider(value: $sliderValue, in: 0...15)
         }
+        .padding()
     }
 }
 
