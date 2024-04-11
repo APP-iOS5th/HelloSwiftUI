e0164bed52f9246c6c42275c7204bdd076e85674
Jin Park <jinpark1504@gmail.com>
2024-04-11 09:26:30 +0900

    disclosureGroup

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,49 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var flag = false
     @State var message = ""
+    @State var flag = true
+    @State var sliderValue = 0.0
+    @State var disclosureExpanded = false
     var body: some View {
-        VStack (spacing: 20) {
-            Form {
-                Section {
-                    Text("This is a section has no header")
-                }
-                Section("Just a Header") {
-                    Text("This section uses a simple header")
-                }
-                Section {
-                    Text("This section uses a simple footer")
-                } footer: {
-                    Text("Just a Footer")
-                }
-                Section {
-                    Text("This section use both a header and a footer")
-                } header: {
-                    Text("The Header")
-                } footer: {
-                    Text("The Footer")
-                }
-                
+        DisclosureGroup(("Expand"), isExpanded: $disclosureExpanded) {
+            Text("text = \(message)")
+            TextField("Type here", text: $message)
+                .padding()
+            Text(flag ? "Toggle = true" : "Toggle = false")
+            Toggle(isOn: $flag) {
+                Text("Toggle")
             }
-            
-            Form {
-                Text("This is a Form")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-            }
-            .frame(maxHeight: 150)
-            
-            GroupBox(label: Text("Group Box")) {
-                Text("This is a Group Box")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-                TextField("Type here", text: $message)
-                    .disabled(flag)
-            }
+            Text("The slider value = \(sliderValue)")
+            Slider(value: $sliderValue, in: 0...15)
+                .padding()
         }
+        .padding()
     }}
 
 #Preview {
