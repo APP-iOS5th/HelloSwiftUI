7d91f29b500b160599987a0fb8cfd01396f7605f
Jongchan Yun <jcyun1996@gmail.com>
2024-04-11 17:42:41 +0900

    animation option and withAnimation block

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,17 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myDegrees: Double = 0.0
+    @State var myDegrees: Double = -180.0
     @State var flag = false
     
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
-                .animation(.default, value: flag)
+        VStack (spacing: 50) {
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+            //                        .animation(.default, value: flag)
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+            //                        .animation(.easeIn, value: flag)
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+            //                        .animation(.easeOut, value: flag)
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+            //                        .animation(.easeInOut, value: flag)
+            
             Button("Animate now") {
-                flag.toggle()
+                withAnimation(.easeInOut(duration: 2.1)) {
+                    flag.toggle()
+                }
             }
             Slider(value: $myDegrees, in: -180...180, step: 1)
                 .padding()
