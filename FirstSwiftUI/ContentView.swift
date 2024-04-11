ed28a59d685f7d071dd700b8b986a7e07c522b1f
avan31017 <avan31017@gmail.com>
2024-04-11 15:17:50 +0900

    TabView

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -7,46 +7,44 @@
 
 import SwiftUI
 
-class ShareViewModel: ObservableObject {
-    @Published var message = ""
-}
-
-struct FileView: View {
-    
-    @EnvironmentObject var choiceVM: ShareViewModel
-    
-    var body: some View {
-        HStack {
-            Spacer()
-            VStack {
-                Spacer()
-                Text("Your choice = \(choiceVM.message)")
-                Spacer()
-            }
-            Spacer()
-        }.background(Color.yellow)
-    }
-}
-
 //특정 버전 지원 제한 -> 15.0 버전 이하는 실행불가.
 @available(iOS 15.0, *)
 struct ContentView: View {
-    
-    @StateObject var showVM = ShareViewModel()
+
     
     var body: some View {
-        NavigationStack {
-            Text("Choose Head or Holy")
-            TextField("Send a message", text: $showVM.message)
-            NavigationLink(destination: FileView()) {
-                Text("button")
-            }
-            NavigationLink(destination: SeparateFile()) {
-                Text("Go to SeparateFile View.")
-            }
-            .navigationTitle("Flip a Coin")
+        TabView {
+            Text("1")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("One")
+                }
+            Text("2")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Two")
+                }
+            Text("3")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Three")
+                }
+            Text("4")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Four")
+                }
+            Text("5")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Five")
+                }
+            Text("6")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Six")
+                }
         }
-        .environmentObject(showVM)
     }
 }
 //#Preview {
