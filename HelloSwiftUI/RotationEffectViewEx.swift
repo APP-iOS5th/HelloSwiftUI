e9bc98499601ff9d5aec49de43b620ce2a9212ac
JongWon Jeong <jjwon214@gmail.com>
2024-04-11 17:20:15 +0900

    RotateAnimate

---
+++ HelloSwiftUI/RotationEffectViewEx.swift
@@ -0,0 +1,30 @@
+//
+//  RotationEffectViewEx.swift
+//  HelloSwiftUI
+//
+//  Created by 정종원 on 4/11/24.
+//
+
+import SwiftUI
+
+struct RotationEffectViewEx: View {
+    @State var myDegrees: Double = 0.0
+    @State var flag = false
+    var body: some View {
+        VStack {
+            Text("Hello world!")
+                .padding()
+                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
+                .animation(.default, value: flag)
+            Button("Animate now") {
+                flag.toggle()
+            }
+            Slider(value: $myDegrees, in: -180...180, step: 1)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    RotationEffectViewEx()
+}
