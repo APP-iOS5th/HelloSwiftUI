d7c43489d86bf39873ea4c5121c1f104f2381a7b
avan31017 <avan31017@gmail.com>
2024-04-11 16:05:29 +0900

    TabView Tag Use

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -10,40 +10,77 @@ import SwiftUI
 //특정 버전 지원 제한 -> 15.0 버전 이하는 실행불가.
 @available(iOS 15.0, *)
 struct ContentView: View {
+    
+    @State var selectedView = 1
 
     
     var body: some View {
-        TabView {
-            Text("1")
-                .tabItem {
-                    Image(systemName: "heart.fill")
-                    Text("One")
+        VStack {
+            HStack {
+                Button(action: {
+                    selectedView = 1
+                }) {
+                    Text("Button 1")
                 }
-            Text("2")
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Two")
+                Button(action: {
+                    selectedView = 2
+                }) {
+                    Text("Button 2")
                 }
-            Text("3")
-                .tabItem {
-                    Image(systemName: "person.fill")
-                    Text("Three")
+                Button(action: {
+                    selectedView = 3
+                }) {
+                    Text("Button 3")
                 }
-            Text("4")
-                .tabItem {
-                    Image(systemName: "person.fill")
-                    Text("Four")
+                Button(action: {
+                    selectedView = 4
+                }) {
+                    Text("Button 4")
                 }
-            Text("5")
-                .tabItem {
-                    Image(systemName: "person.fill")
-                    Text("Five")
+                Button(action: {
+                    selectedView = 5
+                }) {
+                    Text("Button 5")
                 }
-            Text("6")
-                .tabItem {
-                    Image(systemName: "person.fill")
-                    Text("Six")
+                Button(action: {
+                    selectedView = 6
+                }) {
+                    Text("Button 6")
                 }
+            }
+            TabView(selection: $selectedView) {
+                Text("1")
+                    .tabItem {
+                        Image(systemName: "heart.fill")
+                        Text("One")
+                    }.tag(1)
+                Text("2")
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Two")
+                    }.tag(2)
+                Text("3")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Three")
+                    }.tag(3)
+                Text("4")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Four")
+                    }.tag(4)
+                Text("5")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Five")
+                    }.tag(5)
+                Text("6")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Six")
+                    }.tag(6)
+            }
+            .accentColor(.purple)
         }
     }
 }
