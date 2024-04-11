67b81cd80fa3e3b03242a1b203d0865ebe1061de
YuleGlycerine <figurefig13@gmail.com>
2024-04-11 15:56:35 +0900

    hex code color extension

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -2,44 +2,57 @@
 
 import SwiftUI
 
- struct ContentView: View {
-     var body: some View {
-         TabView {
-             Text("One")
-                 .tabItem {
-                     Image(systemName: "heart.fill")
-                     Text("One")
-                 }
-             Text("Two")
-                 .tabItem {
-                     Image(systemName: "hare.fill")
-                     Text("Two")
-                 }
-             Text("Three")
-                 .tabItem {
-                     Image(systemName: "tortoise.fill")
-                     Text("Three")
-                 }
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet(charactersIn: "#"))
+        let rgbValue = UInt32(hex, radix: 16)
+        let r = Double((rgbValue! & 0xFF0000) >> 16) / 255
+        let g = Double((rgbValue! & 0x00FF00) >> 8) / 255
+        let b = Double(rgbValue! & 0x0000FF) / 255
+        self.init(red: r, green: g, blue: b)
+    }
+}
 
-             Text("Four")
-                 .tabItem {
-                     Image(systemName: "folder.fill")
-                     Text("Four")
-                 }
-             Text("Five")
-                 .tabItem {
-                     Image(systemName: "internaldrive.fill")
-                     Text("Five")
-                 }
-             Text("Six")
-                 .tabItem {
-                     Image(systemName: "cloud.drizzle.fill")
-                     Text("Six")
-                 }
 
-         }
-     }
- }
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("One")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("One")
+                }
+            Text("Two")
+                .tabItem {
+                    Image(systemName: "hare.fill")
+                    Text("Two")
+                }
+            Text("Three")
+                .tabItem {
+                    Image(systemName: "tortoise.fill")
+                    Text("Three")
+                }
+            
+            Text("Four")
+                .tabItem {
+                    Image(systemName: "folder.fill")
+                    Text("Four")
+                }
+            Text("Five")
+                .tabItem {
+                    Image(systemName: "internaldrive.fill")
+                    Text("Five")
+                }
+            Text("Six")
+                .tabItem {
+                    Image(systemName: "cloud.drizzle.fill")
+                    Text("Six")
+                }
+            
+        }
+        .accentColor(Color(hex: "#3E3AFF"))
+    }
+}
 #Preview {
     ContentView()
 }
