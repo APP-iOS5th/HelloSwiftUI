2981df6ba12eaac7a625577f1647097c0d4990ae
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-11 17:37:18 +0900

    Image animation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -5,13 +5,34 @@ struct ContentView: View {
     @State var flag = false
     var body: some View {
         VStack{
-            Text("Hello, world!")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegress : 0))
+            
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
                 .animation(.default, value: flag)
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+                .animation(.easeIn, value: flag)
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+                .animation(.easeOut, value: flag)
+            Image(systemName: "tortoise.fill")
+                .font(.system(size: 50))
+                .foregroundStyle(.red)
+                .scaleEffect(flag ? 1.75 : 1)
+                .animation(.easeInOut, value: flag)
+            
             Button("Animate now") {
-                flag.toggle()
+                withAnimation(.easeInOut(duration: 2.1)) {
+                    flag.toggle()
+                }
             }
+            
             Slider(value: $myDegress, in: -180...180, step: 1)
                 .padding()
             
