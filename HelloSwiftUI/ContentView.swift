2019a8261fd43e13e746f83276fd166b8af0f202
ksiomng0929 <nadana0929@gmail.com>
2024-04-11 17:27:54 +0900

    rotationEffect

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,10 +1,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var myDegress: Double = 0.0
+    @State var flag = false
     
     var body: some View {
         VStack {
-            
+           Text("Hello, world")
+                .padding()
+                .rotationEffect(Angle(degrees: flag ? myDegress : 0))
+            Button("Animate now") {
+                flag.toggle()
+            }
+            Text("\(myDegress)°")
+                .padding()
+            Slider(value: $myDegress, in: -100...100, step: 1)
         }
     }
 }
