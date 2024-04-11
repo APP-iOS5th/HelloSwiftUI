79631655b2a4844cda8b9269dbc230b98cd7d2e3
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-11 09:50:27 +0900

    OutlineGroup

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,22 +1,40 @@
 import SwiftUI
 
+class Species: Identifiable {
+    let id = UUID()
+    var name: String
+    var classification: [Species]?
+    init(name: String, classification: [Species]? = nil) {
+        self.name = name
+        self.classification = classification
+    }
+}
+
 struct ContentView: View {
-    @State var flag = false
-    @State var message = ""
-    @State var sliderValue = 0.0
+    var Animals: [Species] = [
+        Species(name: "포유류", classification: [
+            Species(name: "개"),
+            Species(name: "Poodle"),
+            Species(name: "Collie"),
+            Species(name: "St. Bernard"),
+            Species(name: "고양이"),
+            Species(name: "코끼리"),
+            Species(name: "고래"),
+        ]),
+        Species(name: "조류", classification: [
+            Species(name: "Canary"),
+            Species(name: "Parakeet"),
+            Species(name: "Eagle"),
+        ]),
+    ]
     
     var body: some View {
-        DisclosureGroup("펼치기") {
-            Text("입력할 내용 =  \(message)")
-            TextField("Type here", text: $message)
-                .padding()
-            Text(flag ? "Toggle = true" : "Toggle = false")
-            Toggle(isOn: $flag) {
-                Text("The slider value = \(sliderValue)")
-                Slider(value: $sliderValue, in:0...15)
+        List {
+            OutlineGroup(Animals, id: \.id, children: \.classification) { creature in
+                Text(creature.name)
+                
             }
         }
-        .padding()
     }
 }
 
