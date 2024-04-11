231e9fec6195b20009137789e71a2e9f6c95e5fe
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-11 11:01:12 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,35 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var flag = true
-    @State var message = ""
     var body: some View {
         NavigationStack {
-            Text(message)
-            Toggle(isOn: $flag , label: {
-                Text("토글 디스플레이 모드")
-            })
-            .navigationTitle("네비게이션 타이틀")
-            //.navigationBarHidden(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
-            .navigationBarTitleDisplayMode(flag ? .large : .inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button {
-                        message = "iCloud 아이콘 탬됨"
-                    } label: {
-                        Image(systemName: "icloud")
-                    }
-                }
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        message = "완료 버튼 탬됨"
-                    } label: {
-                        Text("완료")
-                    }
-                }
-            }
+            
         }
-        .accentColor(.purple)
     }
 }
 
