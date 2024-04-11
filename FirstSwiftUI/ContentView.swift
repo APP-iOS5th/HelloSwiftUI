f1a65c1eca75f52689fc3ed515f9088e70c088de
avan31017 <avan31017@gmail.com>
2024-04-11 09:47:26 +0900

    OutlineGroup

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -7,18 +7,42 @@
 
 import SwiftUI
 
+class Species: Identifiable {
+    let id = UUID()
+    var name: String
+    var classification:[Species]?
+    init(name: String, classification: [Species]? = nil) {
+        self.name = name
+        self.classification = classification
+    }
+}
+
 //특정 버전 지원 제한 -> 15.0 버전 이하는 실행불가.
 @available(iOS 15.0, *)
 struct ContentView: View {
     
-    @State var sliderValue = 0.0
-    @State var message =  ""
-    @State var flag = true
+    var animals: [Species] = [
+            Species(name: "포유류", classification: [
+                Species(name: "개", classification: [
+                    Species(name: "Poodle"),
+                    Species(name: "Collie"),
+                    Species(name: "St. Bernard"),
+                ]),
+                Species(name: "고양이"),
+                Species(name: "코끼리"),
+                Species(name: "고래"),
+            ]),
+            Species(name: "조류", classification: [
+                Species(name: "Canary"),
+                Species(name: "Parakeet"),
+                Species(name: "Eagle"),
+            ]),
+        ]
     
     var body: some View {
-        ScrollView(Axis.Set.vertical, showsIndicators: true) { // 스크롤 수직/수평 조절과 스크롤 표시여부
-            ForEach(0..<50) {
-                Text("Item #\($0)")
+        List {
+            OutlineGroup(animals, id: \.id, children: \.classification) { creature in
+                Text(creature.name)
             }
         }
     }
