af9d92cc0f6cfc9b30abe7567c9018d01418a21f
avan31017 <avan31017@gmail.com>
2024-04-11 16:45:43 +0900

    animation

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -11,43 +11,15 @@ import SwiftUI
 @available(iOS 15.0, *)
 struct ContentView: View {
     
- 
+    @State var move = true
+    
     var body: some View {
         VStack {
-            let gridItems = [GridItem(.fixed(30)), GridItem(.fixed(30)), GridItem(.fixed(30))]
-            ScrollView(Axis.Set.horizontal) {
-                LazyHGrid(rows: gridItems) {
-                    Image(systemName: "1.circle")
-                    Image(systemName: "2.circle")
-                    Image(systemName: "3.circle")
-                    Image(systemName: "4.circle")
-                    Image(systemName: "5.circle")
-                    Image(systemName: "6.circle")
-                    Image(systemName: "7.circle")
-                    Image(systemName: "8.circle")
-                    Image(systemName: "9.circle")
-                    Image(systemName: "10.circle")
-                    Image(systemName: "11.circle")
-                }
-                .font(.largeTitle)
-                
-            }
-            ScrollView(Axis.Set.vertical) {
-                LazyVGrid(columns: gridItems) {
-                    Image(systemName: "1.square")
-                    Image(systemName: "2.square")
-                    Image(systemName: "3.square")
-                    Image(systemName: "4.square")
-                    Image(systemName: "5.square")
-                    Image(systemName: "6.square")
-                    Image(systemName: "7.square")
-                    Image(systemName: "8.square")
-                    Image(systemName: "9.square")
-                    Image(systemName: "10.square")
-                    Image(systemName: "11.square")
-                }
-                .font(.largeTitle)
-                
+            Text("A Text View")
+                .offset(x: move ? 100 : 0, y: move ? 100 : 0)
+                .animation(.default, value: move)
+            Toggle(isOn: $move) {
+                Text("Toggle me.")
             }
         }
     }
