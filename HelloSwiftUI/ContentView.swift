f67e38d864140bbbf46c79d4bf16cd2f2fa26e7a
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-11 09:07:25 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,45 +9,8 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var flag = false
-    @State var message =  ""
-    
     var body: some View {
-        VStack (spacing: 20){
-            Form {
-                Section { //헤더가 없는 Section
-                    Text("This is Section has no header")
-                }
-                Section("Just a Header") {
-                    Text("This is Section uses a simple header")
-                }
-                Section {
-                    Text("This is Section uses a simple footer")
-                } footer: {
-                    Text("Just a Footer")
-                }
-                Section {
-                    Text("This is Section uses both a header and footer")
-                } header: {
-                     Text("The header")
-                } footer: {
-                    Text("the footer")
-                }
-            }
-            Form {
-                Text("This is a Form")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-                .disabled(flag) //토글버튼 비활성화
-            }
-            GroupBox(label: Text("Group Box")) {
-                Text("This is a Group box")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-                TextField("Type here", text: $message)
-            }
+        VStack{
         }
     }
 }
