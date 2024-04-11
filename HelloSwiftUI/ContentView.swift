b492abce43213a125356c59cad913aeda4077738
yehjinjang <j95033176@gmail.com>
2024-04-11 15:17:58 +0900

    TabView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,6 +10,25 @@ struct ContentView: View{
     var body: some View{
         TabView {
             Text("One")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("One")
+                }
+            Text("Two")
+                .tabItem {
+                    Image(systemName: "hare.fill")
+                    Text("Two")
+                }
+            Text("Three")
+                .tabItem {
+                    Image(systemName: "tortoise.fill")
+                    Text("Three")
+                }
+            Text("Four")
+                .tabItem {
+                    Image(systemName: "tortoise.fill")
+                    Text("Four")
+                }
         }
     }
 }
