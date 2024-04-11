fb5190027990646f6974a24a16413ab7f9ea0cfc
jmbae <jmbae@codegrove.co.kr>
2024-04-11 09:26:04 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,24 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var sliderValue = 0.0
-    @State var message = ""
-    @State var flag = true
-    
     var body: some View {
-        DisclosureGroup("펼치기") {
-            Text("입력한 내용 = \(message)")
-            TextField("Type here", text: $message)
-                .padding()
-            Text(flag ? "Toggle = true" : "Toggle = false")
-            Toggle(isOn: $flag) {
-                Text("Toggle")
-            }
-            Text("The slider value = \(sliderValue)")
-            Slider(value: $sliderValue, in: 0...15)
-                .padding()
+        VStack {
+            
         }
-        .padding()
     }
 }
 
