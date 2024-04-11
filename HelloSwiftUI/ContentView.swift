723f128884b83a25d902d5c68afcad21ebb9c2a8
Jongchan Yun <jcyun1996@gmail.com>
2024-04-11 17:10:35 +0900

    animation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,44 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var move = true
     var body: some View {
         VStack {
-            let gridItems = [GridItem(.flexible(minimum: 25, maximum: 450)),
-                             GridItem(.flexible(minimum: 25, maximum: 450)),
-                             GridItem(.flexible(minimum: 25, maximum: 450))]
-            ScrollView(Axis.Set.horizontal, showsIndicators: true) {
-                LazyHGrid(rows: gridItems) {
-                    Image(systemName: "1.circle")
-                    Image(systemName: "2.circle")
-                    Image(systemName: "3.circle")
-                    Image(systemName: "4.circle")
-                    Image(systemName: "5.circle")
-                    Image(systemName: "6.circle")
-                    Image(systemName: "7.circle")
-                    Image(systemName: "8.circle")
-                    Image(systemName: "9.circle")
-                    Image(systemName: "10.circle")
-                }
-                .font(.largeTitle)
-            }
-            .tint(.purple)
-            ScrollView(Axis.Set.vertical, showsIndicators: true) {
-                LazyVGrid(columns: gridItems) {
-                    Image(systemName: "1.square")
-                    Image(systemName: "2.square")
-                    Image(systemName: "3.square")
-                    Image(systemName: "4.square")
-                    Image(systemName: "5.square")
-                    Image(systemName: "6.square")
-                    Image(systemName: "7.square")
-                    Image(systemName: "8.square")
-                    Image(systemName: "9.square")
-                    Image(systemName: "10.square")
-                    
-                }
-                .font(.largeTitle)
-            }
-            
+            Text("A Text view")
+                .offset(x: move ? 100 : 0, y: move ? 100 : 0)
+                .animation(.default, value: move)
+            Toggle(isOn: $move, label: {
+                Text("Toggle me")
+            })
         }
     }
 }
