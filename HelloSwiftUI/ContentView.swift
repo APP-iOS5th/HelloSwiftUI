280348fcf02ae7a931317058868a063bd741d1f3
YuleGlycerine <figurefig13@gmail.com>
2024-04-11 16:48:14 +0900

    animation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,32 +1,18 @@
-
-
 import SwiftUI
 
-extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet(charactersIn: "#"))
-        let rgbValue = UInt32(hex, radix: 16)
-        let r = Double((rgbValue! & 0xFF0000) >> 16) / 255
-        let g = Double((rgbValue! & 0x00FF00) >> 8) / 255
-        let b = Double(rgbValue! & 0x0000FF) / 255
-        self.init(red: r, green: g, blue: b)
-    }
-}
-
-
-struct ContentView: View {
-    
-    
-    var body: some View {
-        VStack {
-            
-        }
-        .font(.largeTitle)
-        
-        
-        .accentColor(Color(hex: "#3E3AFF"))
-    }
-}
+ struct ContentView: View {
+     @State var move = true
+     var body: some View {
+         VStack {
+             Text("A Text view")
+                 .offset(x: move ? 100 : 0, y: move ? 100 : 0)
+                 .animation(.default, value: move)
+             Toggle(isOn: $move, label: {
+                 Text("Toggle me")
+             })
+         }
+     }
+ }
 #Preview {
     ContentView()
 }
