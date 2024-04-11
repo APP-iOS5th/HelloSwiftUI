5cc9fe38cd1e8d688134fe419f2605c98387a8b6
ksiomng0929 <nadana0929@gmail.com>
2024-04-11 17:39:56 +0900

    saleEffect animation4

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,20 +1,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myDegress: Double = 0.0
+    @State var myDegrees: Double = -180.0
     @State var flag = false
     
     var body: some View {
-        VStack {
-           Text("Hello, world")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegress : 0))
+        VStack (spacing: 50) {
+            Image(systemName: "tortoise.fill")
+                        .font(.system(size: 50))
+                        .foregroundStyle(.red)
+                        .scaleEffect(flag ? 1.75 : 1)
+                        .animation(.default, value: flag)
+            Image(systemName: "tortoise.fill")
+                        .font(.system(size: 50))
+                        .foregroundStyle(.red)
+                        .scaleEffect(flag ? 1.75 : 1)
+                        .animation(.easeIn, value: flag)
+            Image(systemName: "tortoise.fill")
+                        .font(.system(size: 50))
+                        .foregroundStyle(.red)
+                        .scaleEffect(flag ? 1.75 : 1)
+                          .animation(.easeOut, value: flag)
+            Image(systemName: "tortoise.fill")
+                        .font(.system(size: 50))
+                        .foregroundStyle(.red)
+                        .scaleEffect(flag ? 1.75 : 1)
+                        .animation(.easeInOut, value: flag)
+
             Button("Animate now") {
-                flag.toggle()
+                withAnimation(.easeInOut(duration: 2.1)) {
+                    flag.toggle()
+                }
             }
-            Text("\(myDegress)°")
+            Slider(value: $myDegrees, in: -180...180, step: 1)
                 .padding()
-            Slider(value: $myDegress, in: -100...100, step: 1)
         }
     }
 }
