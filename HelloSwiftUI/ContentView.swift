be3dd874487f33c320eba6ec543b8e6c3519a287
YuleGlycerine <figurefig13@gmail.com>
2024-04-11 16:19:00 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -15,68 +15,11 @@ extension Color {
 
 
 struct ContentView: View {
-    @State var selectedView = 1
+    
     
     var body: some View {
         VStack {
-            HStack {
-                Button("1") {
-                    selectedView = 1
-                }
-                Button("2") {
-                    selectedView = 2
-                }
-                Button("3") {
-                    selectedView = 3
-                }
-                Button("4") {
-                    selectedView = 4
-                }
-                Button("5") {
-                    selectedView = 5
-                }
-                Button("6") {
-                    selectedView = 6
-                }
-                
-            }
-            TabView (selection: $selectedView) {
-                Text("One")
-                    .tabItem {
-                        Image(systemName: "heart.fill")
-                        Text("One")
-                    }.tag(1)
-                //태그가 꼬리표로 연결시켜준다
-                Text("Two")
-                    .tabItem {
-                        Image(systemName: "hare.fill")
-                        Text("Two")
-                    }.tag(2)
-                Text("Three")
-                    .tabItem {
-                        Image(systemName: "tortoise.fill")
-                        Text("Three")
-                    }.tag(3)
-                
-                Text("Four")
-                    .tabItem {
-                        Image(systemName: "folder.fill")
-                        Text("Four")
-                    }.tag(4)
-                
-                Text("Five")
-                    .tabItem {
-                        Image(systemName: "internaldrive.fill")
-                        Text("Five")
-                    }.tag(5)
-                Text("Six")
-                    .tabItem {
-                        Image(systemName: "cloud.drizzle.fill")
-                        Text("Six")
-                    }.tag(6)
-            }
-            .tabViewStyle(.page)
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+           
             
         }
         .accentColor(Color(hex: "#3E3AFF"))
