6dfc53e48a263f90bc316749b1f244e6b3348dbb
seonggeun an <eventscripts@nate.com>
2024-04-11 09:37:57 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,10 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView(Axis.Set.vertical, showsIndicators: true) {
-            ForEach(0..<50) {
-                Text("Item # \($0)")
-            }
+        VStack{
+            
         }
     }
 }
