b0f25c3b397849265ad80f0b5d078ad126cbd563
yehjinjang <j95033176@gmail.com>
2024-04-11 10:43:31 +0900

    NavigationStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -6,45 +6,30 @@
 
 import SwiftUI
 
-
-class Species: Identifiable{
-    //hasable 한 id
-    let id = UUID()
-    var name : String
-    //나랑 같은 종류의 타입들을 가지는 자료구조
-    //classification때문에 재귀적으로 구조화 할수있게됨
-    //depth를 아래에서 정의해줌 포유류 >개 > 종류  그래서 재귀적으로 타입들을 가짐
-    var classification:[Species]?
-    init(name: String, classification: [Species]? = nil) {
-        self.name = name
-        self.classification = classification
-    }
-}
-
 struct ContentView: View {
-    var Animals: [Species] = [
-        Species(name: "포유류", classification: [
-            Species(name: "개", classification: [
-                Species(name: "Poodle"),
-                Species(name: "Collie"),
-                Species(name: "St. Bernard"),
-            ]),
-            Species(name: "고양이"),
-            Species(name: "코끼리"),
-            Species(name: "고래"),
-        ]),
-        Species(name: "조류", classification: [
-            Species(name: "Canary"),
-            Species(name: "Parakeet"),
-            Species(name: "Eagle"),
-        ]),]
+    @State var flag = true
+    @State var message = ""
     
     var body: some View {
-        List {
-            OutlineGroup(Animals, id: \.id, children: \.classification){ creature in
-                Text(creature.name)
+        NavigationStack {
+            Text(message)
+            Toggle(isOn: $flag, label: {
+                Text("toggle display mode")
+            })
+            .navigationTitle("navigation title")
+//            .navigationBarHidden(false)
+            .navigationBarTitleDisplayMode(flag ? .large : .inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        message = "iCloud icon tap"
+                    } label: {
+                        Image(systemName: "icloud")
+                    }
+                }
             }
         }
+        .accentColor(.purple)
     }
 }
 #Preview {
