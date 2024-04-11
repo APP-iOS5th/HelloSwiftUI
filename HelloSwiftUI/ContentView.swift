4f422b11971043c8c2bcd51b3d5532231aa50e5f
yehjinjang <j95033176@gmail.com>
2024-04-11 16:45:32 +0900

    animation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,42 +9,17 @@ import SwiftUI
 
 struct ContentView: View{
     
-    @State var selectedView = 1
+    @State var move = true
     
     var body: some View{
         VStack{
-            //가로로 출력하는 그리드아이템은 3개의 기준으로 나뉘어짐 (3개로 설정했기때문)
-            let gridItems = [GridItem(),GridItem(),GridItem()]
-            ScrollView(Axis.Set.horizontal, showsIndicators: true ){
-                LazyHGrid(rows: gridItems){
-                    Image(systemName: "1.circle")
-                    Image(systemName: "2.circle")
-                    Image(systemName: "3.circle")
-                    Image(systemName: "4.circle")
-                    Image(systemName: "5.circle")
-                    Image(systemName: "6.circle")
-                    Image(systemName: "7.circle")
-                    Image(systemName: "8.circle")
-                    Image(systemName: "9.circle")
-                    Image(systemName: "10.circle")
-                }
-                .font(.largeTitle)
-            }
-            ScrollView(Axis.Set.vertical, showsIndicators: true ){
-                LazyVGrid(columns: gridItems){
-                    Image(systemName: "1.square")
-                    Image(systemName: "2.square")
-                    Image(systemName: "3.square")
-                    Image(systemName: "4.square")
-                    Image(systemName: "5.square")
-                    Image(systemName: "6.square")
-                    Image(systemName: "7.square")
-                    Image(systemName: "8.square")
-                    Image(systemName: "9.square")
-                    Image(systemName: "10.square")
-                }
-                .font(.title)
-            }
+            Text("A Text view")
+            //ofset을 position으로 바꿀수있음
+                .offset(x: move ? 100 : 0 , y: move ? 100 : 0)
+                .animation(.default, value: move)
+            Toggle(isOn: $move, label:{
+                Text("Toggle me")
+            })
         }
     }
 }
