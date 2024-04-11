d37f961fab833e0776489ac37b377dfe9c66214d
Jin Park <jinpark1504@gmail.com>
2024-04-11 11:09:22 +0900

    NavigationLink

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -6,35 +6,26 @@
 //
 
 import SwiftUI
+
+struct FileView: View {
+    var choice: String
+    
+    var body: some View {
+        VStack {
+            Text("Selected = \(choice)")
+        }
+    }
+}
 struct ContentView: View {
-    @State var message = ""
-    @State var flag = true
     var body: some View {
         NavigationStack {
-            Text(message)
-            Toggle(isOn: $flag, label: {
-                Text("toggle display mode")
-            })
-            .navigationTitle("Navigation Title")
-            .navigationBarHidden(false)
-            .navigationBarTitleDisplayMode(flag ? .large : .inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button {
-                        message = "iCloud Icond Tapped"
-                    } label: {
-                        Image(systemName: "icloud")
-                    }
-                }
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        message = "Complete Button Tapped"
-                    } label: {
-                        Text("Complete")
-                    }
-                }
+            NavigationLink(destination: FileView(choice: "Head")) {
+                Text("Select Head")
             }
-        }.accentColor(.purple)
+            NavigationLink(destination: FileView(choice: "Second")) {
+                Text("Select Second")
+            }
+        }
     }}
 
 #Preview {
