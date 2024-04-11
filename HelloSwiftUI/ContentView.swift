8e1796528ce651b103d099f9f12dddd71d5f5c1f
wotjs020708 <wotjs020708@gmail.com>
2024-04-11 13:33:14 +0900

    add separte file, FileView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,48 +7,29 @@
 
 import SwiftUI
 
-struct Movie: Identifiable, Hashable {
-    let id = UUID()
-    let title: String
-    let descriptions: String
-}
-
-class MovieListViewModel {
-    var movies: [Movie] = [
-        Movie(title: "영화 1", descriptions: "영화 1 설명"),
-        Movie(title: "영화 2", descriptions: "영화 2 설명"),
-        Movie(title: "영화 3", descriptions: "영화 3 설명")
-    ]
-}
-
-struct MovieDetailView: View {
-    let movie: Movie
+struct FileView: View {
     var body: some View {
-        VStack {
-            Text(movie.title).font(.title)
-            Text(movie.descriptions).padding()
-        }
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                Text("This is a separate structure")
+                Text("that's stored in the same file")
+                Spacer()
+            }
+            Spacer()
+        }.background(Color.yellow)
     }
 }
 
 struct ContentView: View {
-    private var viewModel = MovieListViewModel()
     var body: some View {
-        
-        NavigationStack{
-            List(viewModel.movies) {
-                movie in NavigationLink(movie.title, value: movie)
+        NavigationStack {
+            NavigationLink(destination: FileView()) {
+                Text("Send a message")
             }
-            .navigationTitle("영화 목록")
-            .navigationDestination(for: Movie.self) {
-                movie in
-                //Movie Detail
-                MovieDetailView(movie: movie)
-            }
-
         }
     }
-    
 }
 
 
---
+++ HelloSwiftUI/SeparateFile.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct SeparateFile: View {
+    var body: some View {
+        HStack{
+            Spacer()
+            VStack{
+                Spacer()
+                Text("This is another structure")
+                Text("but stored in a separate file")
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SeparateFile()
+}
