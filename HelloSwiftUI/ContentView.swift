8424b150806e24ff670e9ae82abbca387c0875f8
jmbae <jmbae@codegrove.co.kr>
2024-04-11 11:42:59 +0900

    FileView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 
-struct ContentView: View {    
+struct FileView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                Text("This is a separate structure")
+                Text("that's stored in the same file")
+                Spacer()
+            }
+            Spacer()
+        }.background(Color.yellow)
+    }
+}
+
+struct ContentView: View {
     var body: some View {
         NavigationStack {
+            NavigationLink(destination: FileView()) {
+                Text("Send a message")
+            }
         }
     }
 }
