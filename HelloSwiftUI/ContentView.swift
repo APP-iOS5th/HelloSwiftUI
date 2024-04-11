b8d22c08f0e541a230a7fb1ab428c4c7d998cd58
YuleGlycerine <figurefig13@gmail.com>
2024-04-11 17:22:30 +0900

    rotation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,9 +1,20 @@
 import SwiftUI
 
  struct ContentView: View {
-     @State var changeMe = true
+     @State var myDegree: Double = 0.0
+     @State var flag = false
+    
      var body: some View {
          VStack {
+             Text("Hello, world!")
+                 .padding()
+                 .rotationEffect(Angle(degrees: flag ? myDegree : 0))
+                 .animation(.default, value: flag)
+             Button("Animate now"){
+                 flag.toggle()
+             }
+             Slider(value: $myDegree, in: -180...180, step: 1)
+                 .padding()
              }
          
      }
