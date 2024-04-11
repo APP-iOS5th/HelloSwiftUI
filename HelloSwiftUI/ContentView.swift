009d0a6707c96eaf0f701d32ceae36a3bb3c3b6d
Ruel <c0407h@gmail.com>
2024-04-11 09:30:06 +0900

    scrollView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -14,20 +14,16 @@ struct ContentView: View {
     @State var flag = true
     
     var body: some View {
-        DisclosureGroup("펼치기"){
-            Text("입력한 내용 = \(message)")
-            TextField("Type here", text: $message)
-            Text(flag ? "Toogle = true" : "Toggle = false")
-            Toggle(isOn: $flag) {
-                Text("Toggle")
+        
+        ScrollView(Axis.Set.vertical, showsIndicators: true) {
+            ForEach(0..<50) {
+                Text("Item #\($0)")
             }
-            Text("The slider value = \(sliderValue)")
-            Slider(value: $sliderValue, in: 0...15)
-                .padding()
         }
-        .padding()
+        
+
     }
-    
+
 }
 
 #Preview {
