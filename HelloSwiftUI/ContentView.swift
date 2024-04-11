82f1e64de6f86d41cd5bd49fe18945eab4877e59
DopeKS <kyusang.dev@gmail.com>
2024-04-11 09:50:13 +0900

    OutlineGroup

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,10 +8,40 @@
 
 import SwiftUI
 
+class Species: Identifiable {
+    let id = UUID()
+    var name: String
+    var classification: [Species]? //재귀적으로 불릴 수 있다.(자기자신을 부름)
+    init(name: String, classification: [Species]? = nil) {
+        self.name = name
+        self.classification = classification
+    }
+}
+
 struct ContentView: View {
+    var Animals: [Species] = [
+        Species(name: "포유류", classification: [
+            Species(name: "개", classification: [
+                Species(name: "Poodle"),
+                Species(name: "Collie"),
+                Species(name: "St. Bernard"),
+            ]),
+            Species(name: "고양이"),
+            Species(name: "코끼리"),
+            Species(name: "고래"),
+        ]),
+        Species(name: "조류", classification: [
+            Species(name: "Canary"),
+            Species(name: "Parakeet"),
+            Species(name: "Eagle"),
+        ]),
+    ]
+    
     var body: some View {
         List {
-            
+            OutlineGroup(Animals, id: \.id, children: \.classification) { creature in
+                Text(creature.name)
+            }
         }
     }
 }
