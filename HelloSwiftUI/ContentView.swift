b53c52bc2ef418157f9a5e108d7335821f822c57
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-11 16:45:54 +0900

    grid

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,47 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var selectedView = 1
-
+    let gridItems = [GridItem(.fixed(25)), GridItem(.fixed(25)), GridItem(.fixed(25))]
+    
     var body: some View {
         VStack {
-            HStack {
-                ForEach(1..<5) { index in
-                    Button("\(index)") {
-                        selectedView = index
-                    }
+            ScrollView(Axis.Set.horizontal, showsIndicators: true) {
+                LazyHGrid(rows: gridItems) {
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                    Image(systemName: "4.circle")
+                    Image(systemName: "5.circle")
+                    Image(systemName: "6.circle")
+                    Image(systemName: "7.circle")
+                    Image(systemName: "8.circle")
+                    Image(systemName: "9.circle")
+                    Image(systemName: "10.circle")
                 }
+                .font(.largeTitle)
             }
-            TabView (selection: $selectedView) {
-                Text("One")
-                    .tabItem {
-                        Image(systemName: "heart.fill")
-                        Text("One")
-                    }.tag(1)
-                
-                Text("Two")
-                    .tabItem {
-                        Image(systemName: "hare.fill")
-                        Text("Two")
-                    }.tag(2)
-                
-                Text("Three")
-                    .tabItem {
-                        Image(systemName: "tortoise.fill")
-                        Text("Three")
-                    }.tag(3)
-                
-                
-                Text("Four")
-                    .tabItem {
-                        Image(systemName: "folder.fill")
-                        Text("Four")
-                    }.tag(4)
+            
+            ScrollView(Axis.Set.vertical, showsIndicators: true) {
+                LazyVGrid(columns: gridItems) {
+                    Image(systemName: "1.square")
+                    Image(systemName: "2.square")
+                    Image(systemName: "3.square")
+                    Image(systemName: "4.square")
+                    Image(systemName: "5.square")
+                    Image(systemName: "6.square")
+                    Image(systemName: "7.square")
+                    Image(systemName: "8.square")
+                    Image(systemName: "9.square")
+                    Image(systemName: "10.square")
+                }
+                .font(.largeTitle)
             }
-            .tabViewStyle(.page)
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         }
-        .tint(.teal)
     }
 }
 
