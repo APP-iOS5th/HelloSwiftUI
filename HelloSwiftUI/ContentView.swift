2a5ce8c4c16050d45a98b4904d37604b62cd3617
DopeKS <kyusang.dev@gmail.com>
2024-04-11 16:46:53 +0900

    Animation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,9 +9,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var move = true
+    
     var body: some View {
         VStack {
-            
+            Text("A Text view")
+                .offset(x: move ? 100 : 0, y: move ? 100 :0)
+                .animation(.easeIn, value: move)
+            Toggle(isOn: $move, label: {
+                Text("Toggle me")
+            })
         }
     }
 }
