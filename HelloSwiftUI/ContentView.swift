9a9b18e044bc7cae201ceda4e5bfd723c3ff3398
SungWoon Lee <lsw.netline@gmail.com>
2024-04-11 09:30:49 +0900

    discloser

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,35 +1,28 @@
-//
-//  ContentView.swift
-//  HelloSwiftUI
-//
-//  Created by Jungman Bae on 4/8/24.
-//
-
 import SwiftUI
-
-
 struct ContentView: View {
-    @State private var tempValue: CGFloat = 0
-    @State private var finalValue: CGFloat = 1
-    
+    @State var sliderValue = 0.0
+    @State var message = ""
+    @State var flag = true
+
     var body: some View {
-        VStack {
-            Image(systemName: "star.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width:200, height:200)
-                .scaleEffect(finalValue + tempValue)  //커지고 작아지는 효과.
-                .gesture(
-                MagnificationGesture()
-                    .onChanged{amount in tempValue = amount - 1 }
-                
-                    .onEnded{amount in finalValue += tempValue
-                    tempValue = 0
-                })
-            
-            
+        DisclosureGroup("펼치기") {
+            Text("입력한 내용 = \(message)")
+            TextField("Type here", text: $message)
+            // text : $message 에서 :는 text를 입력하면 $에 text가 업데이트 되는 설정.
+            // 설명은 $에서 text로 방향이 향할 것 같은데, text의 내용이 $에 반영된다.
+            //textㅇ $를 담아서 상자 안에 상자($)에 받는 개념일까.
+                .padding()
+            Text(flag ? "Toggle = true" : "Toggle = false")
+            //상황연산자는 true면 앞에가, false면 뒤가 실행됨. : 를 말함.
+            Toggle(isOn: $flag) {
+                Text("Toggle")
+            }
+            Text("The slider value = \(sliderValue)")
+            Slider(value: $sliderValue, in: 0...15)
+                .padding()
             
         }
+        .padding()
     }
 }
 
