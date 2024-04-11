333c571705dc96b8d64c14d281d66fb03b548484
yehjinjang <j95033176@gmail.com>
2024-04-11 15:14:35 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -6,40 +6,11 @@
 
 import SwiftUI
 
-class ShareString: ObservableObject {
-    @Published var message = ""
-}
-
-struct FileView: View {
-    @EnvironmentObject var choice: ShareString
-    var body: some View {
-        HStack {
-            Spacer()
-            VStack {
-                Spacer()
-                TextField("Type here:", text: $choice.message)
-                Spacer()
-            }
-            Spacer()
-        }.background(Color.yellow)
-    }
-}
-
-struct ContentView: View {
-    @StateObject var showMe = ShareString()
-    
-    var body: some View {
-        NavigationStack {
-            TextField("Type here:", text: $showMe.message)
-            NavigationLink(destination: FileView()) {
-                Text("Heads")
-            }
-            NavigationLink(destination: SeparateFile()) {
-                Text("Tails")
-            }
-            .navigationTitle("Flip a Coin")
+struct ContentView: View{
+    var body: some View{
+        TabView {
+            Text("One")
         }
-        .environmentObject(showMe)
     }
 }
 
