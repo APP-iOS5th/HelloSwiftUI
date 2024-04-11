23caa306259bf011502ab8fc20167cb2650256ec
YuleGlycerine <figurefig13@gmail.com>
2024-04-11 13:54:21 +0900

    parameter passing

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,30 +1,32 @@
-
-
 import SwiftUI
 
 
  struct FileView: View {
+     @Binding var choice: String
      var body: some View {
          HStack {
              Spacer()
              VStack {
                  Spacer()
-                 Text("This is a separate structure")
-                 Text("that's stored in the same file")
+                 TextField("Type here:", text: $choice)
                  Spacer()
              }
              Spacer()
-         }.background(Color.yellow)
-     }
- }
-
+        }.background(Color.yellow)
+    }
+}
  struct ContentView: View {
+     @State var message = ""
      var body: some View {
          NavigationStack {
-             NavigationLink(destination: FileView()) {
-                 Text("Send a message")
-                 Text("Send a message")
+             TextField("Type here:", text: $message)
+             NavigationLink(destination: FileView(choice: $message)) {
+                 Text("Heads")
              }
+             NavigationLink(destination: SeperateFile(passedData: "Tails")) {
+                 Text("Tails")
+             }
+             .navigationTitle("Flip a Coin")
          }
      }
  }
