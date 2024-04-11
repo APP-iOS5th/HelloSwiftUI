c557fec96accfade57f0c272a24e2a25f84bd8b2
wotjs020708 <wotjs020708@gmail.com>
2024-04-11 16:20:50 +0900

    tabStyle

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -32,37 +32,39 @@ struct ContentView: View {
                 }
                 
             }
-        }
-        TabView (selection: $selectedView){
-            Text("One")
-                .tabItem {
-                    Image(systemName: "heart.fill")
-                    Text("One")
-                }.tag(1)
-            Text("Two")
-                .tabItem {
-                    Image(systemName: "hare.fill")
-                    Text("Two")
-                }.tag(2)
-            Text("Three")
-                .tabItem {
-                    Image(systemName: "tortoise.fill")
-                    Text("Three")
-                }.tag(3)
-            Text("Four")
-                .tabItem {
-                    Image(systemName: "folder.fill")
-                    Text("Four")
-                }.tag(4)
-            Text("Five")
-                .tabItem {
-                    Image(systemName: "internaldrive.fill")
-                    Text("Fiv")
-                }.tag(5)
-            Text("Six")
-                .tabItem { Image(systemName: "cloud.drizzle.fill")
-                    Text("Six")
-                }.tag(6)
+            TabView (selection: $selectedView){
+                Text("One")
+                    .tabItem {
+                        Image(systemName: "heart.fill")
+                        Text("One")
+                    }.tag(1)
+                Text("Two")
+                    .tabItem {
+                        Image(systemName: "hare.fill")
+                        Text("Two")
+                    }.tag(2)
+                Text("Three")
+                    .tabItem {
+                        Image(systemName: "tortoise.fill")
+                        Text("Three")
+                    }.tag(3)
+                Text("Four")
+                    .tabItem {
+                        Image(systemName: "folder.fill")
+                        Text("Four")
+                    }.tag(4)
+                Text("Five")
+                    .tabItem {
+                        Image(systemName: "internaldrive.fill")
+                        Text("Fiv")
+                    }.tag(5)
+                Text("Six")
+                    .tabItem { Image(systemName: "cloud.drizzle.fill")
+                        Text("Six")
+                    }.tag(6)
+            }
+            .tabViewStyle(.page)
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         }
         .tint(.black)
     }
