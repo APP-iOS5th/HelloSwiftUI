c325e0a70c17eb2486f17003de860122b6145e58
Jin Park <jinpark1504@gmail.com>
2024-04-11 15:19:36 +0900

    TabView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,39 +1,29 @@
 
 import SwiftUI
-class ShareString: ObservableObject{
-    @Published var message = ""
-}
-struct FileView: View {
-    @EnvironmentObject var choice: ShareString
-    var body: some View {
-        HStack {
-            Spacer()
-            VStack {
-                Spacer()
-                TextField("Type here:", text: $choice.message)
-                    .padding()
-                Spacer()
-            }
-            Spacer()
-        }.background(Color.yellow)
-    }
-}
-
 struct ContentView: View {
-    @StateObject var showMe = ShareString()
     var body: some View {
-        NavigationStack {
-            TextField("Type here: ", text: $showMe.message)
-                .padding()
-            NavigationLink(destination: FileView()) {
-                Text("Send a message")
-            }
-            NavigationLink(destination: SeparateFile()) {
-                Text("Send a message")
-            }
-            .navigationTitle("Flip a Coin")
+        TabView {
+            Text("One")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("One")
+                }
+            Text("Two")
+                .tabItem {
+                    Image(systemName: "hare.fill")
+                    Text("Two")
+                }
+            Text("Three")
+                .tabItem {
+                    Image(systemName: "tortoise.fill")
+                    Text("Three")
+                }
+            Text("Four")
+                .tabItem {
+                    Image(systemName: "folder.fill")
+                    Text("Four")
+                }
         }
-        .environmentObject(showMe)
     }
 }
 
--- HelloSwiftUI/SeparateFile.swift
+++ HelloSwiftUI/SeparateFile.swift
@@ -1,23 +1,13 @@
 import SwiftUI
 
 struct SeparateFile: View {
-    @EnvironmentObject var passedData: ShareString
     var body: some View {
-        HStack {
-            Spacer()
-            VStack {
-                Spacer()
-                TextField("Type here:", text: $passedData.message)
-                    .padding()
-                Spacer()
-            }
-            Spacer()
+        VStack {
+            
         }
     }
 }
 
 #Preview {
-    // preview 를 위해 .constant("") 를 사용해서 가까 부모를 만들어 줌
     SeparateFile()
-        .environmentObject(ShareString())
 }
