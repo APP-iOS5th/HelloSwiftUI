418078e82cbec1696ac08143280022214ed15cd6
Jihye Park <deviloper.park@gmail.com>
2024-04-11 09:26:46 +0900

    DisclosureGroup

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,49 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var flag = false
+    @State var sliderValue = 0.0
     @State var message = ""
+    @State var flag = true
     
     var body: some View {
-        VStack {
-            Form {
-                Section {
-                    Text("This is Section has no header")
-                }
-                Section("Just a Header") {
-                    Text("This Section uses a simple header") // header는 대문자
-                }
-                Section {
-                    Text("This Section uses a simple footer")
-                } footer: {
-                    Text("Just a Footer")
-                }
-                Section {
-                    Text("This Section uses both a header and footer")
-                } header: {
-                    Text("The header").textCase(nil)
-                } footer: {
-                    Text("The footer")
-                }
+        DisclosureGroup("펼치기") {
+            Text("입력한 내용 = \(message)")
+            TextField("Type here", text: $message)
+                .padding()
+            Text(flag ? "Toggle = true" : "Toggle = false")
+            Toggle(isOn: $flag) {
+                Text("Toggle")
             }
-            
-            Form {
-                Text("This is a Form")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-                .disabled(flag) // 토글 비활성화
-            }
-            // 기본 배경색에 텍스트 출력
-            GroupBox(label: Text("Group Box")) {
-                Text("This is a Group box")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-                TextField("Type here", text: $message)
-            }
-            
+            Text("The slider value = \(sliderValue)")
+            Slider(value: $sliderValue, in: 0...15)
+                .padding()
         }
+        .padding()
     }
 }
 
