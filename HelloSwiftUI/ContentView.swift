a69816cc06bdac188b3bb375f1e995fafd0cab55
YuleGlycerine <figurefig13@gmail.com>
2024-04-11 17:16:23 +0900

    clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -3,14 +3,7 @@ import SwiftUI
  struct ContentView: View {
      @State var changeMe = true
      var body: some View {
-         
-             Image(systemName: "tortoise.fill")
-             .font(.system(size: 100))
-             .foregroundStyle(.blue)
-             .scaleEffect(changeMe ? 1.75 : 1)
-             .animation(.default, value: changeMe)
-             .onTapGesture {
-                 changeMe.toggle()
+         VStack {
              }
          
      }
