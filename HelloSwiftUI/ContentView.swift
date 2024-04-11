18a9eab3364cf91945685313ef067fa9038f2d06
DopeKS <kyusang.dev@gmail.com>
2024-04-11 11:01:37 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,37 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var flag = true
-    @State var message = ""
-    
     var body: some View {
         NavigationStack {
-            Text(message)
-            Toggle(isOn: $flag, label: {
-                Text("토글 디스플레이 모드")
-            })
-            .navigationTitle("네비게이션 타이틀")
-//            .navigationBarHidden(false)
-//            .navigationBarHidden(flag)
-            .navigationBarTitleDisplayMode(flag ? .large : .inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button {
-                        message = "iCloud 아이콘 탭됨"
-                    } label: {
-                        Image(systemName: "icloud")
-                    }
-                }
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        message = "완료 버튼 탭됨"
-                    } label: {
-                        Text("완료")
-                    }
-                }
-            }
+            
         }
-        .accentColor(.purple)
     }
 }
 
