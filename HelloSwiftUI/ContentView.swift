0c9fb0ad8107be3c0fd7fbf2202ce07d82a3e116
SungWoon Lee <lsw.netline@gmail.com>
2024-04-11 11:44:22 +0900

    뭔지 못들음. 따로 복습. Struct FileView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,47 +1,38 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by Jungman Bae on 4/8/24.
+//
+
 import SwiftUI
 
 
-struct Movie: Identifiable, Hashable {
-    let id = UUID()
-    let title: String
-    let description: String
+struct FileView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                Text("This is a separate structure")
+                Text("that's stored in the same file")
+                Spacer()
+            }
+            Spacer()
+        }.background(Color.yellow)
+    }
 }
-
-class MovieListModel: ObservableObject {
-    @Published var movies: [Movie] = [
-        Movie(title: "영화 1", description: "영화 1 설명"),
-        Movie(title: "영화 2", description: "영화 2 설명"),
-        Movie(title: "영화 3", description: "영화 3 설명")
-    ]
-}
-
-struct MovieDetailView: View {
-     let movie: Movie
-
-     var body: some View {
-         VStack {
-             Text(movie.title)
-                 .font(.title)
-             Text(movie.description)
-                 .padding()
-         }
-     }
- }
 
 struct ContentView: View {
-    @StateObject private var viewModel = MovieListModel()
-    
     var body: some View {
         NavigationStack {
-            List(viewModel.movies) { movie in
-                NavigationLink(movie.title, value: movie)
+            NavigationLink(destination: FileView()) {
+                Text("Send a message")
             }
-            .navigationTitle("영화 목록")
-            .navigationDestination(for: Movie.self) {movie in MovieDetailView(movie: movie)
         }
     }
 }
-}
+
 #Preview {
     ContentView()
 }
