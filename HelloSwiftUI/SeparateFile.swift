02edf096a50bb29802130b0d14c50dff87eac5ac
Jongchan Yun <jcyun1996@gmail.com>
2024-04-11 11:52:56 +0900

    add separate file

--- HelloSwiftUI/SeparateFile.swift
+++ HelloSwiftUI/SeparateFile.swift
@@ -5,4 +5,23 @@
 //  Created by John Yun on 4/11/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct SeparateFile: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                Text("This is another structure")
+                Text("but stored in a separate file")
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SeparateFile()
+}
