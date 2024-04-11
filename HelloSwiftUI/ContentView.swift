88d94acc178fff30f7954d2718bf3c824f7c7a67
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-11 15:11:43 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -4,43 +4,15 @@
 //
 //  Created by 차지용 on 4/8/24.
 //
-//바인딩을쓰면 거기에 쓰이는 변수를 고칠 수 있게 해줌
 
 import SwiftUI
 
-class ShareString: ObservableObject {
-    @Published var message = ""
-}
-
-struct FileView: View {
-    @EnvironmentObject var choice: ShareString
-    var body: some View {
-        HStack {
-            Spacer()
-            VStack {
-                Spacer()
-                TextField("Type here:", text: $choice.message)
-                Spacer()
-            }
-            Spacer()
-        }
-    }
-}
 
 struct ContentView: View {
-    @StateObject var showMe = ShareString()
     var body: some View {
         NavigationStack {
-            TextField("Type here:", text: $showMe.message)
-            NavigationLink(destination: FileView()) {
-                Text("Head")
-            }
-            NavigationLink(destination: SeparateFile()) {
-                Text("Tails")
-            }
-            .navigationTitle("Flip a Coin")
+            
         }
-        .environmentObject(showMe)
     }
 }
 
