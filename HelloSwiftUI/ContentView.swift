0cf9c5166b9496aeb60a39834e016423ac4faf17
seonggeun an <eventscripts@nate.com>
2024-04-11 15:13:45 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,40 +7,14 @@
 
 import SwiftUI
 
-class ShareString: ObservableObject {
-     @Published var message = ""
- }
 
-struct FileView: View {
-    @EnvironmentObject var choice: ShareString
-    var body: some View {
-        HStack {
-            Spacer()
-            VStack {
-                Spacer()
-                TextField("Type here:", text: $choice.message)
-                Spacer()
-            }
-            Spacer()
-            
-        }
-    }
-}
+
+
  struct ContentView: View {
-     @StateObject var showMe = ShareString()
-
      var body: some View {
-         NavigationStack {
-             TextField("Type here:", text: $showMe.message)
-             NavigationLink(destination: FileView()) {
-                 Text("Heads")
-             }
-             NavigationLink(destination: SeparateFile()) {
-                 Text("Tails")
-             }
-             .navigationTitle("Flip a Coin")
+         VStack{
+             
          }
-         .environmentObject(showMe)
      }
  }
          
