1ae5045e1352bf9c548ec9315aa35dee3ccf247b
Jin Park <jinpark1504@gmail.com>
2024-04-11 11:46:22 +0900

    List in NavigationStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,23 +7,43 @@
 
 import SwiftUI
 
-struct FileView: View {
-    var choice: String
+struct Movie: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let description: String
+}
+
+class MovieListViewModel: ObservableObject {
+    @Published var movies: [Movie] = [
+        Movie(title: "Movie 1", description: "Movie 1 Description"),
+        Movie(title: "Movie 2", description: "Movie 2 Description"),
+        Movie(title: "Movie 3", description: "Movie 3 Description")
+    ]
+}
+
+struct MovieDetailView: View {
+    let movie: Movie
     
     var body: some View {
         VStack {
-            Text("Selected = \(choice)")
+            Text(movie.title)
+                .font(.title)
+            Text(movie.description)
+                .padding()
         }
     }
 }
+
 struct ContentView: View {
+    @StateObject private var viewModel = MovieListViewModel()
     var body: some View {
         NavigationStack {
-            NavigationLink(destination: FileView(choice: "Head")) {
-                Text("Select Head")
+            List(viewModel.movies) { movie in
+                NavigationLink(movie.title, value: movie)
             }
-            NavigationLink(destination: FileView(choice: "Second")) {
-                Text("Select Second")
+            .navigationTitle("Movie List")
+            .navigationDestination(for: Movie.self) { movie in
+                    MovieDetailView(movie: movie)
             }
         }
     }}
