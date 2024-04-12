c80bb13e9ff4a7ff6ad3a5106a4d784d501379f5
jihye <ganzi410@gmail.com>
2024-04-12 09:44:20 +0900

    GeometryReader with VStack,HStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,17 +10,25 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Text("Local X origin = \(geometry.frame(in: .local).origin.x)")
-                Text("Local Y origin = \(geometry.frame(in: .local).origin.y)")
-                Divider()
-                Text("Global X origin = \(geometry.frame(in: .global).origin.x)")
-                Text("Global Y origin = \(geometry.frame(in: .global).origin.y)")
+        VStack {
+            Text("This text View pushes the GeometryReader down")
+            HStack{
+                Text("Pushes to the right")
+                GeometryReader { geometry in
+                    VStack {
+                        Text("Width = \(geometry.size.width)")
+                        Divider()
+                        Text("Local X origin = \(geometry.frame(in: .local).origin.x)")
+                        Text("Local Y origin = \(geometry.frame(in: .local).origin.y)")
+                        Divider()
+                        Text("Global X origin = \(geometry.frame(in: .global).origin.x)")
+                        Text("Global Y origin = \(geometry.frame(in: .global).origin.y)")
+                    }
+                }
+                .background(.yellow)
+                //.ignoresSafeArea()
             }
         }
-        .background(.yellow)
-        // .ignoresSafeArea()
     }
 }
 
