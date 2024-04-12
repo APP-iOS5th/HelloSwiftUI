03d1efd9ee8034565e26f3faadb2b405f7dcbe88
JongWon Jeong <jjwon214@gmail.com>
2024-04-12 09:40:43 +0900

    GeometryReader with VStack,HStack

--- HelloSwiftUI/GeometryReaderView.swift
+++ HelloSwiftUI/GeometryReaderView.swift
@@ -9,21 +9,31 @@ import SwiftUI
 
 struct GeometryReaderView: View {
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Text("Width = \(geometry.size.width)")
-                Text("Height = \(geometry.size.height)")
-                
-                Text("UIScreen = \(UIScreen.main.bounds)")
-
-                Text("Local X origin = \(geometry.frame(in: .local).origin.x)")
-                Text("Local y origin = \(geometry.frame(in: .local).origin.y)")
-                Text("global X origin = \(geometry.frame(in: .global).origin.x)")
-                Text("global y origin = \(geometry.frame(in: .global).origin.y)")
-                //safeArea 공간 뺴고 알려줌
+        
+        VStack {
+            Text("This Text view pushes the geometryReader down")
+            HStack {
+                Text("This Text view pushes tothe geometryReader right")
+                GeometryReader { geometry in
+                    VStack {
+                        
+                        Text("Width = \(geometry.size.width)")
+                        Text("Height = \(geometry.size.height)")
+                        
+                        Text("UIScreen = \(UIScreen.main.bounds)")
+                        
+                        Text("Local X origin = \(geometry.frame(in: .local).origin.x)")
+                        Text("Local y origin = \(geometry.frame(in: .local).origin.y)")
+                        Text("global X origin = \(geometry.frame(in: .global).origin.x)")
+                        Text("global y origin = \(geometry.frame(in: .global).origin.y)")
+                        //safeArea 공간 뺴고 알려줌
+                    }
+                }
+                .background(Color.yellow)
+                .ignoresSafeArea()
             }
         }
-        .backgroundStyle(Color.yellow)
+        
     }
     
 }
