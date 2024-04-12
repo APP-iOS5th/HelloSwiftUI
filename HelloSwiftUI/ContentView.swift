b57c702644f6b0228567454f92ff971cbd9bd7d1
Jin Park <jinpark1504@gmail.com>
2024-04-12 09:41:53 +0900

    GeometryReader Origin

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -6,8 +6,11 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                Text("Width: \(geometry.size.width)")
-                Text("Height: \(geometry.size.height)")
+                Text("Local x origin = \(geometry.frame(in: .local).origin.x)")
+                Text("Local y origin = \(geometry.frame(in: .local).origin.y)")
+                Divider()
+                Text("Global x origin = \(geometry.frame(in: .global).origin.x)")
+                Text("Global y origin = \(geometry.frame(in: .global).origin.y)")
             }
         }
         .background(Color.yellow)
