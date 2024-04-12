8935deb38bc1cf62ff98055ba7f8f26e532207fa
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-12 09:41:13 +0900

    Geometry spcae

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,19 +1,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myDegress: Double = 0.0
-    @State var flag = false
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Text("Local X origin = \(geometry.frame(in: .local).origin.x)")
-                Text("Local Y origin = \(geometry.frame(in: .local).origin.y)")
-                Divider()
-                Text("Global X origin = \(geometry.frame(in: .global).origin.x)")
-                Text("Global Y origin = \(geometry.frame(in: .global).origin.y)")
+        VStack {
+            Text("This Text view pushes the GeometryReader down")
+            HStack {
+                Text("Pushes to the right")
+                GeometryReader { geometry in
+                    VStack {
+                        Text("Local X origin = \(geometry.frame(in: .local).origin.x)")
+                        Text("Local Y origin = \(geometry.frame(in: .local).origin.y)")
+                        Divider()
+                        Text("Global X origin = \(geometry.frame(in: .global).origin.x)")
+                        Text("Global Y origin = \(geometry.frame(in: .global).origin.y)")
+                    }
+                }
+                .background(Color.yellow)
+                .ignoresSafeArea()
             }
         }
-        .background(Color.yellow)
     }
 }
 struct ContentView_Previews: PreviewProvider {
