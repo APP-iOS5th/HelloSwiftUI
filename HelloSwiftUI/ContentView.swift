4367da392df1f74459074afacff608dbc459ca41
jihye <ganzi410@gmail.com>
2024-04-12 09:33:34 +0900

    GeometryReader

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,40 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myDegree: Double = 0.0
-    @State var flag = false
     
     var body: some View {
-        VStack (spacing: 50) {
-            Image(systemName: "tortoise.fill")
-                        .font(.system(size: 100))
-                        .foregroundStyle(.red)
-                        .scaleEffect(flag ? 1.75 : 1)
-//                        .animation(.default, value: flag)
-            Image(systemName: "tortoise.fill")
-                        .font(.system(size: 100))
-                        .foregroundStyle(.red)
-                        .scaleEffect(flag ? 1.75 : 1)
-//                        .animation(.easeIn, value: flag)
-            Image(systemName: "tortoise.fill")
-                        .font(.system(size: 100))
-                        .foregroundStyle(.red)
-                        .scaleEffect(flag ? 1.75 : 1)
-//                        .animation(.easeOut, value: flag)
-            Image(systemName: "tortoise.fill")
-                        .font(.system(size: 100))
-                        .foregroundStyle(.red)
-                        .scaleEffect(flag ? 1.75 : 1)
-//                        .animation(.easeInOut, value: flag)
-            
-            Button("Animate Now") {
-                withAnimation(.easeInOut(duration: 2.1)) {
-                    flag.toggle()
-                }
+        GeometryReader { geometry in
+            VStack {
+                Text("Width = \(geometry.size.width)")
+                Text("Height = \(geometry.size.height)")
             }
-            Slider(value: $myDegree, in: -180...180, step: 1)
-                .padding()
         }
+        .background(.yellow)
+        .ignoresSafeArea()
     }
 }
 
