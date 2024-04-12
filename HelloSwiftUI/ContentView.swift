bed8a57a6d3753431836450057e385fb2d7302d6
SungWoon Lee <lsw.netline@gmail.com>
2024-04-12 09:36:50 +0900

    geord 주석추가

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,8 +12,16 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                Text("width = \(geometry.size.width)")
-                Text("height = \(geometry.size.height)")
+            Text("Local X origin = \(geometry.frame(in: .local).origin.x)")
+                Text("Local Y origin = \(geometry.frame(in: .local).origin.y)")
+                Divider()
+                Text("Global X origin = \(geometry.frame(in: .global).origin.x)")
+                Text("Global X origin = \(geometry.frame(in: .global).origin.y)")
+                // origin을 기준으로 width height 를 정해서 상자를 그린다.
+                //origin은 local/global 을 기준으로 찍는다.
+                //global은 좌표영역에 알약까지 포함해서 나타내는 것 0,59.0...에서 시작한다. local은 그걸 빼고 0,0으로 시작.
+                
+                
             }
             
         }.background(Color.gray)
