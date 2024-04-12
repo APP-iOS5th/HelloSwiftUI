ca84589f00280380994a35bb5edad654dd84fce4
ksiomng0929 <nadana0929@gmail.com>
2024-04-12 09:48:20 +0900

    Geometry min mid max x,y

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,9 +1,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     var body: some View {
         VStack {
+            Text("This Text view pushes the GeometryReader down")
+            HStack {
+                Text("Pushes to the right")
+                GeometryReader { geometry in
+                    VStack {
+                        Text("minX = \(geometry.frame(in: .local).minX)")
+                        Text("midX = \(geometry.frame(in: .local).midX)")
+                        Text("maxX = \(geometry.frame(in: .local).maxX)")
+                        Divider()
+                        Text("minY = \(geometry.frame(in: .local).minY)")
+                        Text("midY = \(geometry.frame(in: .local).midY)")
+                        Text("maxY = \(geometry.frame(in: .local).maxY)")
+                    }
+                }
+                .background(Color.yellow)
+                .ignoresSafeArea()
+            }
         }
     }
 }
