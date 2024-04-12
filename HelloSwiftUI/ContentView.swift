2ff5a361b0389e1acb8ad47592a6e7fc84a86140
seonggeun an <eventscripts@nate.com>
2024-04-12 09:43:54 +0900

    GeometryReader with VStack, HStack

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,22 +9,29 @@ import SwiftUI
 
 
 struct ContentView: View {
-     var body: some View {
-         //기본적으로 아이폰의 SafeArea 영역
-         GeometryReader { geometry in
-             VStack{
-                 Text("Local X origin = \(geometry.frame(in: .local).origin.x)")
-                 Text("Local Y origin = \(geometry.frame(in: .local).origin.y)")
-                 Divider() // 구분선
-                 Text("Global X origin = \(geometry.frame(in: .global).origin.x)")
-                 Text("Global Y origin = \(geometry.frame(in: .global).origin.y)")
-             }
-         }
-         .background(Color.green)
-        // .ignoresSafeArea() SafeArea 영역 허용X
-      }
- }
-         
+    var body: some View {
+        VStack{
+            Text("This Text view pushed the GeometryReader down")
+            HStack {
+                Text("Pushes to the right")
+                //기본적으로 아이폰의 SafeArea 영역
+                GeometryReader { geometry in
+                    VStack{
+                        Text("Width = \(geometry.size.width)")
+                        Divider()
+                        Text("Local X origin = \(geometry.frame(in: .local).origin.x)")
+                        Text("Local Y origin = \(geometry.frame(in: .local).origin.y)")
+                        Divider() // 구분선
+                        Text("Global X origin = \(geometry.frame(in: .global).origin.x)")
+                        Text("Global Y origin = \(geometry.frame(in: .global).origin.y)")
+                    }
+                }
+                .background(Color.green)
+                // .ignoresSafeArea() SafeArea 영역 허용X
+            }
+        }
+    }
+}
 #Preview {
     ContentView()
 }
