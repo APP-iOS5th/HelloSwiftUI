7323de2eb49eac9427aa81e8f25e89b9a2436b6b
Jun Beom Hong <KuKaH99@gmail.com>
2024-04-12 09:21:58 +0900

    GeometryReader

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,43 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myDegrees: Double = 0.0
-    @State var flag = false
-    
     var body: some View {
-        VStack (spacing: 50){
-            Image(systemName: "tortoise.fill")
-                .font(.system(size: 50))
-                .foregroundStyle(Color.red)
-                .scaleEffect(flag ? 1.75 : 1)
-            //.animation(.default, value: flag)
-            Image(systemName: "tortoise.fill")
-                .font(.system(size: 50))
-                .foregroundStyle(Color.red)
-                .scaleEffect(flag ? 1.75 : 1)
-            //.animation(.easeIn, value: flag)
-            Image(systemName: "tortoise.fill")
-                .font(.system(size: 50))
-                .foregroundStyle(Color.red)
-                .scaleEffect(flag ? 1.75 : 1)
-            // .animation(.easeOut, value: flag)
-            Image(systemName: "tortoise.fill")
-                .font(.system(size: 50))
-                .foregroundStyle(Color.red)
-                .scaleEffect(flag ? 1.75 : 1)
-            //.animation(.easeInOut, value: flag)
-            
-            Button("Animate now") {
-                withAnimation(.easeInOut(duration: 2.1)) {
-                    flag.toggle()
+        VStack {
+            GeometryReader { geometry in
+                VStack {
+                    Text("Width = \(geometry.size.width)")
+                    Text("Height = \(geometry.size.height)")
                 }
-                
             }
+            .background(Color.yellow)
+            .ignoresSafeArea()
         }
+        
     }
 }
-
-
 
 #Preview {
     ContentView()
