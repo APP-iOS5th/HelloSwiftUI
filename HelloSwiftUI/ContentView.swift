2434c99539af7c10c0a35ecf8869787b7dbe977d
Jin Park <jinpark1504@gmail.com>
2024-04-12 09:43:59 +0900

    moves GeometryReader

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -4,17 +4,26 @@ struct ContentView: View {
     @State var myDegrees: Double = -180.0
     @State var flag = false
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Text("Local x origin = \(geometry.frame(in: .local).origin.x)")
-                Text("Local y origin = \(geometry.frame(in: .local).origin.y)")
-                Divider()
-                Text("Global x origin = \(geometry.frame(in: .global).origin.x)")
-                Text("Global y origin = \(geometry.frame(in: .global).origin.y)")
+        VStack {
+            Text("This Text view pushes the GeometryReader down")
+            HStack {
+                Text("Pushes it to the right")
+                GeometryReader { geometry in
+                    VStack {
+                        Text("Width = \(geometry.size.width)")
+                        Divider()
+                        Text("Local x origin = \(geometry.frame(in: .local).origin.x)")
+                        Text("Local y origin = \(geometry.frame(in: .local).origin.y)")
+                        Divider()
+                        Text("Global x origin = \(geometry.frame(in: .global).origin.x)")
+                        Text("Global y origin = \(geometry.frame(in: .global).origin.y)")
+                    }
+                }
+                .background(Color.yellow)
+        //        .ignoresSafeArea()
             }
         }
-        .background(Color.yellow)
-//        .ignoresSafeArea()
+        
     }
 }
 
