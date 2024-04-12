2b8c07336ae29eb0e86db481d0e18cb4b4179bd9
seonggeun an <eventscripts@nate.com>
2024-04-12 09:11:59 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,41 +9,8 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
-    @State var myDegrees: Double = 0.0
-    @State var flag = false
-    
      var body: some View {
-         VStack(spacing: 25){
-             Image(systemName: "tortoise.fill")
-                 .font(.system(size:50))
-                 .foregroundStyle(.red)
-                 .scaleEffect(flag ? 1.75 : 1)
-            //     .animation(.default, value: flag)
-             Image(systemName: "tortoise.fill")
-                 .font(.system(size:50))
-                 .foregroundStyle(.red)
-                 .scaleEffect(flag ? 1.75 : 1)
-               //  .animation(.easeIn, value: flag)
-             Image(systemName: "tortoise.fill")
-                 .font(.system(size:50))
-                 .foregroundStyle(.red)
-                 .scaleEffect(flag ? 1.75 : 1)
-               //  .animation(.easeOut, value: flag)
-             Image(systemName: "tortoise.fill")
-                 .font(.system(size:50))
-                 .foregroundStyle(.red)
-                 .scaleEffect(flag ? 1.75 : 1)
-                // .animation(.easeInOut, value: flag)
-             
-             Button("Animate now"){
-                 withAnimation(.easeInOut(duration: 2.1)) {
-                     flag.toggle()
-                 }
-             }
-             Slider(value: $myDegrees, in: -100...180, step: 1)
-                 .padding()
-                 .accentColor(.green)
+         VStack{
          }
      }
  }
