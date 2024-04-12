b8c98ea6d2bd6e8f669bb361b56f93ed65966910
Ruel <c0407h@gmail.com>
2024-04-12 09:33:28 +0900

    origin x,y

--- HelloSwiftUI/GerometryReader.swift
+++ HelloSwiftUI/GerometryReader.swift
@@ -13,13 +13,17 @@ struct GerometryReader: View {
         GeometryReader { geometry in
             
             VStack {
-                Text("Width = \(geometry.size.width)")
-                Text("height = \(geometry.size.height)")
+                
+                Text("Local X origin = \(geometry.frame(in: .local).origin.x)" )
+                Text("Local Y origin = \(geometry.frame(in: .local).origin.y)" )
+                Divider()
+                Text("Global X origin = \(geometry.frame(in: .global).origin.x)" )
+                Text("Global Y origin = \(geometry.frame(in: .global).origin.y)" )
             }
             
         }
-        .background(.red)
-        .ignoresSafeArea()
+        .background(.yellow)
+//        .ignoresSafeArea()
     }
 }
 
