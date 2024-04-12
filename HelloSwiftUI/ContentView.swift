e24cff1cb29a6b54ea1313e7a1615f63233a1c05
Jin Park <jinpark1504@gmail.com>
2024-04-12 09:40:48 +0900

    GeometryReader

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -4,32 +4,14 @@ struct ContentView: View {
     @State var myDegrees: Double = -180.0
     @State var flag = false
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
-//                .animation(.easeIn(duration: 2), value: flag)
-            Text("Hello, world!")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
-//                .animation(.easeOut(duration: 2), value: flag)
-            Text("Hello, world!")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
-//                .animation(.easeInOut(duration: 2), value: flag)
-            Text("Hello, world!")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegrees : 0))
-//                .animation(.default, value: flag)
-            Button("Animate now"){
-                withAnimation(.easeInOut(duration: 2).delay(1)) {
-                    flag.toggle()
-                }
+        GeometryReader { geometry in
+            VStack {
+                Text("Width: \(geometry.size.width)")
+                Text("Height: \(geometry.size.height)")
             }
-            Slider(value: $myDegrees, in: -180...180, step: 1)
-                .padding()
-            Text("The degree is \(Int(myDegrees))")
         }
+        .background(Color.yellow)
+//        .ignoresSafeArea()
     }
 }
 
